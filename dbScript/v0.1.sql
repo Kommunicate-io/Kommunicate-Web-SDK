c22a7fd681aa0ed052697b3054d9f277b30d0c32
Aditya Mehra <mehraaditya71@gmail.com>
2017-10-24 12:10:15 -0600

    KM-39: Added sql query to alter auto_suggest

--- dbScript/v0.1.sql
+++ dbScript/v0.1.sql
@@ -11,24 +11,26 @@ ALTER TABLE business_hours MODIFY COLUMN `closing_day` enum('SUNDAY','MONDAY','T
 
 alter table customers add column role varchar(50), add column industry varchar(50), add column contact_no varchar(20);
 
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('android', 'Github', 'https://github.com/Applozic/Applozic-Android-SDK');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('android', 'Push Notification', 'https://www.applozic.com/docs/android-chat-sdk.html#step-4-push-notification-setup');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('ios', 'Github', 'https://github.com/Applozic/Applozic-iOS-SDK/');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('ios', 'Objective-C', 'https://www.applozic.com/docs/ios-chat-sdk.html#objective-c');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('ios', 'Swift', 'https://www.applozic.com/docs/ios-chat-sdk.html#swift');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('ios', 'Push Notification', 'https://www.applozic.com/docs/android-chat-sdk.html#step-4-push-notification-setup');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('web', 'Github', 'https://github.com/Applozic/Applozic-Web-Plugin/');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('web', 'Sidebox Layout Plugin', 'https://www.applozic.com/docs/web-chat-plugin.html#sidebox-layout');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('web', 'Full Layout Plugin', 'https://www.applozic.com/docs/web-chat-plugin.html#full-view-layout');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('price', 'Its free only for development and testing', 'Let me forward your request to the tech team');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('price', 'Link', 'https://www.applozic.com/price.html');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('greetings', 'Unknown User', 'Hi, may I know your name and company');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('greetings', 'Unknown User', 'May I know your company name');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('docs', 'Android Docs', 'https://www.applozic.com/docs/android-chat-sdk.html#overview');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('docs', 'iOS Docs', 'https://www.applozic.com/docs/ios-chat-sdk.html#overview');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('docs', 'Web Docs', 'https://www.applozic.com/docs/web-chat-plugin.html#overview');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('docs', 'PhoneGap Docs', 'https://www.applozic.com/docs/phonegap-chat-plugin.html');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('docs', 'Platform APIs', 'https://www.applozic.com/docs/platform-api-chat.html');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('docs', 'Configuration', 'https://www.applozic.com/docs/configuration.html');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('check', 'Tech team', 'Let me check with the tech team');
-INSERT INTO auto_suggests (`category`, `name`, `content`) VALUES ('check', 'Forward request', 'Let me forward your request to the tech team');
+alter table auto_suggests add user_name varchar(50);
+
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','android', 'Github', 'https://github.com/Applozic/Applozic-Android-SDK');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','android', 'Push Notification', 'https://www.applozic.com/docs/android-chat-sdk.html#step-4-push-notification-setup');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','ios', 'Github', 'https://github.com/Applozic/Applozic-iOS-SDK/');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','ios', 'Objective-C', 'https://www.applozic.com/docs/ios-chat-sdk.html#objective-c');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','ios', 'Swift', 'https://www.applozic.com/docs/ios-chat-sdk.html#swift');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','ios', 'Push Notification', 'https://www.applozic.com/docs/android-chat-sdk.html#step-4-push-notification-setup');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','web', 'Github', 'https://github.com/Applozic/Applozic-Web-Plugin/');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','web', 'Sidebox Layout Plugin', 'https://www.applozic.com/docs/web-chat-plugin.html#sidebox-layout');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','web', 'Full Layout Plugin', 'https://www.applozic.com/docs/web-chat-plugin.html#full-view-layout');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','price', 'Its free only for development and testing', 'Let me forward your request to the tech team');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','price', 'Link', 'https://www.applozic.com/price.html');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','greetings', 'Unknown User', 'Hi, may I know your name and company');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','greetings', 'Unknown User', 'May I know your company name');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','docs', 'Android Docs', 'https://www.applozic.com/docs/android-chat-sdk.html#overview');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','docs', 'iOS Docs', 'https://www.applozic.com/docs/ios-chat-sdk.html#overview');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','docs', 'Web Docs', 'https://www.applozic.com/docs/web-chat-plugin.html#overview');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','docs', 'PhoneGap Docs', 'https://www.applozic.com/docs/phonegap-chat-plugin.html');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','docs', 'Platform APIs', 'https://www.applozic.com/docs/platform-api-chat.html');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','docs', 'Configuration', 'https://www.applozic.com/docs/configuration.html');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','check', 'Tech team', 'Let me check with the tech team');
+INSERT INTO auto_suggests (`user_name`,`category`, `name`, `content`) VALUES ('km-dev-user','check', 'Forward request', 'Let me forward your request to the tech team');
