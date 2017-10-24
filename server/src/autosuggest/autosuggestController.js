f1745f0eaa0d362d49953c7023fae48888db8a24
Aditya Mehra <mehraaditya71@gmail.com>
2017-10-24 11:30:23 -0600

    KM-39: Added getSuggestionsByUser

--- server/src/autosuggest/autosuggestController.js
+++ server/src/autosuggest/autosuggestController.js
@@ -13,18 +13,33 @@ exports.getAllSuggestions = (req, res) => {
 		.catch(err => {res.status(500).json({code:"INTERNAL_SERVER_ERROR", message:"Something in auto suggest went wrong!"})});
 }
 
+exports.getSuggestionsByUser = (req, res) => {
+
+	autosuggestService.getSuggestionsByUser(req.params.userName)
+		.then(suggestions => {
+			if(!suggestions){
+				res.status(404).json({code:"NO_SUGGESTIONS_FOUND"});
+			}else{
+				res.status(200).json({code:"GOT_ALL_SUGGESTIONS_BY_USER", data:suggestions});
+			}
+		})
+		.catch(err => {res.status(500).json({code:"INTERNAL_SERVER_ERROR", message:"Something in auto suggest went wrong!"})});
+}
+
 exports.createSuggestion = (req, res) => {
 
 	console.log('Request received ', req.body);
 
 	const suggestion = {
+		userName:req.body.userName,
 		category: req.body.category,
 		name:req.body.content,
 		content: req.body.content
 	}
 
-	autosuggestService.createSuggestion()
-		.then(response => {console.log(response)
+	autosuggestService.createSuggestion(suggestion)
+		.then(response => {
+			console.log(response)
 			res.status(200).json({code:"SUGESSTION_CREATED", data:response})
 		})
 		.catch(err => {console.log(err)})
