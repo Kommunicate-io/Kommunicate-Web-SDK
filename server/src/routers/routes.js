7b452fe42ca51b6f9369d8fdfe48feaba1625e9a
Aditya Mehra <mehraaditya71@gmail.com>
2017-10-24 11:32:22 -0600

    KM-39: Added getSuggestionsByUser

--- server/src/routers/routes.js
+++ server/src/routers/routes.js
@@ -61,5 +61,6 @@ miscRouters.get('/tz',userController.getTimezone);
 miscRouters.post('/process-off-business-hours',userController.processOffBusinessHours);
 miscRouters.post('/mail', validate(mailValidation.sendMail),mailController.sendMail);
 autoSuggestRouter.get('/', autoSuggestController.getAllSuggestions);
+autoSuggestRouter.get('/:userName', autoSuggestController.getSuggestionsByUser);
 autoSuggestRouter.post('/', validate(autoSuggestValidation.createSuggestion),autoSuggestController.createSuggestion);
 chatRouter.get('/visitor',chatController.visitorChat);
