283f234b5755a5c94d9a657b40eecad34de79f51
Aditya Mehra <mehraaditya71@gmail.com>
2017-10-25 10:03:49 -0600

    KM-39: Added application_key

--- server/src/autosuggest/validation.js
+++ server/src/autosuggest/validation.js
@@ -2,6 +2,7 @@ const joi = require('joi');
 
 module.exports.createSuggestion = {
 	body:{
+		applicationKey: joi.string().required(),
 		userName: joi.string().required(),
 		category: joi.string().required(),
 		name: joi.string().required(),
