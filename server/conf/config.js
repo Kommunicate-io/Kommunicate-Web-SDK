fdbc1e380adeccb532bf3d75090ce50e218be12b
Suraj Khanduri <surajkhanduri@Surajs-MacBook-Air.local>
2017-11-07 00:02:09 +0530

    updating db url

--- server/conf/config.js
+++ server/conf/config.js
@@ -48,7 +48,7 @@ const config ={
   test: {
     port: 3999,
     db: {
-      url: "mysql://admin:adm1n2ol6@staging-db.czgdklkcj9zm.us-east-1.rds.amazonaws.com:3306/kommunicate_test",
+      url: "mysql://testdbauser:db@u$er2o16@testdb.cz881axbtufc.ap-southeast-1.rds.amazonaws.com:3306/kommunicate_test",
       options: {
         pool: {
           threadPoolMax: 5,
