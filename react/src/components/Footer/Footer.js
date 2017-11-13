1e56df35f1c3a61546ace03bb78378cd15eb7975
devashish <devashish.mamgain@gmail.com>
2017-11-13 13:16:47 +0530

    KM-495 add userreport

--- react/src/components/Footer/Footer.js
+++ react/src/components/Footer/Footer.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
+
+  openUserReport() {
+    console.log("####inside openUserReport");
+    window._urq.push(['Feedback_Open']);
+    //alert('hello');
+  }
+
   render() {
     return (
       <footer className="app-footer">
-        
+        <div className="center">
+          <input type="button" className="user-report btn btn-sm btn-success" onClick={this.openUserReport} value="Ask for a feature" />
+        </div>
       </footer>
     )
   }
