6246051590c0ff5a6e7486a8daa89e07eed50be1
devashish <devashish.mamgain@gmail.com>
2017-11-14 02:05:10 +0530

    KM-479 moving scripts to footer and using config urls

--- react/src/components/Footer/Footer.js
+++ react/src/components/Footer/Footer.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import { getConfig } from '../../config/config';
 
 class Footer extends Component {
 
   openUserReport() {
     window._urq.push(['Feedback_Open']);
+  }
+
+  componentWillMount () {
+    //Support Chat
+    (function(d, m){
+      let o = {"appId":"kommunicate-support","isAnonymousChat":true,"agentId":"devashish@kommunicate.io","agentName":"devashish@kommunicate.io",
+        "groupName":"devashish@kommunicate.io","baseUrl":getConfig().homeUrl};
+      let s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = getConfig().kommunicateApi.pluginUrl;
+      let h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = o;
+    })(document, window.kommunicate || {});
+
+
+
+    //UserReport
+    window._urq = window._urq || [];
+    window._urq.push(['initSite', 'de3cfc74-25e0-4299-aa3b-7c0746ce5966']);
+    var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true;
+    ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s);
   }
 
   render() {
