10aec131e1f7ed3baac4ac30823ef36d6c51ad0b
devashish <devashish.mamgain@gmail.com>
2017-11-29 17:50:45 +0530

    KM-520 change notification icon to Kommunicate favicon

--- react/public/applozic/code/web.js
+++ react/public/applozic/code/web.js
@@ -71,7 +71,7 @@ function initAutoSuggestions() {
     $kmApplozic.fn.applozic({
       baseUrl: applozicBaseUrl,
       notificationIconLink:
-          'https://www.applozic.com/resources/images/applozic_icon.png',
+          'https://www.kommunicate.io/favicon.ico',
       notificationSoundLink: 'https://api.kommunicate.io/plugin/audio/notification_tone.mp3',
       userId: userId,
       appId: appId,
