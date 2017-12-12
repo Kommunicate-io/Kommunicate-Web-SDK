a3d384180f4f4a0e2d848dc0ab0265a9faee1156
devashish <devashish.mamgain@gmail.com>
2017-12-12 22:59:20 +0530

    prod issue error after clicking on notification for incoming chat

--- react/src/components/Aside/Aside.js
+++ react/src/components/Aside/Aside.js
@@ -134,7 +134,7 @@ class Aside extends Component {
         var groupUser = group.users[key];
 
         window.$kmApplozic.fn.applozic("getContactDetail", {"userId": groupUser.userId, callback: function(user) {
-            if (typeof user.roleType !== "undefined" && user.roleType == 1 && user.userId !== "bot") {
+            if (typeof user !== "undefined" && typeof user.roleType !== "undefined" && user.roleType == 1 && user.userId !== "bot") {
               var takeOverEle = document.getElementById("takeover-from-bot");
               takeOverEle.classList.remove("n-vis");
               takeOverEle.classList.add("vis");          
