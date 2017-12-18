a2d7738b10ff205a89605a3c61e21beb74464ccf
rudranand <k.anand2189@gmail.com>
2017-12-18 14:56:43 +0530

    KM-569 api as per REST

--- server/src/routers/routes.js
+++ server/src/routers/routes.js
@@ -86,8 +86,8 @@ miscRouters.post('/mail', validate(mailValidation.sendMail),mailController.sendM
 autoSuggestRouter.get('/', autoSuggestController.getAllSuggestions);
 autoSuggestRouter.get('/:applicationId', autoSuggestController.getSuggestionsByAppId);
 autoSuggestRouter.post('/', validate(autoSuggestValidation.createSuggestion), autoSuggestController.createSuggestion);
-autoSuggestRouter.post('/update',validate(autoSuggestValidation.updateSuggestion), autoSuggestController.updateSuggestion)
-autoSuggestRouter.delete('/delete',validate(autoSuggestValidation.deleteSuggetion), autoSuggestController.deleteSuggetion)
+autoSuggestRouter.patch('/',validate(autoSuggestValidation.updateSuggestion), autoSuggestController.updateSuggestion)
+autoSuggestRouter.delete('/',validate(autoSuggestValidation.deleteSuggetion), autoSuggestController.deleteSuggetion)
 chatRouter.get('/visitor',chatController.visitorChat);
 profileImageRouter.post('/', upload.single('file'), profileImageController.uploadImageToS3);
 conversationRouter.post('/', validate(conversationValidation.createConversation),conversationController.createConversation);
