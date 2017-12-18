9cdccf305a140205404df3b5cf348a43efbbdf74
Suraj Khanduri <surajkhanduri@Surajs-MacBook-Air.local>
2017-12-19 00:39:58 +0530

    KM-562-rich test messaging & templates

--- server/src/webplugin/js/app/mck-sidebox-1.0.js
+++ server/src/webplugin/js/app/mck-sidebox-1.0.js
@@ -1036,7 +1036,8 @@ var MCK_CLIENT_GROUP_MAP = [];
                 var messagePxy = {
                     'type': params.messageType,
                     'contentType': params.type,
-                    'message': message
+                    'message': message,
+                    'metadata':params.metadata||{}
                 };
                 if (params.groupId) {
                     messagePxy.groupId = $applozic.trim(params.groupId);
@@ -3933,18 +3934,12 @@ var MCK_CLIENT_GROUP_MAP = [];
             var FILE_PREVIEW_URL = "/rest/ws/aws/file/";
             var LINK_EXPRESSION = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
             var LINK_MATCHER = new RegExp(LINK_EXPRESSION);
-            var markup = '<div name="message" data-msgdelivered="${msgDeliveredExpr}" data-msgsent="${msgSentExpr}" data-msgtype="${msgTypeExpr}" data-msgtime="${msgCreatedAtTime}" data-msgcontent="${replyIdExpr}" data-msgkey="${msgKeyExpr}" data-contact="${toExpr}" class="mck-m-b ${msgKeyExpr} ${msgFloatExpr} ${msgAvatorClassExpr}">' +
+            /*var markup1 = '<div name="message" data-msgdelivered="${msgDeliveredExpr}" data-msgsent="${msgSentExpr}" data-msgtype="${msgTypeExpr}" data-msgtime="${msgCreatedAtTime}" data-msgcontent="${replyIdExpr}" data-msgkey="${msgKeyExpr}" data-contact="${toExpr}" class="mck-m-b ${msgKeyExpr} ${msgFloatExpr} ${msgAvatorClassExpr}">' +
                 '<div class="mck-clear">' +
                 '<div class="blk-lg-12">' +
                 '<div class="mck-msg-avator blk-lg-3">{{html msgImgExpr}}</div>' +
                 '<div class="mck-msg-box ${msgClassExpr}">' +
-<<<<<<< HEAD
             
-||||||| merged common ancestors
-=======
-                
-                '<div id ="mck-msg-box-text">'
->>>>>>> KM-455 : adding template to get number of  pax per room
                 '<div class= "move-right mck-msg-text"></div>' +
                 '<div class ="mck-msg-reply mck-verticalLine ${msgReplyToVisibleExpr}">' +
                 '<div class="mck-msgto">${msgReplyTo} </div>' +
@@ -3956,13 +3951,43 @@ var MCK_CLIENT_GROUP_MAP = [];
                 '<div class="${nameTextExpr} ${showNameExpr}"><span class="mck-ol-status ${contOlExpr}"><span class="mck-ol-icon" title="${onlineLabel}"></span>&nbsp;</span>${msgNameExpr}</div>' +
                 '<div class="mck-file-text notranslate mck-attachment downloadimage ${downloadIconVisibleExpr}" data-filemetakey="${fileMetaKeyExpr}" data-filename="${fileNameExpr}" data-fileurl= "${fileUrlExpr}" data-filesize="${fileSizeExpr}"><div>{{html fileExpr}}</div> {{html downloadMediaUrlExpr}}</div>' +
                 '<div class="mck-msg-text mck-msg-content"></div>' +
-                '</div>'+
-                '<div id = "mck-msg-box-rich-text class =${kmRichTextMarkupVisibility} >'+
-                '${kmRichTextMarkup}'+
-                '</div>'+
                 '</div>' +
                 '</div>' +
-              
+                '<div id = "mck-msg-box-rich-text-container" class =${kmRichTextMarkupVisibility} >'+
+                '{{html kmRichTextMarkup}}</div>'+
+                '<div class="${msgFloatExpr}-muted mck-text-light mck-text-muted mck-text-xs mck-t-xs">${createdAtTimeExpr} <span class="${statusIconExpr} mck-message-status"></span></div>' +
+                '</div>' +
+                '<div class="n-vis mck-context-menu">' +
+                '<ul>' +
+                '<li><a class="mck-message-forward ${msgForwardVisibleExpr}">${msgForwardExpr}</a></li>' +
+                '<li><a class="mck-message-delete">${msgDeleteExpr}</a></li>' +
+                '<li><a class="mck-message-reply">${msgReplyExpr}</a></li>' +
+                '</ul>' +
+                '</div>' +
+                '</div>';*/
+
+
+
+                var markup=   '<div name="message" data-msgdelivered="${msgDeliveredExpr}" data-msgsent="${msgSentExpr}" data-msgtype="${msgTypeExpr}" data-msgtime="${msgCreatedAtTime}" data-msgcontent="${replyIdExpr}" data-msgkey="${msgKeyExpr}" data-contact="${toExpr}" class="mck-m-b ${msgKeyExpr} ${msgFloatExpr} ${msgAvatorClassExpr}">' +
+                '<div class="mck-clear">' +
+                '<div class="blk-lg-12">' +
+                '<div class="mck-msg-avator blk-lg-3">{{html msgImgExpr}}</div>' +
+                '<div class="mck-msg-box ${msgClassExpr}">' +
+                '<div class= "move-right mck-msg-text"></div>' +
+                '<div class ="mck-msg-reply mck-verticalLine ${msgReplyToVisibleExpr}">' +
+                '<div class="mck-msgto">${msgReplyTo} </div>' +
+                '</div>' +
+                '<div class ="mck-msg-reply mck-verticalLine ${msgReplyDivExpr}">' +
+                '<div class="mck-msgreply-border ${textreplyVisExpr}">${msgReply}</div>' +
+                '<div class="mck-msgreply-border ${msgpreviewvisExpr}">{{html msgPreview}}</div>' +
+                '</div>' +
+                '<div class="${nameTextExpr} ${showNameExpr}"><span class="mck-ol-status ${contOlExpr}"><span class="mck-ol-icon" title="${onlineLabel}"></span>&nbsp;</span>${msgNameExpr}</div>' +
+                '<div class="mck-file-text notranslate mck-attachment downloadimage ${downloadIconVisibleExpr}" data-filemetakey="${fileMetaKeyExpr}" data-filename="${fileNameExpr}" data-fileurl= "${fileUrlExpr}" data-filesize="${fileSizeExpr}"><div>{{html fileExpr}}</div> {{html downloadMediaUrlExpr}}</div>' +
+                '<div class="mck-msg-text mck-msg-content"></div>' +
+                '</div>' +
+                '</div>' +
+                '<div id="mck-msg-box-rich-text-container" class =${kmRichTextMarkupVisibility} >'+
+                '{{html kmRichTextMarkup}}</div>'+
                 '<div class="${msgFloatExpr}-muted mck-text-light mck-text-muted mck-text-xs mck-t-xs">${createdAtTimeExpr} <span class="${statusIconExpr} mck-message-status"></span></div>' +
                 '</div>' +
                 '<div class="n-vis mck-context-menu">' +
@@ -3973,6 +3998,8 @@ var MCK_CLIENT_GROUP_MAP = [];
                 '</ul>' +
                 '</div>' +
                 '</div>';
+
+
               
             var contactbox = '<li id="li-${contHtmlExpr}" class="${contIdExpr}" data-msg-time="${msgCreatedAtTimeExpr}">' + '<a class="${mckLauncherExpr}" href="#" data-mck-conversationid="${conversationExpr}" data-mck-id="${contIdExpr}" data-isgroup="${contTabExpr}">' + '<div class="mck-row" title="${contNameExpr}">' + '<div class="mck-conversation-topic mck-truncate ${contHeaderExpr}">${titleExpr}</div>' + '<div class="blk-lg-3">{{html contImgExpr}}</div>' + '<div class="blk-lg-9">' + '<div class="mck-row">' + '<div class="blk-lg-8 mck-cont-name mck-truncate"><div class="mck-ol-status ${contOlExpr}"><span class="mck-ol-icon" title="${onlineLabel}"></span>&nbsp;</div><strong>${contNameExpr}</strong></div>' + '<div class="mck-text-muted move-right mck-cont-msg-date mck-truncate blk-lg-4">${msgCreatedDateExpr}</div></div>' + '<div class="mck-row">' + '<div class="mck-cont-msg-wrapper blk-lg-6 mck-truncate msgTextExpr"></div>' + '<div class="mck-unread-count-box move-right mck-truncate ${contUnreadExpr}"><span class="mck-unread-count-text">{{html contUnreadCount}}</span></div>' + '</div></div></div></a></li>';
             var convbox = '<li id="li-${convIdExpr}" class="${convIdExpr}">' + '<a class="${mckLauncherExpr}" href="#" data-mck-conversationid="${convIdExpr}" data-mck-id="${tabIdExpr}" data-isgroup="${isGroupExpr}" data-mck-topicid="${topicIdExpr}" data-isconvtab="true">' + '<div class="mck-row mck-truncate" title="${convTitleExpr}">${convTitleExpr}</div>' + '</a></li>';
@@ -4477,19 +4504,13 @@ var MCK_CLIENT_GROUP_MAP = [];
                     fileNameExpr: fileName,
                     fileSizeExpr: fileSize,
                     contOlExpr: olStatus,
-<<<<<<< HEAD
-                    //kmRichTextMarkupVisibility:'n-vis',
-                   // kmRichTextMarkup: Kommunicate.markup.KM_HOTEL_ROOM_PAX_INFO
+                    kmRichTextMarkupVisibility:Kommunicate.isRichTextMessage(msg.metadata)?'vis':'n-vis',
+                    kmRichTextMarkup: Kommunicate.isRichTextMessage(msg.metadata)?Kommunicate.getRichTextMessageTemplate(msg.metadata):""
                     //kmRichTextMarkup: '<div>hello</div>'                    
-||||||| merged common ancestors
-=======
-                    kmRichTextMarkupVisibility:'vis',
-                    kmRichTextMarkup: Kommunicate.markup.KM_HOTEL_ROOM_PAX_INFO
->>>>>>> KM-455 : adding template to get number of  pax per room
                 }];
                 
                 append ? $applozic.tmpl("messageTemplate", msgList).appendTo("#mck-message-cell .mck-message-inner") : $applozic.tmpl("messageTemplate", msgList).prependTo("#mck-message-cell .mck-message-inner");
-
+                Kommunicate.richMsgEventHandler.initializeSlick($applozic);
                 if (msg.contentType === 23) {
 
                     if (msg.metadata.msg_type === "BUTTON") {
@@ -7601,9 +7622,12 @@ var MCK_CLIENT_GROUP_MAP = [];
             var mck_filebox_tmpl = '<div id="mck-filebox-${fileIdExpr}" class="mck-file-box ${fileIdExpr}">' + '<div class="mck-file-expr">' + '<span class="mck-file-content blk-lg-8"><span class="mck-file-lb">{{html fileNameExpr}}</span>&nbsp;<span class="mck-file-sz">${fileSizeExpr}</span></span>' + '<span class="progress progress-striped active blk-lg-3" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><span class="progress-bar progress-bar-success bar" stye></span></span>' + '<span class="move-right">' + '<button type="button" class="mck-box-close mck-remove-file" data-dismiss="div" aria-hidden="true">x</button>' + '</span></div></div>';
             $applozic.template("fileboxTemplate", mck_filebox_tmpl);
             _this.init = function () {
+                //ataching events for rich msh templates
+                Kommunicate.attachEvents ($applozic);
                 $mck_file_upload.on('click', function () {
                     $mck_file_input.trigger('click');
                 });
+
                 $mck_group_icon_upload.on('change', function () {
                     var file = $applozic(this)[0].files[0];
                     _this.uplaodFileToAWS(file, UPLOAD_VIA[0]);
