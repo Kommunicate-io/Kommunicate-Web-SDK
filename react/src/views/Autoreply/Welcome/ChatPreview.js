17d9dde53dee81f3e483e692c04d026365baf0f5
Aditya Mehra <mehraaditya71@gmail.com>
2017-12-21 17:36:59 -0700

    KM-523: Moved welcome part to /Welcome

---
+++ react/src/views/Autoreply/Welcome/ChatPreview.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class ChatPreview extends Component {
+
+  constructor(props) {
+      super(props);
+      console.log(props);
+  }
+
+	render() {
+		return (
+			<div className="user-chat-preview">
+      	<div style={{height: "127px"}}>
+                  {
+                        this.props.chatPreviewComponents.map((chatPreviewComponent, i) => (<div key={i}>{chatPreviewComponent.component}</div>))
+                  }
+      	</div>
+      	<div className="user-reply-section">
+      		<img className="user-reply-section" src="../img/user-reply-section.png" alt="Preview Image" />
+      	</div>
+      </div>
+		)
+	}
+}
+
+export default ChatPreview;
---
+++ react/src/views/Autoreply/Welcome/MessageSection.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+import LinkPopover from './LinkPopover';
+
+class MessageSection extends Component {
+
+	constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
+	state = {
+		msg: ''
+	}
+
+	handleChange = (e) => {
+		this.setState({msg: e.target.value})
+		this.props.getMessage(this.state.msg)
+	}
+
+	render() {
+		console.log(this.props);
+		return (
+			<div>
+				<div className="form-group row">
+					<div className="col-11">
+						<div className="form-group welcome-msg-textarea-btn-container">
+		    			<textarea
+		                	className="form-group script-text-area welcome-message-textarea"
+		                	value={this.state.msg}
+		                	onChange={this.handleChange}
+		                	rows="5"
+		            	/>
+			            {
+			          	 // <button className="welcome-msg-textarea-button"><i className="icon-link icons"></i> Insert Link </button>
+		        		}
+			    			<LinkPopover />
+		    			</div>
+					</div>
+					<div className="col-1">
+	      				<button><i className="fa fa-trash-o fa-lg"></i></button>
+	      			</div>
+	      		</div>
+			</div>	
+		)
+	}
+}
+
+export default MessageSection
