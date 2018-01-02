4e2e86c44ae9d3242bbe98f06260a075065ce978
Aditya Mehra <mehraaditya71@gmail.com>
2018-01-02 00:31:22 -0700

    KM-523: Added api endpoint to get in app msgs

--- server/src/application/validation.js
+++ server/src/application/validation.js
@@ -12,3 +12,33 @@ exports.getWelcomeMessage ={
         appId:joi.string().required()
     }
 }
+exports.getInAppMessages ={
+    param:{
+    	userName:joi.string().required(),
+        appId:joi.string().required()
+    }
+}
+exports.disableInAppMsgs ={
+    param:{
+    	userName:joi.string().required(),
+        appId:joi.string().required()
+    }
+}
+exports.enableInAppMsgs ={
+    param:{
+    	userName:joi.string().required(),
+        appId:joi.string().required()
+    }
+}
+exports.createInAppMsg ={
+    param:{
+    	userName:joi.string().required(),
+        appId:joi.string().required()
+    },
+    body:{
+        eventId:joi.number().required().min(1).max(4),
+        message:joi.string().required(),
+        status:joi.number().required().min(1).max(3),
+        sequence: joi.number().required().min(1).max(3),
+    }
+}
