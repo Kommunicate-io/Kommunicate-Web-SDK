e66eb1be82876087b56cc529b6b8bd84693d9c1f
Vibhor Sharma <vibhor.sharma94@gmail.com>
2018-01-16 18:44:19 +0530

    KM-598 fixed dataName svg error showing in console

--- react/src/components/Sidebar/Sidebar.js
+++ react/src/components/Sidebar/Sidebar.js
@@ -32,8 +32,7 @@ class Sidebar extends Component {
               <NavLink to={'/dashboard'} className="nav-link" activeClassName="active"><i className="icon-speedometer"></i> Dashboard</NavLink>
             </li> */}
             <li className="nav-item">
-              <NavLink to={'/dashboard'} className="nav-link" activeClassName="active" data-rh="Dashboard" data-rh-at="right" data-tip="Dashboard" data-effect="solid" data-place="right"><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' dataName='Layer 1'
-viewBox='0 0 44.5 27.8'>
+              <NavLink to={'/dashboard'} className="nav-link" activeClassName="active" data-rh="Dashboard" data-rh-at="right" data-tip="Dashboard" data-effect="solid" data-place="right"><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' viewBox='0 0 44.5 27.8'>
     <path className='cls-1' d='M24,10.1C11.1,10.1,1.8,20.6,1.8,33.4H17.9a6.4,6.4,0,0,0,12.1,0H46.2C46.2,20.6,36.9,10.1,24,10.1ZM11.2,31H7.2a16.7,16.7,0,0,1,7-13.6l1.5,3.8A12.8,12.8,0,0,0,11.2,31Zm14.7,4.1a3.3,3.3,0,0,1-4.6-1.2l-.2-.5h0v-.2L13.8,14.2,26.5,29.8l.3.3h0l.3.4A3.3,3.3,0,0,1,25.9,35.1ZM24,18.1a12.8,12.8,0,0,0-5.2,1.1l-2.5-3.1A16.8,16.8,0,0,1,40.8,31H36.8A12.8,12.8,0,0,0,24,18.1Z'
     transform='translate(-1.8 -10.1)' />
 </svg></NavLink>
@@ -67,8 +66,7 @@ viewBox='0 0 44.5 27.8'>
               <NavLink to={'/conversations'} className="nav-link" activeClassName="active"><i className="icon-speech"></i> Conversations</NavLink>
             </li> */}
             <li className="nav-item">
-              <NavLink to={'/conversations'} className="nav-link" activeClassName="active" data-tip="Conversations" data-effect="solid" data-place="right"><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' dataName='Layer 1'
-viewBox='0 0 45.8 40'>
+              <NavLink to={'/conversations'} className="nav-link" activeClassName="active" data-tip="Conversations" data-effect="solid" data-place="right"><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' viewBox='0 0 45.8 40'>
     <path className='cls-1' d='M45.7,4H2.3c-.7,0-1.2.8-1.2,1.7V35.2c0,.9.5,1.7,1.2,1.7H19.6l3.7,6.7a.9.9,0,0,0,1.5,0l3.6-6.7H45.7c.7,0,1.2-.8,1.2-1.7V5.7C46.9,4.8,46.4,4,45.7,4ZM26.1,29.4H8.2a1.4,1.4,0,0,1,0-2.8H26.1a1.4,1.4,0,0,1,0,2.8Zm13.6-7.5H8.2a1.3,1.3,0,0,1-1.1-1.4,1.3,1.3,0,0,1,1.1-1.4H39.8a1.3,1.3,0,0,1,1.1,1.4A1.3,1.3,0,0,1,39.8,21.9Zm0-7.6H8.2a1.3,1.3,0,0,1-1.1-1.4,1.3,1.3,0,0,1,1.1-1.4H39.8a1.3,1.3,0,0,1,1.1,1.4A1.3,1.3,0,0,1,39.8,14.3Z'
     transform='translate(-1.1 -4)' />
 </svg></NavLink>
@@ -77,8 +75,7 @@ viewBox='0 0 45.8 40'>
               <NavLink to={'/users'} className="nav-link" activeClassName="active"><i className="icon-user"></i> Customers</NavLink>
             </li> */}
             <li className="nav-item">
-              <NavLink to={'/users'} className="nav-link" activeClassName="active" data-tip="Users" data-effect="solid" data-place="right"><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' dataName='Layer 1'
-viewBox='0 0 45.8 34.6'>
+              <NavLink to={'/users'} className="nav-link" activeClassName="active" data-tip="Users" data-effect="solid" data-place="right"><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' viewBox='0 0 45.8 34.6'>
     <path className='cls-1' d='M12.1,33.5c4.5-1.3,6.1-2.6,6.5-3.1V27.9l-.5-.4a5.7,5.7,0,0,1-1.4-3.6A3.6,3.6,0,0,1,15.2,22a4.5,4.5,0,0,1,.3-3.2,10.6,10.6,0,0,1-.8-3.2,9.3,9.3,0,0,1,1.1-5.2,5.3,5.3,0,0,0-3-.1,6.5,6.5,0,0,0-3.1,1.8,7,7,0,0,0-2,5.4A8.4,8.4,0,0,0,8.4,20a3.3,3.3,0,0,0-.3,2.3,2.5,2.5,0,0,0,1.1,1.4,4.2,4.2,0,0,0,1,2.7l.5.4v2.2c-.3.3-1.4,1.5-5.3,2.6a6.9,6.9,0,0,0-3.6,3.1,6.4,6.4,0,0,0-.7,2.1.8.8,0,0,0,.2.6.8.8,0,0,0,.6.3H7.1v-.2A9.2,9.2,0,0,1,12.1,33.5Z'
     transform='translate(-1.1 -6.7)' />
     <path className='cls-1' d='M46.9,36.9a6.4,6.4,0,0,0-.7-2.1,6.8,6.8,0,0,0-3.7-3.1c-3.9-1.2-5-2.3-5.3-2.6v-2l.4-.4a4.4,4.4,0,0,0,1.1-2.8,2.2,2.2,0,0,0,1.4-1.7,2.9,2.9,0,0,0-.6-2.4c1-3,1-4.9-.1-6.4a3.7,3.7,0,0,0-2.3-1.4h-.5a6,6,0,0,0-4.3-1.7c1.4,2,1.4,4.5.2,8.3a4.2,4.2,0,0,1,.7,3.2,3.3,3.3,0,0,1-1.8,2.4A6.1,6.1,0,0,1,30,27.7l-.4.4v2.1c.5.5,2.1,1.8,6.5,3.1a9.2,9.2,0,0,1,5,4.2v.2h4.9a.9.9,0,0,0,.8-.9Z'
@@ -96,8 +93,7 @@ viewBox='0 0 45.8 34.6'>
               <NavLink to={'/bot'} className="nav-link" activeClassName="active"><i className="icon-magic-wand"></i> Bot <span className="badge badge-info n-vis">NEW</span></NavLink>
             </li> */}
             <li className="nav-item">
-              <NavLink to={'/bot'} className="nav-link" activeClassName="active"  data-tip="Bot" data-effect="solid" data-place="right"><svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" dataName="Layer 1"
-viewBox="0 0 42 46.4" className="bot-icon-svg">
+              <NavLink to={'/bot'} className="nav-link" activeClassName="active"  data-tip="Bot" data-effect="solid" data-place="right"><svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" viewBox="0 0 42 46.4" className="bot-icon-svg">
     <path className="cls-1" d="M40,18.6A18.8,18.8,0,0,0,25,9.3V6.2a2.8,2.8,0,1,0-2.3,0V9.3A18.8,18.8,0,0,0,7.4,18.9,11.5,11.5,0,0,0,7.4,37l.5.8a6.8,6.8,0,0,1-.2,5.8c-1.5,3.2-2.8,3.5-2.8,3.5s6.9.8,11.8-1.7h0a18.8,18.8,0,0,0,23.4-8A11.5,11.5,0,0,0,40,18.6ZM32.4,37.3H15.6a9.4,9.4,0,0,1,0-18.7H32.4a9.4,9.4,0,0,1,0,18.7Z"
     transform="translate(-3 -.8)" />
     <circle className="cls-1" cx="12.4" cy="27.1" r="2.5" />
@@ -113,11 +109,10 @@ viewBox="0 0 42 46.4" className="bot-icon-svg">
               <NavLink to={'/settings/integration'} className="nav-link" activeClassName="active"><i className="fa fa-gear"></i> Integration</NavLink>
             </li> */}
                   <li className="nav-item">
-                    <NavLink to={'/settings/integration'} className={(currentPath.includes('integration') || currentPath.includes('admin') || currentPath.includes('team') || currentPath.includes('autoreply')) ? "nav-link active" : "nav-link"} activeClassName="active" data-tip="Settings" data-effect="solid" data-place="right"><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' dataName='Layer 1'
-viewBox='0 0 46 46'>
-    <path className='cls-1' d='M41,28.9a15.9,15.9,0,0,1-1.5,3.7l2.5,6-3.3,3.3-6-2.5A16,16,0,0,1,28.9,41l-2.6,6H21.6l-2.5-6a15.9,15.9,0,0,1-3.7-1.5l-6,2.5L6.1,38.6l2.5-6A15.9,15.9,0,0,1,7,28.9L1,26.4V21.6l6-2.5a15.9,15.9,0,0,1,1.5-3.7l-2.5-6L9.4,6.1l6,2.5A15.9,15.9,0,0,1,19.1,7l2.6-6h4.7l2.5,6a15.9,15.9,0,0,1,3.7,1.5l6-2.5,3.3,3.3-2.5,6A15.9,15.9,0,0,1,41,19.1l6,2.6v4.7ZM31.1,24A7.1,7.1,0,1,0,24,31.1,7.1,7.1,0,0,0,31.1,24Z'
+                    <NavLink to={'/settings/integration'} className={(currentPath.includes('integration') || currentPath.includes('admin') || currentPath.includes('team') || currentPath.includes('autoreply')) ? "nav-link active" : "nav-link"} activeClassName="active" data-tip="Settings" data-effect="solid" data-place="right"><svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' viewBox='0 0 46 46'>
+                      <path className='cls-1' d='M41,28.9a15.9,15.9,0,0,1-1.5,3.7l2.5,6-3.3,3.3-6-2.5A16,16,0,0,1,28.9,41l-2.6,6H21.6l-2.5-6a15.9,15.9,0,0,1-3.7-1.5l-6,2.5L6.1,38.6l2.5-6A15.9,15.9,0,0,1,7,28.9L1,26.4V21.6l6-2.5a15.9,15.9,0,0,1,1.5-3.7l-2.5-6L9.4,6.1l6,2.5A15.9,15.9,0,0,1,19.1,7l2.6-6h4.7l2.5,6a15.9,15.9,0,0,1,3.7,1.5l6-2.5,3.3,3.3-2.5,6A15.9,15.9,0,0,1,41,19.1l6,2.6v4.7ZM31.1,24A7.1,7.1,0,1,0,24,31.1,7.1,7.1,0,0,0,31.1,24Z'
     transform='translate(-1 -1)' />
-</svg></NavLink>
+                    </svg></NavLink>
                   </li>
             {/* <li className="nav-item">
               <NavLink to={'/team'} className="nav-link" activeClassName="active"><i className="icon-people"></i> Team</NavLink>
