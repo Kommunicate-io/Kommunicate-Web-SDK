8fadd44c4b9ef92598e79b9908a06dcd3b2fb800
Vibhor Sharma <vibhor.sharma94@gmail.com>
2018-01-23 18:40:03 +0530

    KM-674 added ui-library page in jekyll and updated react components

--- react/src/views/Pages/Login/Login.js
+++ react/src/views/Pages/Login/Login.js
@@ -13,6 +13,7 @@ import './login.css';
 import ApplozicClient   from '../../../utils/applozicClient';
 import ValidationUtils from  '../../../utils/validationUtils';
 import { Buffer } from 'buffer';
+import InputField from '../../../components/InputField/InputField';
 
 
 class Login extends Component {
@@ -42,7 +43,9 @@ constructor(props){
     frgtPassSuccessConfirmation: true,
     hideErrorMessage: true,
     errorMessageText: "Please enter user name to login",
-    errorInputColor:''
+    errorInputColor:'',
+    errorMessageTextPassword:'',
+    hideErrorMessagePassword: true
   }
   this.showHide = this.showHide.bind(this);
   this.state=Object.assign({type: 'password'},this.initialState);
@@ -105,8 +108,9 @@ submitForm = ()=>{
   const loginUrl= getConfig().kommunicateApi.login;
   var userName= this.state.userName, password= this.state.password,applicationName=this.state.applicationName, applicationId=this.state.applicationId;
   if(validator.isEmpty(this.state.userName)|| validator.isEmpty(this.state.password)){
-    Notification.warning("Email Id or Password can't be empty!");
-    // _this.setState({hideErrorMessage: false, errorMessageText:"Email Id or Password can't be empty!", errorInputColor:"#ed1c24"});
+    // Notification.warning("Email Id or Password can't be empty!");
+      _this.setState({hideErrorMessagePassword: false, errorMessageTextPassword:"Email Id or Password can't be empty!"});
+    
   }else{
     console.log("inside submit form");
     this.setState({loginButtonDisabled:true});
@@ -182,7 +186,8 @@ login = (event)=>{
     if(!this.state.email && this.state.loginButtonAction ==="getAppList"){
      //alert("please enter user name to login");
     //  Notification.info("please enter user name to login");
-    _this.setState({hideErrorMessage: false, errorMessageText:"Please enter user name to login", errorInputColor:"#ed1c24"});
+    _this.setState({hideErrorMessage: false, errorMessageText:"Please enter user name to login"});
+
       return;
     }
    let param = ValidationUtils.isValidEmail(this.state.email)?"emailId":"userId";
@@ -302,8 +307,7 @@ websiteUrl = (e)=> {
                     <div className="card-block-login-frgtpass-container" hidden={this.state.isLoginFrgtPassHidden}>
                     <h1 className="login-signup-heading">{this.state.loginFormText}</h1>
                     <p className="text-muted login-signup-sub-heading">{this.state.loginFormSubText}</p>
-                    <div className="input-group mb-3" hidden ={this.state.hideUserNameInputbox}>
-                      {/* <span className="input-group-addon"><i className="icon-user"></i></span> */}
+                    {/* <div className="input-group mb-3" hidden ={this.state.hideUserNameInputbox}>
                        <input autoFocus type="text" className="input" placeholder=" "  onChange = { this.setEmail } value={ this.state.email } onBlur ={this.state.handleUserNameBlur} onKeyPress={this.onKeyPress} style={{borderColor: this.state.errorInputColor}} required/>
                        <label className="label-for-input email-label">Email Id</label>
                        <div className="input-error-div" hidden={this.state.hideErrorMessage}>
@@ -321,7 +325,22 @@ websiteUrl = (e)=> {
                         </svg>
                         <p className="input-error-message">{this.state.errorMessageText}</p>
                        </div>
+                    </div> */}
+                    <div hidden ={this.state.hideUserNameInputbox}>
+                      <InputField
+                      inputType={'email'}
+                                  title={'Email Id'}
+                                  name={'email'}
+                                  controlFunc={this.setEmail}
+                                  content={this.state.email}
+                                  errorMessage={this.state.errorMessageText}
+                                  hideErrorMessage={this.state.hideErrorMessage}
+                                  required={'required'}
+                                  blurFunc ={this.state.handleUserNameBlur} keyPressFunc={this.onKeyPress}
+                                  style={{borderColor: this.state.errorInputColor}}/>
                     </div>
+                   
+
                     <div className="input-group mb-4" hidden ={this.state.hideAppListDropdown}>
                       {/* <span className="input-group-addon"><i className="icon-user"></i></span> */}
                       <DropdownButton title={this.state.dropDownBoxTitle}  id="split-button-pull-right" >
@@ -339,7 +358,7 @@ websiteUrl = (e)=> {
                       </DropdownButton>
                     </div>
                     <div className="input-group mb-4" hidden ={this.state.hidePasswordInputbox}>
-                      {/* <span className="input-group-addon"><i className="icon-lock"></i></span> */}
+                    <div className="password-input-label-group">
                       <input type={this.state.type} className="input" placeholder=" "  onChange = { this.setPassword } value={ this.state.password } onKeyPress={this.onKeyPress} style={{borderColor: this.state.errorInputColor}} required/>
                       <label className="label-for-input email-label">Password</label>
                       <span className="show-paasword-btn" onClick={this.showHide}>
@@ -352,7 +371,8 @@ websiteUrl = (e)=> {
                         </svg>}
                         
                       </span>
-                      <div className="input-error-div" hidden={this.state.hideErrorMessage}>
+                      </div>
+                      <div className="input-error-div" hidden={this.state.hideErrorMessagePassword}>
                         <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'>
                           <g id='Page-1' fill='none' fillRule='evenodd'>
                               <g id='Framework' transform='translate(-77 -805)' fill='#ED1C24'>
@@ -365,9 +385,31 @@ websiteUrl = (e)=> {
                               </g>
                           </g>
                         </svg>
-                        <p className="input-error-message">{this.state.errorMessageText}</p>
+                        <p className="input-error-message">{this.state.errorMessageTextPassword}</p>
                        </div>
                     </div>
+                    {/* <div className="password-input-label-group" hidden ={this.state.hidePasswordInputbox}>
+                      <InputField
+                                  inputType={this.state.type}
+                                  title={'Password'}
+                                  name={'password'}
+                                  controlFunc={this.setPassword}
+                                  content={this.state.password}
+                                  errorMessage={this.state.errorMessageTextPassword}
+                                  hideErrorMessage={this.state.hideErrorMessagePassword}
+                                  required={'required'}
+                                  keyPressFunc={this.onKeyPress}/>
+                        <span className="show-paasword-btn" onClick={this.showHide}>
+                      {this.state.type === 'input' ? <svg fill="#999999" height="24" viewBox="0 0 24 24" width="24">
+                          <path d="M0 0h24v24H0z" fill="none"/>
+                          <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+                        </svg> :     <svg xmlns="http://www.w3.org/2000/svg" fill="#999999" height="24" viewBox="0 0 24 24" width="24">
+                          <path d="M0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0z" fill="none"/>
+                          <path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z"/>
+                        </svg>}
+                        
+                      </span>
+                    </div> */}
 
                     <div className="row">
                       <div className="col-3">
