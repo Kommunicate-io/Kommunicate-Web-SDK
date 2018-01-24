c844e148f39cb4d98ac5319025c7fbbab87a1fe1
Vibhor Sharma <vibhor.sharma94@gmail.com>
2018-01-24 13:16:04 +0530

    Added active campaign script in dashboard

--- react/src/utils/ThirdPartyScripts.js
+++ react/src/utils/ThirdPartyScripts.js
@@ -57,6 +57,19 @@ class ThirdPartyScripts extends Component {
             window.fbq('init', '282023555639912'); 
             window.fbq('track', 'PageView');
 
+            //Active Campaign script
+            var trackcmp_email = '';
+  var trackcmp = document.createElement("script");
+  trackcmp.async = true;
+  trackcmp.type = 'text/javascript';
+  trackcmp.src = '//trackcmp.net/visit?actid=66105982&e='+encodeURIComponent(trackcmp_email)+'&r='+encodeURIComponent(document.referrer)+'&u='+encodeURIComponent(window.location.href);
+  var trackcmp_s = document.getElementsByTagName("script");
+  if (trackcmp_s.length) {
+  trackcmp_s[0].parentNode.appendChild(trackcmp);
+  } else {
+  var trackcmp_h = document.getElementsByTagName("head");
+  trackcmp_h.length && trackcmp_h[0].appendChild(trackcmp);
+  }
             
       }
 
