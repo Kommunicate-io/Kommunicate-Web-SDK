cac353000f4d690868b684239a13fa2a9137fa11
Aditya Mehra <mehraaditya71@gmail.com>
2018-02-01 20:17:04 -0700

    KM-572: Added UI

--- react/src/views/Bot/bot.js
+++ react/src/views/Bot/bot.js
@@ -7,8 +7,15 @@ import axios from 'axios';
 import  {getConfig,getEnvironmentId,get} from '../../config/config.js';
 import BotDescription from './BotDescription.js';
 import Notification from '../model/Notification';
-import {getUsersByType,createCustomerOrAgent} from '../../utils/kommunicateClient';
+import {getUsersByType,createCustomerOrAgent, callSendEmailAPI} from '../../utils/kommunicateClient';
 import CommonUtils from '../../utils/CommonUtils';
+import Cato from './images/cato-bot-integration.png'
+import Amazon from './images/amazon-icon.png'
+import Diaglflow from './images/dialogflow-icon.png'
+import Microsoft from './images/microsoft-icon.png'
+import Tick from './images/tick-icon.png'
+import KmIcon from './images/km-icon.png'
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 
 class Tabs extends Component {
 
@@ -28,7 +35,18 @@ class Tabs extends Component {
       dtoken :'',
       // amap of {botId :botName}
       botOptionList:[],
-     
+      useCaseModal: false,
+      dialogFlowModal: false,
+      botProfileModal: false,
+      otherPlatformModal: false,
+      useCaseSubmitted: false,
+      clientToken: '',
+      devToken: '',
+      showNewBot: true,
+      showOldBot: true,
+      botUseCaseText: '',
+      otherPlatformText: '',
+      botName: ''
     };
   let userSession = CommonUtils.getUserSession();
   this.applicationId = userSession.application.applicationId;
@@ -182,10 +200,149 @@ class Tabs extends Component {
         this.setState({userid:e.target.value});
   }
 
+  toggleUseCaseModal = () => {
+      this.setState({
+          useCaseModal: !this.state.useCaseModal
+      });
+    }
+
+  submitUseCase = (type) => {
+
+    if(type === "USE_CASE_REQUEST" && this.state.botUseCaseText.trim().length > 0){
+
+      let options = {
+       templateName: "BOT_USE_CASE_EMAIL",
+       botUseCase: this.state.botUseCaseText,
+       subject: "Custom Bot Request"
+      }
+
+      callSendEmailAPI(options).then(response => {
+        console.log(response);
+        if(response.status ==  200 && response.data.code == "SUCCESS"){
+          Notification.success("Use case submitted");
+          this.toggleUseCaseModal()
+          this.setState({
+            useCaseSubmitted: true,
+            botUseCaseText: ''
+          })
+        }
+      });
+    }else if(type === "BOT_PLATFORM_REQUEST" && this.state.otherPlatformText.trim().length > 0){
+
+      let options = {
+       templateName: "BOT_USE_CASE_EMAIL",
+       botUseCase: this.state.otherPlatformText,
+       subject: "Other Bot Platform Request"
+      }
+
+      callSendEmailAPI(options).then(response => {
+        console.log(response);
+        if(response.status ==  200 && response.data.code == "SUCCESS"){
+          Notification.success("Other bot platform request submitted");
+          this.toggleOtherPlatformModal()
+          this.setState({otherPlatformText: ''})
+        }
+      });
+    }
+  }
+
+  toggleDialogFlowModal = () => {
+      this.setState({
+          dialogFlowModal: !this.state.dialogFlowModal
+      });
+    }
+
+  toggleBotProfileModal = () => {
+    this.setState({
+        botProfileModal: !this.state.botProfileModal
+    });
+  }
+
+  integrateBot = (aiPlatform) => {
+
+    console.log(aiPlatform);
+    console.log(this.state.devToken);
+    console.log(this.state.clientToken);
+
+    if( this.state.clientToken.trim().length > 0 && this.state.devToken.trim().length > 0 ){
+      this.toggleDialogFlowModal()
+      this.toggleBotProfileModal()
+    }
+
+    // var _this =this;
+
+    // var data = {
+    //   "clientToken" : this.state.clientToken,
+    //   "devToken" : this.state.devToken,
+    //   "aiPlatform" : aiPlatform,
+    //   "botname":this.state.bot.value
+    // }
+
+    // let userSession = CommonUtils.getUserSession();
+    // var applicationId = userSession.application.applicationId;
+    // var authorization = userSession.authorization;
+    // var password = CommonUtils.getUserSession().password;
+    // var device = atob(authorization);
+    // var devicekey = device.split(":")[1];
+    // var env = getEnvironmentId();
+    // var userDetailUrl =getConfig().applozicPlugin.userDetailUrl;
+
+    // if(!this.state.bot){
+    //   Notification.info("Please select a bot!!");
+    //   return;
+    // }else if(!this.state.ctoken){
+    //   Notification.info("Please enter the client token!!");
+    //   return;
+    // }else if(!this.state.dtoken){
+    //   Notification.info("Please select a developer token!!");
+    //   return;
+    // }
+
+    // axios({
+    //   method: 'post',
+    //   url:userDetailUrl,
+    //   data:{
+    //        "userIdList" : [data.botname]
+    //      },
+    //   headers: {
+    //     "Apz-Product-App": true,
+    //     "Apz-Token": 'Basic ' + new Buffer(CommonUtils.getUserSession().userName+':'+CommonUtils.getUserSession().password).toString('base64'),
+    //     "Content-Type": "application/json",
+    //     "Apz-AppId":applicationId
+    //   }})
+    //   .then(function(response){
+    //    if(response.status==200 ){
+    //       console.log("success");
+    //        axios({
+    //      method: 'post',
+    //      url:getConfig().applozicPlugin.addBotUrl+"/"+response.data.response[0].id+'/configure',
+    //      data:JSON.stringify(data),
+    //      headers: {
+    //       "Content-Type": "application/json",
+    //      }
+    //       })
+    //   .then(function(response){
+    //    if(response.status==200 ){
+    //     _this.clearBotForm();
+    //       Notification.info("Bot configured successfully");
+         
+    //       }
+    //    });
+    //       }
+    //    });
+  }
+
+  toggleOtherPlatformModal = () => {
+    this.setState({
+      otherPlatformModal: !this.state.otherPlatformModal
+    })
+  }
+
   render() {
     return (
-      <div className="animated fadeIn">
-        <div className="row">
+      <div className="animated fadeIn" >
+        {/* Change showOldBot to false to hide old bot section*/}
+        <div className="row" style={{display: this.state.showOldBot ? null:"none"} }>
           <div className="col-md-6 mb-4">
             <Nav tabs>
               <NavItem>
@@ -314,6 +471,184 @@ class Tabs extends Component {
             <BotDescription type ={this.state.descriptionType} header={this.state.descriptionHeader} />
           </div>
         </div>
+      {/* Change showNewBot to false to hide new bot section*/}
+        <div className="card" style={{display: this.state.showNewBot ? null:"none"} }>
+          <div className="card-block">
+            <div style={{width: "60%", margin: "0 auto"}}>
+              <div className="row">
+                <div className="col-sm-12 km-bot-integration-heading">
+                  <p>Integrating a bot will allow you to send answers to some customer <br />queries automatically</p>
+                </div>
+              </div>
+              <div className={!this.state.useCaseSubmitted ? "row mt-4 km-bot-integration-second-container":"n-vis"}>
+                <div className="col-sm-6 km-bot-integration-second-container-text-container">
+                  <p>Want a custom bot?</p>
+                  <p>Tell us your bot use-case and we will take <br />care of everything else</p>
+                  <p onClick={this.toggleUseCaseModal}>REQUEST CUSTOM BOT</p>
+                </div>
+                <div className="col-sm-1">
+                </div>
+                <div className="col-sm-4 km-bot-integration-second-container-cato-container">
+                  <img src={Cato} className="km-bot-integration-cato" />
+                </div>
+              </div>
+              <div className={this.state.useCaseSubmitted ? "row mt-4 km-bot-integration-use-case-sbmt-container":"n-vis"}>
+                <div className="col-sm-1 km-bot-integration-tick-container">
+                  <img src={Tick} className="km-bot-integration-tick-icon" />
+                </div>
+                <div className="col-sm-9 km-bot-integration-use-case-sbmt-text-container">
+                  <p>Use case submitted. We will get back to you over phone or <br />email soon with more details.</p>
+                  <p>Schedule a call with our Product Team right away if you have something more to add. We will be glad to assist !!</p>
+                  <a className="btn schedule-call-btn" href="https://calendly.com/kommunicate/15min" target="_blank">Schedule a Call</a>
+                </div>
+              </div>
+              <div className="row mt-4">
+                <div className="col-sm-2">
+                </div>
+                <div className="col-sm-8 km-bot-integration-third-container">
+                  <p><strong>OR</strong>, integrate a bot from one of the platforms below</p>
+                </div>
+                <div className="col-sm-2">
+                </div>
+              </div>
+              <div className="row mt-4">
+                <div className="col-sm-3 km-bot-integration-logo-container" style={{textAlign: "center"}}>
+                  <img src={Diaglflow} className="km-bot-integration-dialogflow-icon km-bot-integration-icon-margin" />
+                  <p className="km-bot-integration-dialogflow-text">Dialogflow <br />(Api.ai)</p>
+                  <p onClick={this.toggleDialogFlowModal} style={{cursor: "pointer", color: "#5c5aa7"}}>Settings</p>
+                </div>
+                <div style={{textAlign: "center", width:"12.5%"}}>
+                  <p></p>
+                </div>
+                <div className="col-sm-3 km-bot-integration-logo-container" style={{textAlign: "center"}}>
+                  <img src={Microsoft} className="km-bot-integration-microsoft-icon km-bot-integration-icon-margin" />
+                  <p className="km-bot-integration-microsoft-text">Microsoft Bot <br />Framework</p>
+                  <p className="km-bot-integration-coming-soon">Coming Soon</p>
+                </div>
+                <div style={{textAlign: "center", width:"12.5%"}}>
+                  <p></p>
+                </div>
+                <div className="col-sm-3 km-bot-integration-logo-container" style={{textAlign: "center"}}>
+                  <img src={Amazon} className="km-bot-integration-amazon-icon km-bot-integration-icon-margin" />
+                  <p className="km-bot-integration-amazon-text">Amazon Lex</p>
+                  <p className="km-bot-integration-coming-soon">Coming Soon</p>
+                </div>
+              </div>
+              <div className="row mt-4">
+                <div className="col-sm-12" style={{textAlign: "center"}}>
+                  <a className="btn km-bot-integration-other-pltform" onClick={this.toggleOtherPlatformModal}>Have some other platform in mind? Let us know</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Modal isOpen={this.state.useCaseModal} toggle={this.toggleUseCaseModal} className="modal-dialog">
+          <ModalHeader toggle={this.toggleUseCaseModal}>
+            <img src={KmIcon} className="km-bot-integration-dialogflow-icon" />
+          </ModalHeader>
+          <ModalBody>
+            <div className="row">
+              <div className="col-sm-12">
+                <p className="km-bot-integration-use-case-modal-text">Please explain your bot use case:</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-12">
+                <textArea rows="5" className="form-control" style={{resize: "none"}} placeholder="Example: I need a bot for hotel booking. It should be able to manage bookings." onChange={(event) => this.setState({botUseCaseText: event.target.value})} value={this.state.botUseCaseText} />
+              </div>
+            </div>
+            <div className="row" style={{marginTop: "66px"}}>
+              <div className="col-sm-12 text-right">
+                <button className="btn btn-primary" onClick={ () => {this.submitUseCase("USE_CASE_REQUEST")} }>
+                  Submit Usecase
+                </button>
+              </div>  
+            </div>
+          </ModalBody>
+        </Modal>
+        <Modal isOpen={this.state.otherPlatformModal} toggle={this.toggleOtherPlatformModal} className="modal-dialog">
+          <ModalHeader toggle={this.toggleOtherPlatformModal}>
+            <img src={KmIcon} className="km-bot-integration-dialogflow-icon" />
+          </ModalHeader>
+          <ModalBody>
+            <div className="row">
+              <div className="col-sm-12">
+                <p className="km-bot-integration-use-case-modal-text">Please explain other bot platform:</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-12">
+                <textArea rows="5" className="form-control" style={{resize: "none"}} placeholder="Example: I need to integrate with this bot platform." onChange={(event) => this.setState({otherPlatformText: event.target.value})} value={this.state.otherPlatformText} />
+              </div>
+            </div>
+            <div className="row" style={{marginTop: "66px"}}>
+              <div className="col-sm-12 text-right">
+                <button className="btn btn-primary" onClick={ () => {this.submitUseCase("BOT_PLATFORM_REQUEST")}}>
+                  Submit Platform Request
+                </button>
+              </div>
+            </div>
+          </ModalBody>
+        </Modal>
+        <Modal isOpen={this.state.dialogFlowModal} toggle={this.toggleDialogFlowModal} className="modal-dialog">
+          <ModalHeader toggle={this.toggleDialogFlowModal}>
+            <img src={Diaglflow} className="km-bot-integration-dialogflow-icon" />
+            <span className="km-bot-integration-use-case-modal-text">Integrating your Dialogflow bot with Kommunicate</span>
+          </ModalHeader>
+          <ModalBody>
+            <div className="row">
+              <div className="col-sm-12">
+                <p className="km-bot-integration-use-case-modal-text">Instructions:</p>
+              </div>
+            </div>
+            <div className="row">
+              <label className="col-sm-3" htmlFor="hf-password">Client Token:</label>
+              <div className="col-sm-9">
+                <input type="text" onChange = {(event) => this.setState({clientToken:event.target.value})} value ={this.state.clientToken} name="hf-password" className="form-control input-field"/>
+              </div>
+            </div>
+            <div className="row mt-4">
+              <label className="col-md-3" htmlFor="hf-password">Dev Token:</label>
+              <div className="col-md-9">
+                <input type="text" onChange = {(event) => this.setState({devToken:event.target.value})} value ={this.state.devToken} name="hf-password" className="form-control input-field"/>
+              </div>
+            </div>
+            <div className="row" style={{marginTop: "66px"}}>
+              <div className="col-sm-12 text-right">
+                <button className="btn btn-primary" onClick={() => {this.integrateBot("api.ai")}}>
+                  Next
+                </button>
+              </div>  
+            </div>
+          </ModalBody>
+        </Modal>
+        <Modal isOpen={this.state.botProfileModal} toggle={this.toggleBotProfileModal} className="modal-dialog">
+          <ModalHeader toggle={this.toggleBotProfileModal}>
+            <img src={KmIcon} className="km-bot-integration-dialogflow-icon" />
+            <span style={{fontSize: "14px", color: "#6c6a6a", marginLeft: "10px"}}>Bot Profile</span>
+          </ModalHeader>
+          <ModalBody>
+            <div className="row">
+              <div className="col-sm-12">
+                <p className="km-bot-integration-use-case-modal-text">Give a name to your bot from DialogFlow   </p>
+              </div>
+            </div>
+            <div className="row" style={{marginTop: "75px"}}>
+              <label className="col-sm-3" htmlFor="hf-password">Bot Name:</label>
+              <div className="col-sm-9">
+                <input type="text" onChange = {(event) => this.setState({botName:event.target.value})} value ={this.state.botName} name="hf-password" className="form-control input-field" placeholder="Example: Alex, Bot " />
+                <span className="help-block">The name you select here will be seen by your customers</span>
+              </div>
+            </div>
+            <div className="row" style={{marginTop: "66px"}}>
+              <div className="col-sm-12 text-right">
+                <button className="btn btn-primary">
+                  Integrate and Setup Bot Profile
+                </button>
+              </div>  
+            </div>
+          </ModalBody>
+        </Modal>
       </div>
     )
   }
