4f43b59ce414b1fba1baf170e5a66ec6df68310d
jithinbalu <jithin@kommunicate.io>
2018-02-06 00:38:47 +0530

    Bug Fix , enabling and disabling button

--- react/src/views/Autoreply/Welcome/Welcome.js
+++ react/src/views/Autoreply/Welcome/Welcome.js
@@ -107,7 +107,6 @@ class Welcome extends Component{
   handleCheckboxChange = () => {
 
     // make api call to disable all rows in in_app_msgs where createdBy = user.id 
-
     this.setState({enableDisableCheckbox: !this.state.enableDisableCheckbox}, () => {
       if(this.state.enableDisableCheckbox) {
         enableInAppMsgs({category: 1}).then(result => {
@@ -124,70 +123,81 @@ class Welcome extends Component{
       }
     }) 
   }
-
-render(){
-  return (
-<div className="animated fadeIn">
-  <div className="row">
-    <div className="col-sm-12 col-md-12">
-      <div className="card welcome-message-container" /* style={{'display':'none'}} */ >
-        <div className="card-block">
-          <div>
-            <div className="col-6">
-              <div  className="row">
-                <h4 className="enable-automatic-welcome">Enable automatic welcome message </h4>
-                <Label className="switch switch-3d switch-enable-automatic">
-                    <Input type="checkbox" className="switch-input" onChange={this.handleCheckboxChange} checked={this.state.enableDisableCheckbox}/>
-                    <span className="switch-label"></span>
-                    <span className="switch-handle"></span>
-                </Label>
-              </div>
-              <div className="row" >
-                <p className="welcome-message-will">(welcome message will be automatically shown to the user on opening the chat box)</p>
-              </div>
-             <div className="row"> {/* style={{'display':'none'}} remove style for display*/}
-                <p className="you-can-update-status">You can update status as <span className="you-can-update-status text-style-1">offline</span> or <span className="you-can-update-status text-style-1">online</span> manually from the header above. 
+  updateUserStatus =(status) =>{
+    if(status){
+      this.setState({enableDisableCheckbox: true})
+    }
+    else{
+      this.setState({enableDisableCheckbox: false})
+    }
+    
+  }
+  
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <div className="row">
+          <div className="col-sm-12 col-md-12">
+            <div className="card welcome-message-container" /* style={{'display':'none'}} */ >
+              <div className="card-block">
+                <div>
+                  <div className="col-6">
+                    <div className="row">
+                      <h4 className="enable-automatic-welcome">Enable automatic welcome message </h4>
+                      <Label className="switch switch-3d switch-enable-automatic">
+                        <Input type="checkbox" className="switch-input" onChange={this.handleCheckboxChange} checked={this.state.enableDisableCheckbox} />
+                        <span className="switch-label"></span>
+                        <span className="switch-handle"></span>
+                      </Label>
+                    </div>
+                    <div className="row" >
+                      <p className="welcome-message-will">(welcome message will be automatically shown to the user on opening the chat box)</p>
+                    </div>
+                    <div className="row"> {/* style={{'display':'none'}} remove style for display*/}
+                      <p className="you-can-update-status">You can update status as <span className="you-can-update-status text-style-1">offline</span> or <span className="you-can-update-status text-style-1">online</span> manually from the header above.
                 <span className="you-can-update-status text-style-2" onClick={this.toggleOverlay}>Show me where</span></p>
-                
+
+                    </div>
+                  </div>
+                </div>
+                <div>{/* style={{'display':'none'}} remove this div for display*/}
+                  <hr className="km-welcome-hr" />
+                  <WhenYouAreOnline showOnlinePrefs={this.state.showOnlinePrefs} toggleOnlinePrefs={this.toggleOnlinePrefs}
+                    updateUserStatus={this.updateUserStatus} />
+                  <hr className="km-welcome-hr" />
+                  <WhenYouAreOffline showOfflinePrefs={this.state.showOfflinePrefs} toggleOfflinePrefs={this.toggleOfflinePrefs}
+                    updateUserStatus={this.updateUserStatus} />
+                </div>
+              </div>
+              <div className={this.state.showOverlay ? "full-screen-overlay show-full-screen-overlay" : "full-screen-overlay hide-full-screen-overlay"} onClick={this.toggleOverlay}>
+                <p>You can update your status
+                  as offline or online from here</p>
+                {
+                  <div className="curved-arrow"></div>
+                }
+              </div>
+            </div>
+            <div className="card" style={{ 'display': 'none' }}>
+              <div className="card-header">
+                <div className="card-block">
+                  <div className="form-group row form-control-label">
+                    Message
+                  </div>
+                  <div className="form-group row">
+                    <textarea id="offhourmessage" rows="6" cols="60" onChange={(event) => this.setState({ msg: event.target.value })} value={this.state.msg} required></textarea>
+                  </div>
+                </div>
+              </div>
+              <div className="card-footer">
+                <button type="button" className="btn btn-primary px-4" onClick={this.submitWelcomeMessage}>Save</button>
+                <button type="reset" className="btn btn-danger px-4">Cancel</button>
               </div>
             </div>
           </div>
-          <div>{/* style={{'display':'none'}} remove this div for display*/}
-          <hr className="km-welcome-hr" />
-          <WhenYouAreOnline showOnlinePrefs={this.state.showOnlinePrefs} toggleOnlinePrefs={this.toggleOnlinePrefs} />
-          <hr className="km-welcome-hr" />
-          <WhenYouAreOffline showOfflinePrefs={this.state.showOfflinePrefs} toggleOfflinePrefs={this.toggleOfflinePrefs} />
-          </div>
-        </div>
-        <div className={this.state.showOverlay ? "full-screen-overlay show-full-screen-overlay": "full-screen-overlay hide-full-screen-overlay"} onClick={this.toggleOverlay}>
-          <p>You can update your status
-          as offline or online from here</p>
-          {
-            <div className="curved-arrow"></div>
-          }
         </div>
       </div>
-      <div className="card" style={{'display':'none'}}>
-        <div className="card-header">
-          <div className="card-block">
-            <div className="form-group row form-control-label">
-              Message
-            </div>
-            <div className="form-group row">
-              <textarea id ="offhourmessage" rows="6" cols="60" onChange = {(event) => this.setState({msg:event.target.value})} value={this.state.msg} required></textarea>
-            </div>
-          </div>
-        </div>
-        <div className="card-footer">
-          <button type="button" className="btn btn-primary px-4" onClick={this.submitWelcomeMessage}>Save</button>
-          <button type="reset" className="btn btn-danger px-4">Cancel</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-  )
-}
+    )
+  }
 }
 
 export default Welcome;
