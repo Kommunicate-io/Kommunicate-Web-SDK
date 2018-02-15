1850b19b9054987549bea6ae0218e9824b0140c6
Kusum Dhoundiyal <kusum.dhoundiyal96@gmail.com>
2018-02-15 10:43:04 +0530

    Adding logout function

--- js/app/kommunicate.js
+++ js/app/kommunicate.js
@@ -110,7 +110,16 @@ Kommunicate ={
         });
     },
     logout:function(event,options){
-        $kmApplozic.fn.applozic("logout");
+        window.logout();
+        if (typeof window.$kmApplozic !== "undefined" && typeof window.$kmApplozic.fn !== "undefined" &&typeof window.$kmApplozic.fn.applozic!=="undefined"&& window.$kmApplozic.fn.applozic("getLoggedInUser")) {
+          window.$kmApplozic.fn.applozic('logout');           
+        }
+        if (typeof window.$applozic !== "undefined" && typeof window.$applozic.fn !== "undefined" &&typeof window.$applozic.fn.applozic!=="undefined"&& window.$applozic.fn.applozic("getLoggedInUser")) {
+          window.$applozic.fn.applozic('logout');       
+        }
+        sessionStorage.clear();
+        localStorage.clear();
+        window.appHistory.replace('/login');
     },
     triggerEvent:function(event,options){
         $applozic.ajax({
