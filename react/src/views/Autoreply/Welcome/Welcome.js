92e4cd077f200eb77807144e6f4ca3671ee8108b
jithinbalu <jithin@kommunicate.io>
2018-02-15 18:17:17 +0530

    welcome message new changes

--- react/src/views/Autoreply/Welcome/Welcome.js
+++ react/src/views/Autoreply/Welcome/Welcome.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import validator from 'validator';
-
+import './Welcome.css';
+import SliderToggle from '../../../components/SliderToggle/SliderToggle';
 import Notification from '../../model/Notification';
-import { getAllSuggestions, getSuggestionsByAppId, createSuggestions, getWelcomeMessge, disableInAppMsgs, enableInAppMsgs,getInAppMessagesByEventId }  from '../../../utils/kommunicateClient'
+import { addInAppMsg, deleteInAppMsg, getAllSuggestions, getSuggestionsByAppId, createSuggestions, getWelcomeMessge, disableInAppMsgs, enableInAppMsgs,getInAppMessagesByEventId }  from '../../../utils/kommunicateClient'
 import axios from 'axios';
 import  {getConfig,getEnvironmentId,get} from '../../../config/config.js';
-import { Label, Input } from 'reactstrap';
+import { Label, Input, Row } from 'reactstrap';
 import WhenYouAreOnline from './WhenYouAreOnline'
 import WhenYouAreOffline from './WhenYouAreOffline'
 import CommonUtils from '../../../utils/CommonUtils';
 
 
 class Welcome extends Component{
+  // state = { 
+  //   welcomeMessages:[""]
+  // }
   constructor(props){
     super(props);
     this.state = {
@@ -20,24 +24,41 @@ class Welcome extends Component{
      enableDisableCheckbox: true,
      showOfflinePrefs: false,
      showOnlinePrefs: false,
+     welcomeMessages:[{messageField:''}],
+     enableAddMsgLink: false,
+     activeTextField: -1,
     };
     this.submitWelcomeMessage = this.submitWelcomeMessage.bind(this);
   }
 
   componentDidMount(){
-    // API Resposne for old welcome message design
-    
-    // let userSession = CommonUtils.getUserSession();
-    // getWelcomeMessge(userSession.application.applicationId).then(message=>{
-    //   this.setState({msg:message});
-    // }).catch(err=>{
-    //   console.log("error while fetching welcome message",err);
-    // })
-    
+       this.getWelcomeMessges();
   }
-  
+  getWelcomeMessges = () => {
+    let eventIds = [1, 2, 3, 4];
+    let userSession = CommonUtils.getUserSession();
+    let welcomeMessages = [];
+    return Promise.resolve(getInAppMessagesByEventId(eventIds)).then(response=>{
+      response.eventId3Messages.map(msg => {
+        msg.messages.map(item => {
+          // welcomeMessages.push(item.message)
+          welcomeMessages.push({
+            messageField: item.message,
+            messageId: item.id
+          })
+          
+          this.setState({welcomeMessages:welcomeMessages});
+        
+        })
+      })    
+      
+    }).catch(err=>{
+      console.log("error while fetching welcome message",err);
+    })
+
+  }
   submitWelcomeMessage = () => {
-    if(this.state.msg !== ""){
+    if(this.state.welcomeMessages[0] !== ""){
       var _this =this;
       let userSession = CommonUtils.getUserSession();
        var applicationId = userSession.application.applicationId;
@@ -49,7 +70,7 @@ class Welcome extends Component{
         url:setWelcomeMessageUrl,
         data:{
               "applicationId" : applicationId,
-              "message" : this.state.msg
+              "message" : this.state.welcomeMessages[0]
             }
          }).then(function(response){
            console.log("message successfully send");
@@ -132,72 +153,138 @@ class Welcome extends Component{
     }
     
   }
+  welcomeMessagesMethod =() => {
+    let index = this.state.activeTextField;
+  } 
+  deleteWelcomeMessage = () => {
+    let index = this.state.activeTextField;
+    let welcomeMessages = Object.assign([], this.state.welcomeMessages);
+    let id = welcomeMessages[index].messageId;
+    if (id !== '') {
+      deleteInAppMsg(id).then(response => {
+        if (response) {
+          Notification.success('Successfully deleted');
+          this.getWelcomeMessges()
+        } else {
+          Notification.warning('There was a problem while deleting');
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+    } else {
+      welcomeMessages.splice(index, 1)
+      this.setState({ welcomeMessages: welcomeMessages })
+      console.log("deleted array", this.state.welcomeMessages)
+    }
+  }
+  createWelcomeMessage = () => {
+    let index = this.state.activeTextField;
+    let data = {
+      eventId: 3,
+      message: this.state.welcomeMessages[index],
+      status: 1,
+      category: 1,
+      sequence: index+1
+    }
+    addInAppMsg(data)
+      .then(response => {
+        if (response !== undefined && response.status === 200) {
+          if (response.data.code === 'SUCCESS' && response.data.message.toLowerCase() === 'created') {
+            Notification.success('In app message saved successfully');
+          } else if (response.data.code === 'SUCCESS' && response.data.message.toLowerCase() === 'limit reached') {
+            Notification.warning('Not created, limit of 3 in app messages reached');
+          } else {
+            Notification.error('In app message not saved.');
+          }
+        }
+      })
+
+  }
+  
+  appendMessageTextArea = (index) => {
+   // if(this.state.welcomeMessages[0].messageField =)
+    if(this.state.welcomeMessages[0].messageField !== '' && this.state.welcomeMessages.length < 3 ){
+      let message = Object.assign([], this.state.welcomeMessages);
+      //let messageField = '';
+      let fields = {
+        messageField: '',
+        messageId: ''
+      }
+      message.push(fields);
+      this.setState({ welcomeMessages: message })
+    }
+  }
   
   render() {
+    const textArea = this.state.welcomeMessages.map((message, index) => {
+      return <div key = {index}>
+        <div className = "row text-area-wrapper">
+          <div className="trash-wrapper">
+          <textarea rows="5" cols="60" placeholder="Hi, please leave your email ID and I will get back to you as soon as possible."
+           value={this.state.welcomeMessages[index].messageField}
+            onChange={(e) => {
+              let welcomeMessages = Object.assign([],this.state.welcomeMessages);
+              welcomeMessages[index].messageField = e.target.value;
+              this.setState({
+                welcomeMessages:welcomeMessages,
+              })
+            }}
+            onFocus ={(e) =>{ 
+              this.setState({activeTextField: index}) 
+            } }>
+          </textarea>
+          <div className={index !== 0 ? "trash-btn" : "n-vis"} ><i className="fa fa-trash-o"
+              onClick={(e) => {
+                this.setState({ activeTextField: index },this.deleteWelcomeMessage )
+              }}></i></div>
+          </div>
+        </div>
+      </div>  
+    });
     return (
-      <div className="animated fadeIn">
+      <div className="welcome-message-wrapper">
+        <div className="row">
+          <div className="col-md-8 col-sm-12">
+            <div className="card-block welcome-message-header">
+              <div className="row">
+                <h4 className="welcome-message-title">Show welcome message to customers </h4>
+                <SliderToggle checked={this.state.enableDisableCheckbox} handleOnChange={this.handleCheckboxChange} />
+              </div>
+              <div className="row" >
+                <p className="welcome-message-description">The welcome message will greet your customers when they initiate a
+                new conversation. Welcome messages are common for all team members across your company.</p>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="row">
           <div className="col-sm-12 col-md-12">
-            <div className="card welcome-message-container" /* style={{'display':'none'}} */ >
-              <div className="card-block">
-                <div>
-                  <div className="col-6">
-                    <div className="row">
-                      <h4 className="enable-automatic-welcome">Enable automatic welcome message </h4>
-                      <Label className="switch switch-3d switch-enable-automatic">
-                        <Input type="checkbox" className="switch-input" onChange={this.handleCheckboxChange} checked={this.state.enableDisableCheckbox} />
-                        <span className="switch-label"></span>
-                        <span className="switch-handle"></span>
-                      </Label>
-                    </div>
-                    <div className="row" >
-                      <p className="welcome-message-will">(welcome message will be automatically shown to the user on opening the chat box)</p>
-                    </div>
-                    <div className="row"> {/* style={{'display':'none'}} remove style for display*/}
-                      <p className="you-can-update-status">You can update status as <span className="you-can-update-status text-style-1">offline</span> or <span className="you-can-update-status text-style-1">online</span> manually from the header above.
-                <span className="you-can-update-status text-style-2" onClick={this.toggleOverlay}>Show me where</span></p>
-
-              </div>
-            </div>
-          </div>
-
-          <div>{/* style={{'display':'none'}} remove this div for display*/}
-
-          <hr className="km-welcome-hr" />
-          <WhenYouAreOnline showOnlinePrefs={this.state.showOnlinePrefs} toggleOnlinePrefs={this.toggleOnlinePrefs} updateUserStatus ={this.updateUserStatus} />
-          <hr className="km-welcome-hr" />
-          <WhenYouAreOffline showOfflinePrefs={this.state.showOfflinePrefs} toggleOfflinePrefs={this.toggleOfflinePrefs} updateUserStatus ={this.updateUserStatus} />
-          </div>
-        </div>
-        <div className={this.state.showOverlay ? "full-screen-overlay show-full-screen-overlay": "full-screen-overlay hide-full-screen-overlay"} onClick={this.toggleOverlay}>
-          <p>You can update your status
-          as offline or online from here</p>
-          {
-            <div className="curved-arrow"></div>
-          }
-        </div>
-      </div>
-      
-            <div className="card" style={{ 'display': 'none' }}>
+            <div className="card">
               <div className="card-header">
-                <div className="card-block">
-                  <div className="form-group row form-control-label">
-                    Message
+                <div className="message-wrapper">
+                  <div className="row">
+                    <h5 className="message-title">Welcome message </h5>
                   </div>
-                  <div className="form-group row">
-                    <textarea id="offhourmessage" rows="6" cols="60" onChange={(event) => this.setState({ msg: event.target.value })} value={this.state.msg} required></textarea>
+                  <div className="row">
+                    {/* <textarea id="offhourmessage" placeholder="Hi, please leave your email ID and I will get back to you as soon as possible." 
+                    rows="5" cols="60" onChange={(event) => this.setState({ msg: event.target.value })} value={this.state.msg} required></textarea> */}
+                    {textArea}                  
                   </div>
                 </div>
+                {  this.state.welcomeMessages.length <= 2 &&
+                  <button className ="add-new-msg-link" disabled = { this.state.enableAddMsgLink } onClick={this.appendMessageTextArea} >+ Add a follow up welcome message</button>
+                }                
+                <div className="btn-group">
+                  <button className="km-button km-button--primary save-changes-btn" onClick={this.createWelcomeMessage} >Save changes</button>
+                  <button className="km-button km-button--secondary discard-btn">Discard</button>
+                </div>
               </div>
-              <div className="card-footer">
-                <button type="button" className="btn btn-primary px-4" onClick={this.submitWelcomeMessage}>Save</button>
-                <button type="reset" className="btn btn-danger px-4">Cancel</button>
-              </div>
+              
             </div>
           </div>
         </div>
       </div>
-      
+
     )
   }
 }
