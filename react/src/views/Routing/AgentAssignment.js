042ec201976fdac0d5186e982d2f9d98f3d6630d
jithin <jithin@kommunicate.io>
2018-03-13 15:40:15 +0530

    KM-765 Bug fix- Agent routing Dashboard

--- react/src/views/Routing/AgentAssignment.js
+++ react/src/views/Routing/AgentAssignment.js
@@ -11,50 +11,78 @@ class AgentAssignemnt extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            checked: 1
+            checked: 1,
+            checkedNotifyEverybody:1,
+            checkedAutomaticAssignemnt:0,
+            preventMultiCallAutoAssignment:false,
+            preventMultiCallNotifyEverybody:false
         };
 
     }
 componentDidMount (){
     let userSession = CommonUtils.getUserSession();
     if (userSession.routingState === 1) {
-        this.setState({ checked: 0 })
-    }
-    else {
-        this.setState({ checked: 1 })
-    }
-}
- 
-handleRadioButton = () => {
-    this.setState({
-            checked: !this.state.checked
-    })
-    if (this.state.checked === 1) {
-        enableAutomaticAssignment({ routingState: ROUTING_STATE.ENABLE }).then(response => {
-            if (response.status === 200 && response.data.code === "SUCCESS") {
-                let userSession = CommonUtils.getUserSession();
-                userSession.routingState = ROUTING_STATE.ENABLE;
-                CommonUtils.setUserSession(userSession);
-                Notification.success('Automatic assignment is enabled');
-            }
+        this.setState({
+            checkedNotifyEverybody: 0,
+            checkedAutomaticAssignemnt: 1,
+            preventMultiCallAutoAssignment: true,
+            preventMultiCallNotifyEverybody: false
         })
     }
     else {
+        this.setState({
+            checkedNotifyEverybody: 1,
+            checkedAutomaticAssignemnt: 0,
+            preventMultiCallNotifyEverybody: true,
+            preventMultiCallAutoAssignment: false
+        })
+    }
+}
+handleRadioBtnNotifyEverybody = () => {
+    this.setState({
+        checkedNotifyEverybody: 1,
+        checkedAutomaticAssignemnt: 0
+    })
+    if (this.state.preventMultiCallNotifyEverybody == false) {
         enableNotifyEveryBody({ routingState: ROUTING_STATE.DISABLE }).then(response => {
             if (response.status === 200 && response.data.code === "SUCCESS") {
                 let userSession = CommonUtils.getUserSession();
                 userSession.routingState = ROUTING_STATE.DISABLE;
                 CommonUtils.setUserSession(userSession)
                 Notification.success('Notify everybody is enabled');
+                this.setState({
+                    preventMultiCallAutoAssignment: false,
+                    preventMultiCallNotifyEverybody: true
+                })
             }
         })
     }
-
 }
+handleRadioBtnAutomaticAssignment = () => {
+    this.setState({
+        checkedNotifyEverybody: 0,
+        checkedAutomaticAssignemnt: 1,
+    })
+    if (this.state.preventMultiCallAutoAssignment == false) {
+        enableAutomaticAssignment({ routingState: ROUTING_STATE.ENABLE }).then(response => {
+            if (response.status === 200 && response.data.code === "SUCCESS") {
+                let userSession = CommonUtils.getUserSession();
+                userSession.routingState = ROUTING_STATE.ENABLE;
+                CommonUtils.setUserSession(userSession);
+                Notification.success('Automatic assignment is enabled');
+                this.setState({
+                    preventMultiCallAutoAssignment: true,
+                    preventMultiCallNotifyEverybody: false
+                })
+            }
+        })
+    }
+}
+
 
   render() {
       const notifyEverybodyContainer = (
-          <div className={this.state.checked ? "row notify-everybody-wrapper active-agent-routing" : "row notify-everybody-wrapper non-active-agent-routing"}>
+          <div className={this.state.checkedNotifyEverybody ? "row notify-everybody-wrapper active-agent-routing" : "row notify-everybody-wrapper non-active-agent-routing"}>
               <div className="col-radio-btn col-md-1 col-lg-1">
               </div>
               <div className="col-md-11 col-lg-11">
@@ -64,7 +92,7 @@ handleRadioButton = () => {
           </div>
       )
       const automaticAssignmentContainer = (
-          <div className={!this.state.checked ? "row notify-everybody-wrapper active-agent-routing" : "row notify-everybody-wrapper non-active-agent-routing"}>
+          <div className={!this.state.checkedNotifyEverybody ? "row notify-everybody-wrapper active-agent-routing" : "row notify-everybody-wrapper non-active-agent-routing"}>
               <div className="col-radio-btn col-md-1 col-lg-1">
               </div>
               <div className="col-md-11 col-lg-11">
@@ -82,10 +110,10 @@ handleRadioButton = () => {
                     <div className="options-wrapper">
                         <h4 className="options-wrapper-title">Select from one of the options below </h4>
                         <form>
-                            <RadioButton idRadioButton={'notify-everybody-radio'} handleOnChange={this.handleRadioButton}
-                                checked={this.state.checked} label={notifyEverybodyContainer} />
-                            <RadioButton idRadioButton={'automatic-assignemnt-radio'} handleOnChange={this.handleRadioButton}
-                                checked={!this.state.checked} label={automaticAssignmentContainer} />
+                            <RadioButton idRadioButton={'notify-everybody-radio'} handleOnChange={this.handleRadioBtnNotifyEverybody}
+                                checked={this.state.checkedNotifyEverybody} label={notifyEverybodyContainer} />
+                            <RadioButton idRadioButton={'automatic-assignemnt-radio'} handleOnChange={this.handleRadioBtnAutomaticAssignment}
+                                checked={this.state.checkedAutomaticAssignemnt} label={automaticAssignmentContainer} />
                         </form>
                     </div>
                 </div>
