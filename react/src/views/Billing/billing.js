ab24640cbe8acfbe16fe7cfbcebac126d4a971e2
devashish <devashish.mamgain@gmail.com>
2018-03-17 18:15:00 +0530

    KM-729 add chargebee

--- react/src/views/Billing/billing.js
+++ react/src/views/Billing/billing.js
@@ -19,7 +19,7 @@ class Billing extends Component {
                             <div className="card-block">
                                 <div className="download-link-image-container">
                                     <div className="download-link-container">
-                                      <a className="chargebee" href="javascript:void(0)" data-cb-type="checkout" data-cb-plan-id="launch">Subscribe</a>
+                                      <a className="chargebee" href="javascript:void(0)" data-cb-type="checkout" data-cb-plan-id="cbdemo_hustle">Subscribe</a>
                                       <a className="chargebee" href="javascript:void(0)" data-cb-type="portal">Manage account</a>
                                   </div>
                                 </div>
