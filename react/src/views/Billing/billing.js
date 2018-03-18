f68b9ca555e0b49c4e3428b53be764322c62e5d4
devashish <devashish.mamgain@gmail.com>
2018-03-18 17:57:27 +0530

    KM-729 add chargebee

--- react/src/views/Billing/billing.js
+++ react/src/views/Billing/billing.js
@@ -6,8 +6,32 @@ class Billing extends Component {
       let subscribeElems = document.getElementsByClassName("chargebee");
       
       for (var i=0, max=subscribeElems.length; i < max; i++) {
-        subscribeElems[i].click();
+        //subscribeElems[i].click();
       }
+      
+      document.getElementById("checkout").addEventListener("click", function(){
+        var cbInstance = window.Chargebee.getInstance();
+        console.log(cbInstance);
+  
+          cbInstance.setCheckoutCallbacks(function(cart) {
+              // you can define a custom callbacks based on cart object
+              return {
+                  loaded: function() {
+                      console.log("checkout opened");
+                  },
+                  close: function() {
+                      console.log("checkout closed");
+                  },
+                  success: function(hostedPageId) {
+          
+                  },
+                  step: function(value) {
+                      // value -> which step in checkout
+                      console.log(value);
+                  }
+              }
+          });  
+      });
     }
 
     render() {
@@ -20,8 +44,10 @@ class Billing extends Component {
                                 <div className="download-link-image-container">
                                     <div className="download-link-container">
                                       Your plan details
-                                      <a className="chargebee" href="javascript:void(0)" data-cb-type="checkout" data-cb-plan-id="cbdemo_hustle">Subscribe</a>
+                                      <a id="checkout" className="chargebee" href="javascript:void(0)" data-cb-type="checkout" data-cb-plan-id="cbdemo_hustle">Subscribe</a>
+
                                       <a className="chargebee" href="javascript:void(0)" data-cb-type="portal">Manage account</a>
+
                                   </div>
                                 </div>
                             </div>
