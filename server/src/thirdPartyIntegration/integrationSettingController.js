a2af6db92ee4a69073dbd620eb360920ddefab3a
rudranand <k.anand2189@gmail.com>
2018-03-23 13:19:30 +0530

    KM-864 get config API added

--- server/src/thirdPartyIntegration/integrationSettingController.js
+++ server/src/thirdPartyIntegration/integrationSettingController.js
@@ -8,35 +8,36 @@ exports.updateOrCreate = (req, res) => {
     let type = req.params.type;
     return Promise.resolve(registrationService.getCustomerByApplicationId(appId)).then(customer => {
         if (!customer) {
-            res.status(200).json({ code: "SUCCESS", message: "no user found" })
+          return  res.status(200).json({ code: "SUCCESS", message: "no user found" })
         }
         settings.customerId = customer.id;
         settings.type = type;
         return integrationSettingService.updateOrCreate(customer.id, type, settings).then(response => {
             console.log('response of creating settings', response);
-            res.status(200).json({ code: "SUCCESS", message: response })
+           return res.status(200).json({ code: "SUCCESS", message: response })
         });
 
     }).catch(err => {
         console.log('error while inserting thirdparty setting', err)
-        res.status(500).json({ code: "ERROR", message: "creation error" });
+        return res.status(500).json({ code: "ERROR", message: "creation error" });
     })
 
 }
 
 exports.getZendeskIntegrationSetting = (req, res) => {
     let appId = req.params.appId;
+    let type = req.params.type;
     return Promise.resolve(registrationService.getCustomerByApplicationId(appId)).then(customer => {
         if (!customer) {
-            res.status(200).json({ code: "SUCCESS", message: "no user found" })
+          return res.status(200).json({ code: "SUCCESS", message: "no user found" })
         }
-        integrationSettingService.getZendeskIntegrationSetting(customer.id, ).then(respons => {
+        integrationSettingService.getZendeskIntegrationSetting(customer.id, type).then(respons => {
             console.log('response', response);
-            res.status(200).json({ code: "SUCCESS", message: response })
+          return res.status(200).json({ code: "SUCCESS", message: response })
         })
     }).catch(err => {
         console.log('error thirdparty setting', err)
-        res.status(500).json({ code: "ERROR", message: "error" });
+        return res.status(500).json({ code: "ERROR", message: "error" });
     })
 
 }
