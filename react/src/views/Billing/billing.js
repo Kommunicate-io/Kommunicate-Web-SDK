cd3a77863350eaff1576d475444af4a865432b72
devashish <devashish.mamgain@gmail.com>
2018-03-25 14:34:23 +0530

    KM-729 show trial status

--- react/src/views/Billing/billing.js
+++ react/src/views/Billing/billing.js
@@ -28,7 +28,8 @@ class Billing extends Component {
             showFeatures:'Show Features',
             subscription: CommonUtils.getUserSession().subscription,
             billingCustomerId: CommonUtils.getUserSession().billingCustomerId,
-            currentPlan: SUBSCRIPTION_PLANS[0]
+            currentPlan: SUBSCRIPTION_PLANS[0],
+            trialLeft: 0
         };
         this.showHideFeatures = this.showHideFeatures.bind(this);
         this.subscriptionPlanStatus = this.subscriptionPlanStatus.bind(this);
@@ -37,7 +38,19 @@ class Billing extends Component {
     componentDidMount() {
       let that = this;
       console.log("#subscription: ");
+      console.log(new Date(CommonUtils.getUserSession().application.createdAtTime));
       console.log(CommonUtils.getUserSession().subscription);
+
+      var now = new Date();
+      var trialStarted = new Date(CommonUtils.getUserSession().application.createdAtTime);
+      var timeDiff = now.getTime() - trialStarted.getTime();
+      var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      if (diffDays < 31) {
+        this.setState({trialLeft: (31 - diffDays)});
+      } else {
+        //Todo: show trial ended
+      }
+
 
       this.setState({currentPlan: this.subscriptionPlanStatus()});
 
@@ -216,13 +229,17 @@ class Billing extends Component {
                     <div className="col-md-10">
                         <div className="card">
                             <div className="card-block">
-                                <div className="info-bar-container">
-                                    <p className="info-bar-text"><strong>21 days trial left.</strong> If no plan is chosen, you will be subscribed to the Startup Plan (FREE) at the end of the trial period.</p>
-                                </div>
-                                {/* Error Banner Design */}
-                                <div className="info-bar-container info-bar--error n-vis">
-                                    <p className="info-bar-text"><strong>21 days trial left.</strong> If no plan is chosen, you will be subscribed to the Startup Plan (FREE) at the end of the trial period.</p>
-                                </div>
+                                { this.state.trialLeft > 0 && this.trialLeft <= 30 && this.currentPlan == 0 ?  
+                                    (<div className="info-bar-container">
+                                        <p className="info-bar-text"><strong>{this.state.trialLeft} days trial left.</strong> If no plan is chosen, you will be subscribed to the Startup Plan (FREE) at the end of the trial period.</p>
+                                    </div>
+                                    ) 
+                                    : (
+                                        <div className="info-bar-container info-bar--error">
+                                        <p className="info-bar-text"><strong>Trial period over.</strong> You have been subscribed to the Startup Plan.</p>
+                                        </div>
+                                    ) 
+                                }
 
                                 <div className="current-plan-container flexi">
                                     <div className="col-md-6">
