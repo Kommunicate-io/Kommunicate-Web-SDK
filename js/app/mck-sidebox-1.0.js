b15accca958f760b3956d3c97cc8e3855e01f751
Kusum Dhoundiyal <kusum.dhoundiyal96@gmail.com>
2018-03-26 14:05:20 +0530

    KM-793 New lines in welcome message is not coming in chat widget

--- js/app/mck-sidebox-1.0.js
+++ js/app/mck-sidebox-1.0.js
@@ -4620,12 +4620,16 @@ var MCK_CLIENT_GROUP_MAP = [];
                 if (emoji_template.indexOf('emoji-inner') === -1 && msg.contentType === 0) {
                     var nodes = emoji_template.split("<br/>");
                     for (var i = 0; i < nodes.length; i++) {
-                        var x = d.createElement('div');
-                        x.appendChild(d.createTextNode(nodes[i]));
-                        if (nodes[i] && nodes[i].match(LINK_MATCHER)) {
-                            x = $applozic(x).linkify({
-                                target: '_blank'
-                            });
+                        if (nodes[i] === "") {
+                            var x = d.createElement('BR');
+                        } else {
+                            var x = d.createElement('div');
+                            x.appendChild(d.createTextNode(nodes[i]));
+                            if (nodes[i] && nodes[i].match(LINK_MATCHER)) {
+                                x = $applozic(x).linkify({
+                                    target: '_blank'
+                                });
+                            }
                         }
                         $textMessage.append(x);
                     }
