aadf73c337254dff9a88223fea247dbdf1665df9
rudranand <k.anand2189@gmail.com>
2018-03-27 18:37:44 +0530

    KM-895  price deffernece for multiple days

--- server/src/webplugin/js/app/km-richtext-markup-1.0.js
+++ server/src/webplugin/js/app/km-richtext-markup-1.0.js
@@ -113,11 +113,11 @@ getRoomDetailTemplate: function (options, sessionId) {
                             </div>
                             <div class="km-blocked-room-price">
                                 <p>Price:<br><span>(Per Room Per Night)</span></p>
-                                <span>`+(options.Price.CurrencyCode == "INR" ? '&#x20B9;' : options.Price.CurrencyCode ) + " " + options.Price.RoomPrice + `</span>
+                                <span>`+(options.Price.CurrencyCode == "INR" ? '&#x20B9;' : options.Price.CurrencyCode ) + " " + Math.ceil(options.DayRates.Amount) + `</span>
                             </div>
                             <div class="km-blocked-room-sub-total">
                                 <p>Total:<br><span>(1 Room for `+ options.NoOfNights +` Nights)</span></p>
-                                <span> `+ (options.Price.CurrencyCode=="INR"?'&#x20B9;': options.Price.CurrencyCode ) + " " +options.NoOfNights * options.Price.RoomPrice + ` </span>
+                                <span> `+ (options.Price.CurrencyCode=="INR"?'&#x20B9;': options.Price.CurrencyCode ) + " " +Math.ceil(options.Price.RoomPrice) + ` </span>
                             </div>
                         </div>
                         <div class="km-blocked-room-button-container">
