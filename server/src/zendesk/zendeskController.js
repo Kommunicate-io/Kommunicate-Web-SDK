f67a689b436f589a4ba2372515f1e3609d9a5feb
rudranand <k.anand2189@gmail.com>
2018-03-28 18:34:55 +0530

    update zendesk APIs

--- server/src/zendesk/zendeskController.js
+++ server/src/zendesk/zendeskController.js
@@ -8,23 +8,23 @@ const ZENDESK = require('../application/utils').INTEGRATION_PLATFORMS.ZENDESK;
 exports.createZendeskTicket = (req, res) => {
     let ticket = req.body;
     let appId = req.params.appId;
-    registrationService.getCustomerByApplicationId(appId).then(customer => {
+    return registrationService.getCustomerByApplicationId(appId).then(customer => {
         if (!customer) {
-            res.status(200).json({ code: "SUCCESS", message: 'no customer found for this applicationId' });
+            return res.status(200).json({ code: "SUCCESS", message: 'no customer found for this applicationId' });
         }
-        integrationSettingService.getZendeskIntegrationSetting(customer.id, ZENDESK).then(settings => {
+        return integrationSettingService.getIntegrationSetting(customer.id, ZENDESK).then(settings => {
             if (!settings) {
-                res.status(200).json({ code: "SUCCESS", message: 'no configuration found for zendesk' });
+                return res.status(200).json({ code: "SUCCESS", message: 'no configuration found for zendesk' });
             }
-            zendeskService.createZendeskTicket(ticket, settings).then(response => {
+            return zendeskService.createZendeskTicket(ticket, settings).then(response => {
                 console.log("response from zendesk", response);
-                res.status(200).json({ code: "SUCCESS", data: response.data });
+                return res.status(200).json({ code: "SUCCESS", data: response.data });
             });
         });
 
     }).catch(err => {
         console.log('error while creating ticket', err);
-        res.status(500).json({ code: "ERROR", message: "ticket creation error" });
+        return res.status(500).json({ code: "ERROR", message: "ticket creation error" });
     });
 }
 
@@ -32,26 +32,26 @@ exports.updateZendeskTicket = (req, res) => {
     let id = req.params.id;
     let ticket = req.body;
     let appId = req.params.appId;
-    registrationService.getCustomerByApplicationId(appId).then(customer => {
+    return registrationService.getCustomerByApplicationId(appId).then(customer => {
         if (!customer) {
-            res.status(200).json({ code: "SUCCESS", message: 'no customer found for this applicationId' });
+            return res.status(200).json({ code: "SUCCESS", message: 'no customer found for this applicationId' });
         }
-        integrationSettingService.getZendeskIntegrationSetting(customer.id, ZENDESK).then(settings => {
+        return integrationSettingService.getIntegrationSetting(customer.id, ZENDESK).then(settings => {
             if (!settings) {
-                res.status(200).json({ code: "SUCCESS", message: 'no configuration found for zendesk' });
+                return res.status(200).json({ code: "SUCCESS", message: 'no configuration found for zendesk' });
             }
-            zendeskService.updateTicket(id, ticket, settings).then(response => {
+            return zendeskService.updateTicket(id, ticket, settings).then(response => {
                 console.log("response from zendesk", response);
                 if (response.statusText && response.statusText == "OK") {
-                    res.status(200).json({ code: "SUCCESS", data: response.data });
+                    return res.status(200).json({ code: "SUCCESS", data: response.data });
                 } else {
-                    res.status(response.status).json({ code: "ERROR", message: response })
+                    return res.status(response.status).json({ code: "ERROR", message: response })
                 }
             });
         });
     }).catch(err => {
         console.log('error while updating ticket', err);
-        res.status(err.response.status).json({ code: "ERROR", message: err.message });
+        return res.status(err.response.status).json({ code: "ERROR", message: err.message });
     })
 
 }
