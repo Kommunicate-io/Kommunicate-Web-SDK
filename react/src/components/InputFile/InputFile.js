1236c75954b76fc6cc8f27b4fd533b4053fc6c56
Vibhor Sharma <vibhor.sharma94@gmail.com>
2018-03-31 00:43:51 +0530

    Updated input type file component as per the mocks

--- react/src/components/InputFile/InputFile.js
+++ react/src/components/InputFile/InputFile.js
@@ -3,7 +3,7 @@ import './InputFile.css';
 
 export default class InputFile extends Component {
 
-    constructor(props: any)
+    constructor(props)
     {
         super(props);
         this.state = {message:'some initial message', text: props.text, id: props.id, multiple: props.multiple, accept: props.accept, className: props.className, onBlur: props.onBlur, hideCloseBtn: true};
@@ -33,7 +33,7 @@ export default class InputFile extends Component {
 
      removeSelectedFiles = ( id, text) => {
         let emptyInputFile = document.getElementById(`${id}`).value = null;
-        console.log(emptyInputFile);
+        // console.log(emptyInputFile);
         this.setState({text: this.props.text, hideCloseBtn: true});
      }  
 
@@ -47,16 +47,15 @@ export default class InputFile extends Component {
             <div>
                 <input id={id} type="file" className="km-btn-file" data-multiple-caption={this.state.message} multiple={multiple} onChange={this.getUploadedFileName} accept={accept} onBlur={onBlur}></input>
                 <label htmlFor={id} className={(className === 'primary') ? "km-button km-button--primary km-btn-file-label" : (className === 'secondary') ? "km-button km-button--secondary km-btn-file-label" : "km-button km-button--primary km-btn-file-label"}>
-                    <span>{text}</span>
-                    {/* <span hidden={}>{text}</span> */}
+                    <span className={(text !== this.props.text) ? "pad-right-40" : " "}>{text}</span>
                 </label>
-                <span hidden={this.state.hideCloseBtn} onClick={() => this.removeSelectedFiles( id, text)}>
-                        <svg xmlns="http://www.w3.org/2000/svg"  fill={(className === 'primary') ? "#5C5AA7" : (className === 'secondary') ? "#5C5AA7" : "#5C5AA7"} height="24" viewBox="0 0 24 24" width="24" style={{margin: "0px 0px -7px 10px", zIndex:20, width:"20px", height:"20px"}}>
-                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z">
-                            </path>
-                            <path d="M0 0h24v24H0z" fill="none">
-                            </path>
-                        </svg>
+                <span hidden={this.state.hideCloseBtn} onClick={() => this.removeSelectedFiles( id, text)} className="close-btn-container">
+                    <svg xmlns="http://www.w3.org/2000/svg"  fill={(className === 'primary') ? "#FFF" : (className === 'secondary') ? "#5C5AA7" : "#5C5AA7"} height="24" viewBox="0 0 24 24" width="24" style={{margin: "8px 0px -5px 10px", zIndex:20, width:"20px", height:"20px"}}>
+                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z">
+                        </path>
+                        <path d="M0 0h24v24H0z" fill="none">
+                        </path>
+                    </svg>
                     </span>
             </div>
         );
