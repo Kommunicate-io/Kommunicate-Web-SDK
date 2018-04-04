e157f714dfcafb4bec1d233113157a1773b4cc76
Aditya Mehra <mehraaditya71@gmail.com>
2018-04-03 21:56:32 -0600

    KM-103: Modified OAuth endpoint

--- server/src/googleAuth/googleAuthController.js
+++ server/src/googleAuth/googleAuthController.js
@@ -1,5 +1,8 @@
 const axios =require("axios");
 var querystring = require('querystring');
+const registrationService = require("../register/registrationService");
+const userService = require('../users/userService');
+const logger = require('../utils/logger');
 
 const CLIENT_ID = '155543752810-134ol27bfs1k48tkhampktj80hitjh10.apps.googleusercontent.com'
 const CLIENT_SECRET = '67BmE4D4qPn9PfglGn27pAmX'
@@ -16,20 +19,34 @@ const getToken = (authCode) => {
 		grant_type: 'authorization_code'
 	};
 	return axios.post('https://www.googleapis.com/oauth2/v4/token', querystring.stringify(params)).then((response)=> {
-		console.log(response.data)
-  		let access_token = response.data.access_token
-  		let authorization = 'Bearer ' + access_token
-		const headers = {
-  			Authorization : authorization
-  		}  		
-  		return axios.get(G_PLUS_PROFILE_URL, { headers: headers })
+			logger.info("refresh_token");
+			logger.info(response.data);
+	  		let access_token = response.data.access_token
+	  		let authorization = 'Bearer ' + access_token
+			const headers = {
+	  			Authorization : authorization
+	  		}  		
+	  		return axios.get(G_PLUS_PROFILE_URL, { headers: headers })
   	});
 }
 
 exports.authCode = (req, res) => {
 	let authCode = req.query.code;
 	getToken(authCode).then(response => {
-		console.log(response.data)
-		res.redirect(REDIRECT_URL + '?googleSignIn=true&email=' + response.data.emails[0].value + '&name=' + response.data.displayName )
+		logger.info(response.data)
+		
+		let userName = response.data.emails[0].value;
+
+		Promise.all([registrationService.getCustomerByUserName(userName),userService.getUserByName(userName)]).then(([customer,user])=>{
+      		logger.info("got the user from db",user);
+      		if(customer || user){
+        		res.redirect(REDIRECT_URL + '?googleSignUp=false&message="USER_ALREADY_EXISTS"')
+      		}else{
+      			res.redirect(REDIRECT_URL + '?googleSignUp=true&email=' + userName + '&name=' + response.data.displayName )	
+      		}
+      	}).catch(err => {
+
+      	})
+
 	})
 }
