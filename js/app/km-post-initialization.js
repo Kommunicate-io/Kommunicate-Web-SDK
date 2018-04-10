84a5bd2e5796b52a376fce599468696915c36fdb
Kusum Dhoundiyal <kusum.dhoundiyal96@gmail.com>
2018-04-11 02:36:00 +0530

    # KM-915 Chat widget UI to show the article list

--- js/app/km-post-initialization.js
+++ js/app/km-post-initialization.js
@@ -80,6 +80,7 @@ Kommunicate.faqEvents= function (data,helpdocsKey){
         var source = $(this).attr('data-source');
         KommunicateKB.getArticle({
             data: { appId: data.appId, articleId: articleId, source: source, helpdocsAccessKey: helpdocsKey }, success: function (response) {
+                if ($applozic("#km-faqanswer .km-faqanswer-list").length == 0) { 
                 $applozic("#km-faqanswer").append('<div class="km-faqanswer-list"><div class="km-faqquestion">' + response.data.title + '</div> <div class="km-faqanchor km-faqanswer">' + response.data.body + '</div></div>');
                 $applozic('.mck-faq-inner').removeClass("vis").addClass("n-vis");
                 $applozic('.km-faqanswer').removeClass("n-vis").addClass("vis");
@@ -87,7 +88,7 @@ Kommunicate.faqEvents= function (data,helpdocsKey){
                 $applozic('#mck-no-conversations').removeClass("vis").addClass("n-vis");
                 $applozic('.mck-sidebox-ft').removeClass("vis").addClass("n-vis");
 
-            }
+            }}
             , error: function () { }
         });
     });
