5995c671c9a24a42d6aebd3d195d7f12145f3ade
Aditya Mehra <mehraaditya71@gmail.com>
2018-04-19 21:51:01 -0600

    KM-862: Updated routing UI

--- react/src/views/Routing/AgentAssignment.js
+++ react/src/views/Routing/AgentAssignment.js
@@ -6,6 +6,11 @@ import { enableNotifyEveryBody, enableAutomaticAssignment, getCustomerByApplicat
 import axios from 'axios';
 import { ROUND_ROUBIN } from './Constants.js';
 import CommonUtils from '../../utils/CommonUtils';
+import SliderToggle from '../../components/SliderToggle/SliderToggle';
+import {Link} from 'react-router-dom';
+import {SplitButton, MenuItem, DropdownButton} from 'react-bootstrap';
+import { Collapse } from 'reactstrap';
+
 
 class AgentAssignemnt extends Component{
     constructor(props) {
@@ -15,7 +20,10 @@ class AgentAssignemnt extends Component{
             checkedNotifyEverybody:1,
             checkedAutomaticAssignemnt:0,
             preventMultiCallAutoAssignment:false,
-            preventMultiCallNotifyEverybody:false
+            preventMultiCallNotifyEverybody:false,
+            botsAreAvailable: false,
+            assignConversationToBot: false,
+            openAgentRoutingRules: false
         };
 
     }
@@ -85,6 +93,12 @@ handleRadioBtnAutomaticAssignment = () => {
     }
 }
 
+toggleConversationAssignment = () => {
+    this.setState({
+        assignConversationToBot: !this.state.assignConversationToBot
+    })
+}
+
 
   render() {
       const notifyEverybodyContainer = (
@@ -109,27 +123,90 @@ handleRadioBtnAutomaticAssignment = () => {
     )
 
     return (
-        <div className="col-md-8 col-sm-12">
-            <div className="card-block">
-                <div className="row agent-assignment-wrapper">
-                    <h4 className="agent-assignment-title">Set up the way you want conversations to be assigned among your team members</h4>
-                    <div className="options-wrapper">
-                        <h4 className="options-wrapper-title">Select from one of the options below </h4>
-                        <form>
-                            <RadioButton idRadioButton={'notify-everybody-radio'} handleOnChange={this.handleRadioBtnNotifyEverybody}
-                                checked={this.state.checkedNotifyEverybody} label={notifyEverybodyContainer} />
-                             
-                            <RadioButton idRadioButton={'automatic-assignemnt-radio'} handleOnChange={this.handleRadioBtnAutomaticAssignment}
-                                checked={this.state.checkedAutomaticAssignemnt} label={automaticAssignmentContainer} />
-                        
-                                {/* automatic message comming soon
-                                <div  >{automaticAssignmentContainer}</div>*/}
-                        </form>
-
+        <div>
+            <div className="col-md-8 col-sm-12">
+                <div className="row" style={{paddingLeft: "22px", marginLeft: "-7px"}}>
+                    <h4 className="agent-assignment-title">Set up how you want conversations to be assigned to your bots and agents</h4>
+                </div>
+            </div>
+            <div className="col-md-9 col-sm-12">
+                <div className="card-block">
+                    <div className="row agent-assignment-wrapper">
+                        <div className="options-wrapper">
+                            <div className="row">
+                                <div className="col-md-8 col-sm-8">
+                                    <h4 className="options-wrapper-title">Assign new conversations to bot </h4>
+                                </div>
+                                <div className="col-md-4 col-sm-4 text-center">
+                                    <SliderToggle checked={this.state.assignConversationToBot} handleOnChange={this.toggleConversationAssignment} />
+                                </div>
+                            </div>
+                            <div className={this.state.assignConversationToBot ? "n-vis":"row"}>
+                                <div className="col-md-8 col-sm-8" style={{width: "70%", margin:"30px auto"}}>
+                                    <div style={this.state.botsAreAvailable ? { border: "1px dashed #c0c0c0", padding: "23px 17px", backgroundColor: "#cce7f8"}:{ border: "1px dashed #c0c0c0", padding: "23px 17px"}} className="text-center">
+                                        {
+                                            this.state.botsAreAvailable ?  <p className="km-routing-do-not-have-bots">You have bots available. Turn on this section to use them in conversations.</p> : <p className="km-routing-do-not-have-bots">You do not have any bots available. You may start with your <Link to="/bot">Bot Integration</Link> or set up your <Link to="/faq">FAQ</Link> section </p> 
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={!this.state.assignConversationToBot ? "n-vis":null}>
+                                <div className="row" style={{marginTop: "42px"}}>
+                                    <div className="col-md-10 col-sm-12">
+                                        <p className="km-routing-assign-bot-text-1">All new conversations will be assigned to a single bot only. Other available bots (if any) will remain idle.</p>
+                                    </div>
+                                </div>
+                                <div className="row" style={{marginTop: "28px"}}>
+                                    <div className="col-md-7 col-sm-12">
+                                        <p className="km-routing-assign-bot-text-2">Select a bot to handle all new conversations: </p>
+                                    </div>
+                                    <div className="col-md-4 col-sm-12">
+                                        <select>
+                                            <option value="volvo">Batman</option>
+                                            <option value="saab">Superman</option>
+                                            <option value="opel">Ironman</option>
+                                            <option value="audi">Spiderman</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="row" style={{marginTop: "73px"}}>
+                                    <div className="col-md-11 col-sm-11">
+                                        <p style={{ border: "1px solid #c0c0c0", padding:"6px"}} className="km-routing-assign-bot-text-3">Want more routing rules for bot assignment? <Link to="https://docs.kommunicate.io/docs/web-installation.html">See Docs</Link></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-block">
+                    <div className="row agent-assignment-wrapper">
+                        <div className="options-wrapper">
+                            <div className="row" onClick={() => {this.setState({openAgentRoutingRules:!this.state.openAgentRoutingRules})}} style={{cursor: "pointer"}}>
+                                <div className="col-md-8 col-sm-8">
+                                    <h4 className="options-wrapper-title">Routing rules for agents </h4>
+                                </div>
+                                <div className="col-md-4 col-sm-4 text-center">
+                                    <i className={this.state.openAgentRoutingRules ? "icon-arrow-up icons font-2xl d-blockx":"icon-arrow-down icons font-2xl d-blockx"}></i>
+                                </div>
+                            </div>
+                            <form className={this.state.openAgentRoutingRules ? null:"n-vis"}>
+                                <RadioButton idRadioButton={'notify-everybody-radio'} handleOnChange={this.handleRadioBtnNotifyEverybody}
+                                    checked={this.state.checkedNotifyEverybody} label={notifyEverybodyContainer} />
+                                 
+                                <RadioButton idRadioButton={'automatic-assignemnt-radio'} handleOnChange={this.handleRadioBtnAutomaticAssignment}
+                                    checked={this.state.checkedAutomaticAssignemnt} label={automaticAssignmentContainer} />
+                            
+                                    {/* automatic message comming soon
+                                    <div  >{automaticAssignmentContainer}</div>*/}
+                            </form>
+                            <div className={this.state.openAgentRoutingRules ? "row":"n-vis"} style={{backgroundColor: "#cce7f8"}}>
+                                <span style={{padding: "6px"}} className="km-agent-routing-note-text"><strong>NOTE:</strong> An agent will also be assigned to every conversation irrespective of bot routing rules</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>             
+        </div>            
     )
   }
 }
