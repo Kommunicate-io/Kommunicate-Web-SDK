1c387b683120262becb00bc8e070aa2a0ff57d85
Aditya Mehra <mehraaditya71@gmail.com>
2018-04-21 14:29:04 -0600

    KM-862: Updated diagflow bots logic.

--- react/src/utils/kommunicateClient.js
+++ react/src/utils/kommunicateClient.js
@@ -669,19 +669,24 @@ const getIntegratedBots = () => {
   return Promise.all([axios.get(url), getUsersByType(appId, 2)])
     .then( ([mongoBots, sqlBots])=> {
       let bots = []
-      let dialogFlowBots = mongoBots.data.filter(bot=>{
-        return (bot.aiPlatform && bot.aiPlatform.toLowerCase() === 'dialogflow');
-      });
+      let dialogFlowBots = []
+
       for(let i= 0; i < sqlBots.length; i++){
         for(let j = 0; j < mongoBots.data.length; j++ ){
           if(sqlBots[i].name !== "bot" && sqlBots[i].name.toLowerCase() == mongoBots.data[j].name.toLowerCase()){
             let bot1 = sqlBots[i];
             let bot2 = mongoBots.data[j];
             bots[i] = {...bot1, ...bot2};
+
+            if(bots[i].aiPlatform && bots[i].aiPlatform === 'dialogflow'){
+              dialogFlowBots.push(bots[i])
+            }
           }
         }
       }
 
+      console.log(bots);
+      console.log(dialogFlowBots);
 
       return {'allBots': bots, 'dialogFlowBots': dialogFlowBots};
 
@@ -840,9 +845,7 @@ const conversationHandlingByBot = (botId, status) => {
   return Promise.resolve(axios({
     method: 'patch',
     url: converstaionHandlingByBotUrl,
-  })).then(function(response){
-    console.log(response);
-  }).catch(err => {console.log(err)});
+  }))
 
 }
 
