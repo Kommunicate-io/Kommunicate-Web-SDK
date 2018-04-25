966c20171d8af660280085b70c700373ea6b300d
Suraj Khanduri <surajkhanduri@voldemort.local>
2018-04-26 02:19:41 +0530

    creating kommunicate support user when user sign up.

--- react/src/utils/applozicClient.js
+++ react/src/utils/applozicClient.js
@@ -18,7 +18,44 @@ const ApplozicClient ={
             }
         })
 
-    }
+    },
+   createKommunicateSupportUser:function(user){
+    return Promise.resolve(axios.post(getConfig().applozicPlugin.applozicHosturl+"/rest/ws/register/client",user )).then(response=>{
+        let err = {};
+        console.log("Applozic server returned : ",response.status);
+        if (response.status == 200) {
+          if (response.data.message == "INVALID_PARAMETER") {
+            console.log("INVALID_PARAMETER received from applozic Server");
+            err.code = "INVALID_PARAMETER";
+            throw err;
+          } else if (response.data.message == "REGISTERED.WITHOUTREGISTRATIONID") {
+            console.log("received status 200, user created successfully ");
+            return response.data;
+          } else if (response.data.message == "INVALID_APPLICATIONID") {
+            console.log("invalid application Id");
+            err.code = "NVALID_APPLICATIONID";
+            throw err;
+          } else if(response.data.message == "UPDATED"){
+            console.log("user already exists in db userName : ", user.userName, "applicationId : ", user.applicationId);
+            err.code = "USER_ALREADY_EXISTS";
+            err.data = response.data;
+            throw err;
+          }else if(response.data.message == "PASSWORD_INVALID"){
+            console.log("user already exists in db userName : ", user.userName, "applicationId : ", user.applicationId);
+            err.code = "USER_ALREADY_EXISTS_PWD_INVALID";
+            err.data = response.data;
+            throw err;
+          }
+        } else {
+          console.log("received error code  : ", response.status, "from applozic serevr");
+          err.code = "APPLOZIC_ERROR";
+          throw err;
+        }
+      }).catch(err=>{
+        console.log(err);
+        throw err;
+      });
+   }
 }
 
 export default ApplozicClient;
