61c5e6f9ab4e3ea0bcdce08cdc15da0e6f650f16
devashish <devashish.mamgain@gmail.com>
2018-05-01 01:11:56 +0530

    changed 100 to 50 in subscription count

--- react/src/views/Dashboard/Dashboard.js
+++ react/src/views/Dashboard/Dashboard.js
@@ -379,7 +379,7 @@ class Dashboard extends Component {
 
     axios.get(getConfig().kommunicateApi.subscriptionCount)
       .then(function (response) {
-        that.setState({ offerRemaining: (50 - parseInt(response.data)) });
+        that.setState({ offerRemaining: (70 - parseInt(response.data)) });
       });
   }
 
