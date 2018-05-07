fc8d5b8ce1639bc8a571dd2c9ae8125fb257af5f
devashish <devashish.mamgain@gmail.com>
2018-05-08 00:14:00 +0530

    bring template old back file with kmtemplate and kmtmpl changes

--- react/public/applozic/js/app/fullview/applozic.fullview.js
+++ react/public/applozic/js/app/fullview/applozic.fullview.js
@@ -6196,10 +6196,10 @@ var KM_ASSIGNE_GROUP_MAP =[];
 				_this.enableGroupAdminMenuToggle();
 			};
 			_this.enableGroupAdminMenuToggle = function() {
-				$kmApplozic(d).on('click', '.km-group-member-info', function(e) {
+				/*$kmApplozic(d).on('click', '.km-group-member-info', function(e) {
 					e.stopPropagation();
 					$kmApplozic(this).find('.km-menu-box').addClass('open');
-				});
+				});*/
 
 				$kmApplozic('.km-group-member-info').bind("mouseenter", function() {
 					$kmApplozic(this).find('.km-menu-box').removeClass('n-vis');
