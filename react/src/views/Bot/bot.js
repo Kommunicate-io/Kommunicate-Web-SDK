2d6b588c3548ce8eda4c855614c46d718ea48497
Vibhor Sharma <vibhor.sharma94@gmail.com>
2018-05-31 04:15:52 +0530

    added state to TbsComponent

--- react/src/views/Bot/bot.js
+++ react/src/views/Bot/bot.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './bot.css';
-import { Tab } from 'semantic-ui-react';
-import IntegratedBots from '../../components/IntegratedBots/IntegratedBots';
-import BotStore from '../../views/Bot/BotStore';
-// import TabsComponent from '../../components/TabsComponent/TabsComponent';
+// import { Tab } from 'semantic-ui-react';
+// import IntegratedBots from '../../components/IntegratedBots/IntegratedBots';
+// import BotStore from '../../views/Bot/BotStore';
+import TabsComponent from '../../components/TabsComponent/TabsComponent';
 
-const panes = [
-  { menuItem: 'Bot Store', render: () => <Tab.Pane attached={false}><BotStore /></Tab.Pane> },
-  { menuItem: 'Integrated Bots', render: () => <Tab.Pane attached={false} ><IntegratedBots/></Tab.Pane> },
-]
+// const panes = [
+//   { menuItem: 'Bot Store', render: () => <Tab.Pane attached={false}><BotStore /></Tab.Pane> },
+//   { menuItem: 'Integrated Bots', render: () => <Tab.Pane attached={false} ><IntegratedBots/></Tab.Pane> },
+// ]
 
 class Tabs extends Component {
   render() {
@@ -20,8 +20,9 @@ class Tabs extends Component {
             <div className="bot-main-card-container">
               <div className="row">
                 <div style={{width:"100%"}}>
-                  {/* <TabsComponent /> */}
-                  <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+                  <TabsComponent />
+                  {/* <p>Currently using React {React.version}</p> */}
+                  {/* <Tab menu={{ secondary: true, pointing: true }} panes={panes} /> */}
                 </div>
               </div>
             </div>
