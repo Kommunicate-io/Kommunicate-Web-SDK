1ed1bd9fe8975af3e3e5b5bccd184e3253030b67
Suraj Khanduri <surajkhanduri@voldemort.local>
2018-05-31 04:00:39 +0530

    fixing bugs

--- react/src/components/TabsComponent/TabsComponent.js
+++ react/src/components/TabsComponent/TabsComponent.js
@@ -6,8 +6,8 @@ import BotStore from '../../views/Bot/BotStore';
 export default class TabsComponent extends Component {
     render() {
         const panes = [
-            { menuItem: 'Bot Store', render: () => <Tab.Pane attached={false}><BotStore /></Tab.Pane> },
-            { menuItem: 'Integrated Bots', render: () => <Tab.Pane attached={false} ><IntegratedBots/></Tab.Pane> },
+            { menuItem: 'Bot Store', render: () => {return (<Tab.Pane attached={false}><BotStore /></Tab.Pane> )}},
+            { menuItem: 'Integrated Bots', render: () => {return (<Tab.Pane attached={false} ><IntegratedBots/></Tab.Pane> )}},
           ]
           return(
             <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
