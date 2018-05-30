9e3d12c24ac81953862bdd060a702352f0734dd5
Vibhor Sharma <vibhor.sharma94@gmail.com>
2018-05-31 03:32:12 +0530

    Updated Bot.js file

--- react/src/views/Bot/bot.js
+++ react/src/views/Bot/bot.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './bot.css';
-import TabsComponent from '../../components/TabsComponent/TabsComponent';
+import { Tab } from 'semantic-ui-react';
+import IntegratedBots from '../../components/IntegratedBots/IntegratedBots';
+import BotStore from '../../views/Bot/BotStore';
+// import TabsComponent from '../../components/TabsComponent/TabsComponent';
 
+const panes = [
+  { menuItem: 'Bot Store', render: () => <Tab.Pane attached={false}><BotStore /></Tab.Pane> },
+  { menuItem: 'Integrated Bots', render: () => <Tab.Pane attached={false} ><IntegratedBots/></Tab.Pane> },
+]
 
-// const panes = [
-//   { menuItem: 'Bot Store', render: () => <Tab.Pane attached={false}><BotStore /></Tab.Pane> },
-//   { menuItem: 'Integrated Bots', render: () => <Tab.Pane attached={false} ><IntegratedBots/></Tab.Pane> },
-// ]
-// const TabExampleSecondaryPointing = () => (
-//   <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
-// )
 class Tabs extends Component {
   render() {
     return (
@@ -20,11 +20,9 @@ class Tabs extends Component {
             <div className="bot-main-card-container">
               <div className="row">
                 <div style={{width:"100%"}}>
-                  <TabsComponent />
+                  {/* <TabsComponent /> */}
+                  <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
                 </div>
-                {/* <div className="col-sm-12 km-bot-integration-heading">
-                  <p>Integrating a bot will allow you to send answers to some customer <br />queries automatically</p>
-                </div> */}
               </div>
             </div>
           </div>
