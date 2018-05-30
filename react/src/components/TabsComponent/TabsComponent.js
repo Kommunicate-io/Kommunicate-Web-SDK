baf8ccad01459f197b174e5b59e54f77d5c09cd6
Vibhor Sharma <vibhor.sharma94@gmail.com>
2018-05-31 02:41:31 +0530

    Fixed "No `render` method found" error

---
+++ react/src/components/TabsComponent/TabsComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Tab } from 'semantic-ui-react';
+import IntegratedBots from '../../components/IntegratedBots/IntegratedBots';
+import BotStore from '../../views/Bot/BotStore';
+
+export default class TabsComponent extends Component {
+    render() {
+        const panes = [
+            { menuItem: 'Bot Store', render: () => <Tab.Pane attached={false}><BotStore /></Tab.Pane> },
+            { menuItem: 'Integrated Bots', render: () => <Tab.Pane attached={false} ><IntegratedBots/></Tab.Pane> },
+          ]
+          return(
+            <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+          )
+    }
+}
