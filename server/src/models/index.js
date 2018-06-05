76d09b2d4ede3dd5ed372d29db59732d754478a2
rudranand <k.anand2189@gmail.com>
2018-06-05 11:36:44 +0530

    KM-1048 application alias removed

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -17,7 +17,7 @@ models.forEach(function(model) {
 db[model]= sequelize.import(path.join(__dirname, model));
 });
 
-db['customer'].hasMany(db['Application'],{targetKey:'customerId', as:'applications'});
+db['customer'].hasMany(db['Application'],{targetKey:'customerId'});
 db['Application'].belongsTo(db['customer'], {sourceKey:'customerId'} );
 //dont use foreign keys 
 //db.OffBusinessHoursConfig.belongsTo(db.BusinessHour,{constraints:false});
