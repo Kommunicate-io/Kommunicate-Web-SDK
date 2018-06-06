f839d2393eabcf883691b7ea80c8409794227060
Hardik Parashar <dev.hardik.parashar@gmail.com>
2018-06-06 18:25:48 +0530

    KM-1122 dialogue directory error in run.sh fixed

--- machine-learning/rasa/faqbot/run.sh
+++ machine-learning/rasa/faqbot/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf models/
 python -m rasa_nlu.train --config faq_config.yml --data faq_data.json --path models/nlu --fixed_model_name faq_model_v1
-python -m rasa_core.train -d faq_domain.yml -s faq_stories.md -o models/current/dialogue --epochs 300
+python -m rasa_core.train -d faq_domain.yml -s faq_stories.md -o models/dialogue --epochs 300
 exit 0
