d79aceea52353dd6caff7d55f161c396e11cb08c
Suraj Khanduri <surajkhanduri@voldemort.Dlink>
2018-06-25 16:34:57 +0530

    adding email config for staging

--- server/conf/emailConfig.js
+++ server/conf/emailConfig.js
@@ -41,7 +41,18 @@ var config ={
       userName : "support@kommunicate.io",
       password:"knowledge123"
     }
+  },
+  staging:{
+
+    hostUrl:"smtp.gmail.com",
+    port:587,
+    auth:{
+    //userName:"support@applozic.com",
+    //password:"knowledge1234"
+    userName : "support@kommunicate.io",
+    password:"knowledge123"
   }
+}
 }
 exports.getProperties = function(){
   var envId = getEnvId()?getEnvId():"default";
