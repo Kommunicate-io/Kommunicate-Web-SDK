c45f06c0a548e9679bb574b4083f63a5a567365f
Hardik Parashar <dev.hardik.parashar@gmail.com>
2018-06-27 18:55:40 +0530

    KM-1178 changes-2

--- machine-learning/rasa/fetch_customers.py
+++ machine-learning/rasa/fetch_customers.py
@@ -1,10 +1,15 @@
 from pymongo import MongoClient
 from datetime import datetime, timedelta
+from config.config import get_config
+from sys import argv
 import time
 import requests
-from config.default import *
 import json
-client = MongoClient(mongo_url)
+if(len(argv) < 2):
+    env = get_config(None)
+else:
+    env = get_config(argv[1])
+client = MongoClient(env.mongo_url)
 db = client['kommunicate']
 collection = db['knowledgebase']
 appkeys = []
@@ -13,5 +18,5 @@ for data in collection.find({'created_at':{'$gte':(time.time() - 8640000)*1000}}
     appkeys.append(str(data))
 for key in appkeys:
     print key
-r = requests.post(rasa_endpoint,headers={'content-type':'application/json'},data=json.dumps({'data':appkeys}))
+r = requests.post(env.rasa_endpoint,headers={'content-type':'application/json'},data=json.dumps({'data':appkeys}))
 print r.text
