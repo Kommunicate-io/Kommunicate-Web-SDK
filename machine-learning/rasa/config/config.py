2e7dff1cafb02f80dd805cccbc29c0846748e450
Hardik Parashar <dev.hardik.parashar@gmail.com>
2018-06-27 20:46:18 +0530

    KM-1178 changes-3

--- machine-learning/rasa/config/config.py
+++ machine-learning/rasa/config/config.py
@@ -1,8 +1,3 @@
 from importlib import import_module
 def get_config(param):
-    if (param == 'test'):
-        return import_module('config.test')
-    elif(param == 'prod'):
-        return import_module('config.prod')
-    else:
-        return import_module('config.default')
+        return import_module('config.' + param)
