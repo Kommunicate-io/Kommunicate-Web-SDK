8636af37464e6c619f848a4062e3ef583e3fc9ee
abhinav_bhardwaj <ab14bhardwaj@gmail.com>
2018-06-28 17:04:04 +0530

    KM-1183 Made suggested changes

--- react/src/views/UserItem/CustomerListItem.js
+++ react/src/views/UserItem/CustomerListItem.js
@@ -144,45 +144,23 @@ class CustomerListItem extends Component {
       textDecoration: 'underline',
       color: '#0000EE'
     };
-    var conversationClass = this.props.hideConversation
-      ? 'n-vis'
-      : 'vis';
+    var conversationClass = this.props.hideConversation? 'n-vis': 'vis';
     var user = this.props.user;
     var emailId = user.email;
     var status = user.status;
     var displayName = CommonUtils.getDisplayName(user);
-    var online = (user.connected === true)
-      ? 'avatar-status badge-success '
-      : 'n-vis';
-    var latestConversation = user.messagePxy
-      ? user.messagePxy.message
-      : null;
-    var lastMessageTime = user.messagePxy
-      ? (window.$kmApplozic.fn.applozic('getDateTime', user.messagePxy.createdAtTime))
-      : '';
-    var asignee = user.assignee
-      ? user.assignee
-      : "";
-    var groupId = user.messagePxy
-      ? user.messagePxy.groupId
-      : "";
-    var image = (user.imageLink)
-      ? (user.imageLink)
-      : '';
-    var imageExpr = (user.imageLink)
-      ? 'img-avatar vis'
-      : 'n-vis';
-    var nameExpr = (user.imageLink)
-      ? 'n-vis'
-      : 'km-alpha-contact-image vis';
+    var online = (user.connected)? 'avatar-status badge-success': 'n-vis';
+    var latestConversation = user.messagePxy? user.messagePxy.message: null;
+    var lastMessageTime = user.messagePxy? (window.$kmApplozic.fn.applozic('getDateTime', user.messagePxy.createdAtTime)) : '';
+    var asignee = user.assignee? user.assignee : "";
+    var groupId = user.messagePxy? user.messagePxy.groupId : "";
+    var image = (user.imageLink)? (user.imageLink) : '';
+    var imageExpr = (user.imageLink)? 'img-avatar vis' : 'n-vis';
+    var nameExpr = (user.imageLink)? 'n-vis': 'km-alpha-contact-image vis';
     var name = displayName.charAt(0).toUpperCase();
     var createdAtTime = window.$kmApplozic.fn.applozic('getDateTime', user.createdAtTime);
-    var lastLoggedInAtTime = (typeof user.lastLoggedInAtTime !== 'undefined')
-      ? (window.$kmApplozic.fn.applozic('getDateTime', user.lastLoggedInAtTime))
-      : '';
-    var lastSeenAt = (typeof user.lastSeenAtTime !== 'undefined')
-      ? (this.lastSeenTime(user.lastSeenAtTime))
-      : '';
+    var lastLoggedInAtTime = (typeof user.lastLoggedInAtTime !== 'undefined')? (window.$kmApplozic.fn.applozic('getDateTime', user.lastLoggedInAtTime)) : '';
+    var lastSeenAt = (typeof user.lastSeenAtTime !== 'undefined')? (CommonUtils.lastSeenTime(user.lastSeenAtTime)) : '';
     return (<tr>
       <td className="text-center">
         <div>
@@ -222,7 +200,7 @@ class CustomerListItem extends Component {
                         color: "#a4a3a9",
                         fontSize: "13px"
                       }}>No Conversation</span>
-                  : this.lastSeenTime(user.messagePxy.createdAtTime)
+                  : CommonUtils.lastSeenTime(user.messagePxy.createdAtTime)
               }
             </td>
       }
