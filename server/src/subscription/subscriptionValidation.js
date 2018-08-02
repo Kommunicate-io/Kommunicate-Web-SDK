52e3aad940693b6d8898d41e58655f3c9f3908a5
Suraj Kumar <Suraj-Kumar@users.noreply.github.com>
2018-08-02 20:00:31 +0530

    adding applicationId support in subscription API. (#1580)

--- server/src/subscription/subscriptionValidation.js
+++ server/src/subscription/subscriptionValidation.js
@@ -14,6 +14,7 @@ module.exports.createSubscription = {
       integrationName: joi.string().max(20),
       //events: joi.array().items(events).required()
       eventType: joi.string().max(20).required(),
+      applicationId:joi.string().max(150).required(),
       triggerUrl: joi.string().max(100).regex(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/).required()
     }
     
