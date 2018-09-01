1b447cae2177bb8417dd5d1fb87e6cea3b61b4b2
rudranand <k.anand2189@gmail.com>
2018-09-01 23:21:59 +0530

    add webpack build

--- server/conf/webpack.js
+++ server/conf/webpack.js
@@ -1,27 +1,33 @@
 const path = require("path");
-const outputDirectory = "build";
+const fs = require('fs');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const nodeModules = {};
+
+
+ fs.readdirSync(path.resolve(__dirname, '../node_modules'))
+     .filter(function(x) {
+         return ['.bin'].indexOf(x) === -1;
+     })
+     .forEach(function(mod) {
+         nodeModules[mod] = 'commonjs ' + mod;
+     });
+
 
 module.exports = {
-    entry: "../src/app.js",
+    entry: "./app.js",
     target: 'node',
-    output: {
-      path: path.join(__dirname, outputDirectory),
-      filename: "kommunicateServer.js"
-    },
+    //context: path.resolve(__dirname, 'conf'),
     externals: nodeModules,
+    output: {
+      //path: path.join(__dirname, outputDirectory),
+      filename: "./build/kommunicateServer.js"
+    },
+    //watch:true,
+     // in order to ignore all modules in node_modules folder
     // module: {
     //   rules: [
-    //     // {
-    //     //   test: /\.js$/,
-    //     //   exclude: /node_modules/,
-    //     //   use: {
-    //     //     loader: "babel-loader"
-    //     //   }
-    //     // },
-    //     {
-    //       test: /\.css$/,
-    //       use: ["style-loader", "css-loader"]
-    //     }
+        
     //   ]
     // }
 }
