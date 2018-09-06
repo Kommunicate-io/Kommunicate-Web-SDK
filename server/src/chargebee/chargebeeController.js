7e74a22f5887b4fcb0539f217924e49e38de2c44
rudranand <k.anand2189@gmail.com>
2018-09-06 11:53:56 +0530

    resolve undefiend var

--- server/src/chargebee/chargebeeController.js
+++ server/src/chargebee/chargebeeController.js
@@ -13,8 +13,11 @@ exports.subscriptionCount = function (req, res) {
 exports.getSubscriptionDetail = async function (req, res) {
     try {
         let response = await getSubscription(req.params.userId);;
-        return response ? res.status(200).json({ "code": "success", "response": response.subscription })
-            : res.status(200).json({ "code": "success", "message": "record not found" });
+        if (typeof response == 'object') {
+            return res.status(200).json({ "code": "success", "response": response.subscription })
+        } else {
+            return res.status(200).json({ "code": "success", "message": response });
+        }
     } catch (err) {
         return res.status(500).json({ "code": "error", "response": err.message });
     }
@@ -24,11 +27,11 @@ exports.updateSubscribedAgentCount = async function (req, res) {
     let addPlanQuantity = req.body.addPlanQuantity;
     try {
         let result = await getSubscription(req.params.userId);
-        if (result) {
+        if (typeof result=='object') {
             let response = await chargebeeService.updateSubscription(result.subscription.customer_id, { "plan_quantity": addPlanQuantity + result.subscription.plan_quantity });
             return res.status(200).json({ "code": "success", "response": response });
         } else {
-            return res.status(500).json({ "code": "error", "message": "updation error" });
+            return res.status(500).json({ "code": "error", "message": result});
         }
     } catch (err) {
         return res.status(500).json({ "code": "error", "response": err.message });
@@ -39,10 +42,10 @@ const getSubscription = async function (userId) {
     try {
         let customer = await customerService.getCustomerByUserName(userId);
         if (!customer) {
-            return res.status(200).json({ "code": "success", "message": "customer not found" });
+            return "customer not found";
         }
         if (!customer.billingCustomerId) {
-            return res.status(200).json({ "code": "success", "message": "not subscribe" });
+            return "not subscribe";
         }
         let result = await chargebeeService.getSubscriptionDetail(customer.billingCustomerId);
         return result;
