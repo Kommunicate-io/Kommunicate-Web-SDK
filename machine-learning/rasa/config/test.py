6c107020c2333926185cd6ad49d5fd451c2828b0
devashish <devashish.mamgain@gmail.com>
2018-09-12 21:17:31 +0530

    KM-1465 correct reference_id set to None

--- machine-learning/rasa/config/test.py
+++ machine-learning/rasa/config/test.py
@@ -1,6 +1,6 @@
 mongo_url = 'mongodb://applozicdba:applozicdba@ec2-184-72-95-64.compute-1.amazonaws.com:27017/kommunicate?authSource=admin'
-#rasa_endpoint = 'https://rasa-test.kommunicate.io/'
-rasa_endpoint = 'http://localhost:5001/'
+rasa_endpoint = 'https://rasa-test.kommunicate.io/'
+#rasa_endpoint = 'http://localhost:5001/'
 
 
 #end point of cron time stamp at Node
