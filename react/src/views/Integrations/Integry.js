29acdbee8847e9c2154a0eddd65a1f65b131dd8d
Suraj Khanduri <surajkhanduri@voldemort.local>
2018-10-03 14:08:58 +0530

    refractoring methods

--- react/src/views/Integrations/Integry.js
+++ react/src/views/Integrations/Integry.js
@@ -3,13 +3,51 @@ import React, { Component } from 'react';
 import Integrations from './Integrations';
 import { Link } from 'react-router-dom';
 import CommonUtils from '../../utils/CommonUtils';
-    function callbackFunc(data) { 
-        //in this method you can perform integration post-save actions 
-        //for example you can save bundle instance id returned in your database 
-        console.log("callback 1",data); 
-    } 
-    function callbackFunc_Render_Integration_Row(integration) { 
-        // console.log("callbackFunc_Render_Integration_Row")
+    
+function initilizeIntegry(settings) {
+    if(typeof settings !='object'){
+      return;  
+    }
+    // this function should be called after integry SDK is loaded completely
+    // todo : replace hardcoded data with  settings object. 
+    window.integryAppSDK.load({
+        app_key: "6ab3030f-cbcc-40c0-9e89-7bf33e71a4da",
+        user_id: "yasir@integry.io",
+        hash: "24bee38347a2f3a39b9846ad47b2e57c34c77ac693e63f99bbad619929940a53",
+        bundle_id: "23",
+        bundle_instance_id: "",
+        callback: integrationSuccessCallback,
+        callback_render_integration_row: renderIntegrationRowCallback,
+        callback_render_template_row: renderTemplateRowCallback,
+        render_templates_container: 'templates',
+        render_integrations_container: 'integrations',
+        x_integry_configs: {
+            view_container: 'intcontainer',
+            view_url: '',
+            app_auth: {
+                api_key: 'c690bd29-7987-4e02-8c34-09a1547477f8'
+            }
+        },
+        x_custom_variables: {
+            "application_id": "23cab6b8b34de229c0aaa6add5dfc4f78"
+        }
+    });
+}
+
+function integrationSuccessCallback(data) {
+    // callback when integration process is finished. will be called when use click on the save button in template form.
+    //in this method you can perform integration post-save actions 
+    //for example you can save bundle instance id returned in your database 
+   console.log("integration data ",data); 
+   alert("Integration added successfully");
+   cancelAddNewIntegration();
+}
+
+
+function renderIntegrationRowCallback (integration) {
+    alert("renderIntegrationRowCallback");
+    // not sure if it is in use. check it 
+     // console.log("callbackFunc_Render_Integration_Row")
         // var status_link = (integration.status === 'ACTIVE') ? integration.disable_link : integration.enable_link; 
         // var status_op = (integration.status === 'ACTIVE') ? 'disable' : 'enable'; 
         // var status_text = (integration.status === 'ACTIVE') ? 'Disable' : 'Enable'; 
@@ -34,25 +72,13 @@ import CommonUtils from '../../utils/CommonUtils';
         // var row = window.$kmApplozic('<tr class="bg-white"/>'); 
         // row.append(window.$kmApplozic.parseHTML(html)); 
         // return row; 
-    } 
-    function callbackFunc_Render_Template_Row(template) {
-        console.log("callbackFunc_Render_Template_Row")
+}   
+
+function renderTemplateRowCallback(template) {
+    // it will render the template list(available application list for integration) in the page.  
+    
+    console.log("callbackFunc_Render_Template_Row")
         var img = (template.branding_app && template.branding_app.icon_url) ? template.branding_app.icon_url : 'assets/img/integry-icon.png'; 
-        
-        
-        
-        // var html = `<th scope="row" class="col-md-6">  
-        //         <a class="media align-items-center" href="#"> 
-        //             <img alt="Image" src="` + img + `" class="avatar rounded avatar-sm" /> 
-        //             <div class="media-body"> 
-        //             <span class="h6 mb-0">` + template.name + `</span> 
-        //             <span class="text-muted">` + template.description + `</span> 
-        //             </div> 
-        //         </a> 
-        //         </th> 
-        //         <td> 
-        //         <a href="` + template.link + `" class="btn btn-success">Add</a> 
-        //         </td>`;
         let key = template.name.toLowerCase()         
         thirdPartyList[key] = {
             key: key,
@@ -62,18 +88,21 @@ import CommonUtils from '../../utils/CommonUtils';
             integrationType: 0,
             state: key+"Keys",
             // label :<Link to= {template.link} data-key = {key} className="integration-settings">Add</Link>,
-            label :<a href= {template.link} data-key = {key} className="integration-settings">Add</a>,
+            label :<span  data-key = {key} className="integration-settings" onClick ={ function(e){addNewIntegration(template.id);e.preventDefault();e.stopPropagation();}}>Add</span>,
             docsLink: "",
             domain:"",
-            instructions:""
+            instructions:["instruction1","instruction 2"]
             // label :<span href= {template.link} data-key = {key} target="_blank" className="integration-settings">Add</span> 
         }
-        CommonUtils.triggerCustomEvent("kmIntegryInitilized",{detail:{"status": "success","list":thirdPartyList},bubbles: true,cancelable: true})
-        //  window.dispatchEvent(new CustomEvent("kmIntegryInitilized",{detail:{"status": "success","list":thirdPartyList},bubbles: true,cancelable: true}));
-        // var row = window.$kmApplozic('<tr class="bg-whiteuuyuy"/>'); 
-        // row.append(window.$kmApplozic.parseHTML(html)); 
-        // return row;
+        CommonUtils.triggerCustomEvent("kmIntegryInitilized",{detail:{"status": "success","list":thirdPartyList},bubbles: true,cancelable: true});
         return window.$kmApplozic('<tr>'); 
+}
+function addNewIntegration(templateId) {
+    window.$kmApplozic(".integration-form-preview").show();
+    window.integryAppSDK.renderTemplateForm(templateId);
+}
+function cancelAddNewIntegration() {
+    window.$kmApplozic(".integration-form-preview").hide();
+}
 
-    }
-export {callbackFunc_Render_Integration_Row, callbackFunc_Render_Template_Row }
+export { initilizeIntegry }
