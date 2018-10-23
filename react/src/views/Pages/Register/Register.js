4a64733b28cd63c0c007df56855d031b7718c252
Vibhor Sharma <vibhor.sharma94@gmail.com>
2018-10-23 12:49:58 +0530

    Showing Signup and Login with Google AGAIN

--- react/src/views/Pages/Register/Register.js
+++ react/src/views/Pages/Register/Register.js
@@ -279,7 +279,9 @@ class Register extends Component {
                   <h1 className="login-signup-heading text-center">Sign up to Kommunicate</h1>
                   {/* <p className="text-muted login-signup-sub-heading text-center">Your account information</p> */}
 
-                  <a className={ (this.state.googleOAuth || this.state.isInvited) ? "n-vis":"signup-with-google-btn n-vis"} href={this.state.googleSignUpUrl}>
+                  {/* Signup with Google code STARTS here. */}
+                    {/* To show or hide Signup with Google just add "n-vis" to  "signup-with-google-btn" and "or-seperator" class.*/}
+                  <a className={ (this.state.googleOAuth || this.state.isInvited) ? "n-vis":"signup-with-google-btn"} href={this.state.googleSignUpUrl}>
                     <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48" width="24" height="24">
                       <defs>
                         <path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" />
@@ -295,10 +297,12 @@ class Register extends Component {
                     Sign up with Google
                   </a>
 
-                  <div className="or-seperator n-vis">
+                  <div className="or-seperator">
                     <div className="or-seperator--line"></div>
                     <div className="or-seperator--text">OR</div>
                   </div>
+
+                  {/* Signup with Google code ENDS here. */}
 
                   <div className={this.state.googleOAuth?"input-group mb-3":"n-vis"}>
                   {/*<span className="input-group-addon"><i className="icon-user"></i></span>*/}
