85591392e4679b1f40a812f1f4459d472d9ca117
Abhinav Bhardwaj <ab14bhardwaj@gmail.com>
2018-10-24 14:19:38 +0530

    Access type your message in widget text box from labels.

--- js/app/kommunicate-ui.js
+++ js/app/kommunicate-ui.js
@@ -130,7 +130,7 @@ KommunicateUI={
         $applozic("#mck-email-collection-box").removeClass("vis").addClass("n-vis");
         $applozic("#mck-email-error-alert-box").removeClass("vis").addClass("n-vis");
         $applozic("#mck-btn-attach-box").removeClass("n-vis").addClass("vis");
-        $applozic('#mck-text-box').attr('data-text', "Type your message...");
+        $applozic('#mck-text-box').attr('data-text', MCK_LABELS['input.message']);
     },
     validateEmail: function (sendMsg) {
         var mailformat = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
