cb1c9e4c5c17a2bf32c61f8e349aab3e2c43ba64
Abhinav Bhardwaj <ab14bhardwaj@gmail.com>
2018-11-21 20:50:08 +0530

    Change placeholder of user search from search in users
    1. Change placeholder of user search from search in users from
    Search in users ->  Search for email or user name

--- react/src/views/Users/Users.js
+++ react/src/views/Users/Users.js
@@ -325,7 +325,7 @@ class Users extends Component {
            <svg id="km-search-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52.966 52.966">
                 <path fill="#a8a8a8" d="M51.704 51.273L36.845 35.82c3.79-3.801 6.138-9.041 6.138-14.82 0-11.58-9.42-21-21-21s-21 9.42-21 21 9.42 21 21 21c5.083 0 9.748-1.817 13.384-4.832l14.895 15.491a.998.998 0 0 0 1.414.028 1 1 0 0 0 .028-1.414zM21.983 40c-10.477 0-19-8.523-19-19s8.523-19 19-19 19 8.523 19 19-8.524 19-19 19z"/>
               </svg>
-            <input id="km-search-box" type="text" className="km-search-box required"  onClick={(event) => this.handleClickEventForSearch(event)} onKeyUp={(event) => this.handleKeyboardEventForSearch(event)} placeholder="Search in users"></input>
+            <input id="km-search-box" type="text" className="km-search-box required"  onClick={(event) => this.handleClickEventForSearch(event)} onKeyUp={(event) => this.handleKeyboardEventForSearch(event)} placeholder="Search for email or user name"></input>
             <span id="km-clear-search-text" className=" km-clear-search-text n-vis"  onClick={(event) => this.handleClickEventForSearch(event)}> &times; </span>
             </div>
             <table className={this.state.result.length !== 0 ? "table table-hover mb-0 hidden-sm-down km-show-visibility":"table table-hover mb-0 hidden-sm-down km-hide-visibility"}>
