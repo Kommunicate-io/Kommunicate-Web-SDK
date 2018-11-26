68a2f86195096981703b7400fd851baa9f5fdc3a
Jithin Balakrishnan <34020392+jithinbalu@users.noreply.github.com>
2018-11-26 12:17:29 +0530

    KM-1669: fix - Cannot read property 'application' of null (#2583)

--- react/src/sentry/sentry.js
+++ react/src/sentry/sentry.js
@@ -5,12 +5,14 @@ import * as Sentry from '@sentry/browser'
 export const setTag = (scope) => {
     try {
         let userSession = CommonUtils.getUserSession();
-        scope.setTag("applicationId", userSession.application.applicationId);
-        scope.setTag("userId", userSession.userName);
-        scope.setUser({
+        if(userSession) {
+            scope.setTag("applicationId", userSession.application.applicationId);
+            scope.setTag("userId", userSession.userName);
+            scope.setUser({
             id: userSession.application.applicationId,
             username: userSession.userName
         });
+        }
     } catch (error) {
         Sentry.captureException(error);
     }
