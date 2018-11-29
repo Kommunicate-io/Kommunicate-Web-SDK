8dbf5178fe77d7e20784556be52fa36f55541252
jithinbalu <jithin@kommunicate.io>
2018-11-30 00:35:32 +0530

    integrate chrome redux dev tools

--- react/src/store/store.js
+++ react/src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware  } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
@@ -6,8 +6,11 @@ import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 import rootReducer from '../reducers/index'
 import { getConfig } from '../config/config';
 
+export let store;
+export let persistor;
+export let pReducer;
 
-
+// const enableDevTools = false;
 const enableDevTools =getConfig().enableDevTools
 const persistConfig = {
     key: 'root',
@@ -15,7 +18,25 @@ const persistConfig = {
     stateReconciler: autoMergeLevel2
 };
 
-const pReducer = persistReducer(persistConfig, rootReducer);
+// pReducer = persistReducer(persistConfig, rootReducer);
+pReducer = persistReducer(persistConfig, rootReducer);
+if(enableDevTools) {
+    // redux dev tool extension for chrome 
+    const composeEnhancers =
+        typeof window === 'object' &&
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            }) : compose;
+    const enhancer = composeEnhancers(
+        applyMiddleware(logger),
+    );
+    
+    store = createStore(pReducer, enhancer);
+    // persistor = persistStore(store);
 
-export const store = createStore(pReducer);
-export const persistor = persistStore(store);
+} else {
+    // pReducer = persistReducer(persistConfig, rootReducer);
+    store = createStore(pReducer);
+    // persistor = persistStore(store);
+}
+persistor = persistStore(store);
