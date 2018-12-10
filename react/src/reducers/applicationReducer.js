52abff19ba483c1ebf0e984ec62d92fd6ff275b6
jithinbalu <jithin@kommunicate.io>
2018-12-10 11:31:24 +0530

    declare initial states of app setting

--- react/src/reducers/applicationReducer.js
+++ react/src/reducers/applicationReducer.js
@@ -1,13 +1,37 @@
+import { ROUTING_RULES_FOR_AGENTS } from '../utils/Constant';
+
 const initialState = {
-  appSettings: null,
+    agentRouting:null,
+    applicationId:null,
+    botRouting:null,
+    collectEmailOnAwayMessage:null,
+    collectEmailOnWelcomeMessage:null,
+    defaultConversationAssignee:{},
+    removeBotOnAgentHandOff:null,
+    widgetTheme:{
+      iconIndex:null,
+      primaryColor:null,
+      secondaryColor:null,
+      widgetImageLink:null
+    }
 
 };
+initialState.defaultConversationAssignee[ROUTING_RULES_FOR_AGENTS.NOTIFY_EVERYBODY] = null;
+initialState.defaultConversationAssignee[ROUTING_RULES_FOR_AGENTS.AUTOMATIC_ASSIGNMENT] = null;
+
 const applicationReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SAVE_APP_SETTINGS':
       return {
         ...state,
-        appSettings: Object.assign({}, action.payload)
+        applicationId : action.payload.applicationId,
+        agentRouting :action.payload.agentRouting,
+        botRouting : action.payload.botRouting,
+        collectEmailOnAwayMessage : action.payload.collectEmailOnAwayMessage,
+        collectEmailOnWelcomeMessage : action.payload.collectEmailOnWelcomeMessage,
+        defaultConversationAssignee : action.payload.defaultConversationAssignee,
+        removeBotOnAgentHandOff : action.payload.removeBotOnAgentHandOff,
+        widgetTheme : action.payload.widgetTheme,
       }
     default:
       return state
