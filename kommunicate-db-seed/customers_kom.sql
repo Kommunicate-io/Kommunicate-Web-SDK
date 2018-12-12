19146cd1531c10acae06677f433b001db9ad3d30
Ranjeet Priyadarshi <ranjeet@applozic.com>
2018-12-12 17:47:39 +0530

    Update customers_kom.sql (#2677)

--- kommunicate-db-seed/customers_kom.sql
+++ kommunicate-db-seed/customers_kom.sql
@@ -4,5 +4,6 @@ LIMIT 0, 1000
 
 -- Date: 2018-08-29 16:41
 */
-INSERT INTO `customers` (`id`,`name`,`user_name`,`email`,`company_name`,`company_size`,`created_at`,`updated_at`,`deleted_at`,`role`,`industry`,`contact_no`,`active_campaign_id`,`agent_routing`,`billing_cus_id`,`subscription`,`bot_routing`) VALUES (1,'Devashish','devashish@kommunicate.io','devashish@kommunicate.io','Kommmunicate','20','2017-11-14 17:10:49','2018-08-09 08:40:47',NULL,'CEO','Gaming','9535008745',NULL,0,'1mk51X1QuqehiXNOT','per_agent_yearly',0);
-INSERT INTO `customers` (`id`,`name`,`user_name`,`email`,`company_name`,`company_size`,`created_at`,`updated_at`,`deleted_at`,`role`,`industry`,`contact_no`,`active_campaign_id`,`agent_routing`,`billing_cus_id`,`subscription`,`bot_routing`) VALUES (2,'Kommunicate','techDisrupt@applozic.com','techDisrupt@applozic.com',NULL,NULL,'2017-11-13 11:49:25','2017-11-13 11:49:25',NULL,NULL,NULL,NULL,NULL,0,NULL,'startup',0);
+
+INSERT INTO `customers` (`id`,`name`,`user_name`,`email`,`company_name`,`company_size`,`created_at`,`updated_at`,`deleted_at`,`role`,`industry`,`contact_no`,`active_campaign_id`,`billing_cus_id`,`subscription`) VALUES (1,'Devashish','devashish@kommunicate.io','devashish@kommunicate.io','Kommmunicate','20','2017-11-14 17:10:49','2018-08-09 08:40:47',NULL,'CEO','Gaming','9535008745',NULL,'1mk51X1QuqehiXNOT','per_agent_yearly');
+INSERT INTO `customers` (`id`,`name`,`user_name`,`email`,`company_name`,`company_size`,`created_at`,`updated_at`,`deleted_at`,`role`,`industry`,`contact_no`,`active_campaign_id`,`billing_cus_id`,`subscription`) VALUES (2,'Kommunicate','techDisrupt@applozic.com','techDisrupt@applozic.com',NULL,NULL,'2017-11-13 11:49:25','2017-11-13 11:49:25',NULL,NULL,NULL,NULL,NULL,NULL,'startup');
