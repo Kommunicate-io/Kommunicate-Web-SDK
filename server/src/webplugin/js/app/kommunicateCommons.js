4a66b808834acbd994b565aaebd7309b979002e8
Akshat <akshat.dhabi@gmail.com>
2019-01-03 17:58:40 +0530

    optimize the modifyClassList function to accept id and classes
    -performance improvements

--- server/src/webplugin/js/app/kommunicateCommons.js
+++ server/src/webplugin/js/app/kommunicateCommons.js
@@ -54,22 +54,24 @@ function KommunicateCommonFunction() {
         remove && elem.classList.remove(remove); 
     }
     /* use this method instead of jquery method to manipulate classes. for eg to display [vis] or hide [n-vis] an element
-       addClass and removeClass to be passed as strings in the case of no classes pass "" elem will always be passed as an 
-       array or strings containing IDs or Classes on which the classes need to be manipulated */
+       addClass and removeClass to be passed as strings in the case of no classes pass "" elem will always be passed as an object
+       array of strings containing IDs or Classes on which the classes need to be manipulated infromt of respective object property*/
 
     _this.modifyClassList = function(elem, addClass, removeClass){ 
-        elem.forEach(function(item){
-            if(document.getElementById(item) !== null ){
-                _this.classListChanger(document.getElementById(item),addClass,removeClass);
-            }
-        else{
-            var list = document.getElementsByClassName(item);
-                list.forEach(function(node){
-                this.classListChanger(node,addClass,removeClass);
-                })
-            }
-        })
-
+            var idList = elem.id,
+                classList = elem.class , list=[];
+            idList && idList.forEach(function(id){
+                list.push(document.getElementById(id));
+            })
+            classList && classList.forEach(function(className){
+				var el = document.getElementsByClassName(className);
+                for(var i=0; i<=el.length-1; i++){
+					list.push(el[i]);
+					}
+            })
+            list.forEach(function(node){
+             _this.classListChanger(node,addClass,removeClass);
+            })
         
     }
 
