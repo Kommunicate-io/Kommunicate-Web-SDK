742d637b0a93cd4d510bc59bc59ee143e104d82b
rudranand <k.anand2189@gmail.com>
2019-01-15 17:57:55 +0530

    add user service test case

--- server/test/users/userService.test.js
+++ server/test/users/userService.test.js
@@ -1,16 +1,32 @@
 const expect = require('chai').expect;
-const app = require('../../app');
-const nock = require('nock');
 const userService = require('../../src/users/userService');
 const {user} = require('./user');
 
-describe('Get applozic User', () => {
+describe('should return user by userName', () => {
   it('Get a user by username', () => {
     return userService.getUserByName(user.userName)
       .then(response => {
         expect(typeof response).to.equal('object');
-        expect(response.userName).to.equal('anand@applozic.com')
+        expect(response.userName).to.equal(user.userName)
 
       });
   });
+  
+  it('should return admin user', ()=>{
+    return userService.getAdminUserByAppId(user.applicationId)
+    .then(result=>{
+        expect(typeof result).to.equal('object');
+        expect(result.type).to.equal(3)
+    })
+  })
+
+  it('should return application user', ()=>{
+    return userService.getByUserNameAndAppId(user.userName, user.applicationId)
+    .then(result=>{
+        expect(typeof result).to.equal('object');
+        expect(result.userName).to.equal(user.userName)
+        expect(result.applicationId).to.equal(user.applicationId)
+    })
+
+  })
 });
