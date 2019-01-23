f2c58fb2068fd51e24c76f29a5a2af027af20b32
Abhinav Bhardwaj <ab14bhardwaj@gmail.com>
2019-01-23 17:34:33 +0530

    Delete cookies and hide iframe when someone call logout function in widget
    1. Changed cookie name as follows :
    kommunicate-id -> km_id
    userName -> km_user_name
    
    2. Delete cookies and hide iframe when someone calls logout function.
    
    3. Change quick reply section redirect link.

--- server/src/webplugin/js/app/mck-sidebox-1.0.js
+++ server/src/webplugin/js/app/mck-sidebox-1.0.js
@@ -828,8 +828,11 @@ var MCK_MAINTAIN_ACTIVE_CONVERSATION_STATE;
                 ALStorage.clearMckContactNameArray();
                 ALStorage.clearAppHeaders();
                 $applozic.fn.applozic("reset", appOptions);
+                KommunicateUtils.deleteCookie('km_user_name');
+                KommunicateUtils.deleteCookie('km_id');
                 $applozic("#mck-sidebox").hide();
                 $applozic("#mck-sidebox-launcher").hide();
+                parent.document.getElementById("kommunicate-widget-iframe") && (parent.document.getElementById("kommunicate-widget-iframe").style.display = "none")
             }
             IS_LOGGED_IN = false;
         };
