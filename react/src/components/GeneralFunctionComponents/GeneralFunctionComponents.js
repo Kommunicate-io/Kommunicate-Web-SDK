d0bf5c4648bc73185ec9800721ebae74a24227e9
jithinbalu <jithin@kommunicate.io>
2019-02-05 15:04:11 +0530

    remove unused code

--- react/src/components/GeneralFunctionComponents/GeneralFunctionComponents.js
+++ react/src/components/GeneralFunctionComponents/GeneralFunctionComponents.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
 import {BlockButtonContainer, BlockButtonTextWrapper, BlockButtonArrowWrapper} from './GeneralFunctionComponentsStyle'
 import {RightArrow} from '../../../src/assets/svg/svgs'
 
