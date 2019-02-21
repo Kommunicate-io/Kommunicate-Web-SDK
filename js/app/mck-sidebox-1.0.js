d03c4c7e1426221c8b8768ee753ebdb427bde715
Kusum Dhoundiyal <kusum.dhoundiyal96@gmail.com>
2019-02-21 19:19:01 +0530

    #KM-2040 replace magic number with constant

--- js/app/mck-sidebox-1.0.js
+++ js/app/mck-sidebox-1.0.js
@@ -6000,7 +6000,7 @@ var MCK_MAINTAIN_ACTIVE_CONVERSATION_STATE;
                         }
                         if (message.contentType === KommunicateConstants.MESSAGE_CONTENT_TYPE.LOCATION) {
                             emoji_template = '<span class="mck-icon--location"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="rgba(38,50,56,.52)"/><path d="M0 0h24v24H0z" fill="none"/></svg></span><span>Location</span>';
-                        } else if(message.contentType === KommunicateConstants.MESSAGE_CONTENT_TYPE.TEXT_HTML && message.source === 7) {
+                        } else if(message.contentType === KommunicateConstants.MESSAGE_CONTENT_TYPE.TEXT_HTML && message.source === KommunicateConstants.MESSAGE_SOURCE.MAIL_INTERCEPTOR ) {
                             var s = message.message;
                             var result = s.match(/<b>(.*?)<\/b>/g).map(function(val){
                                 return val.replace(/<\/?b>/g,'');
