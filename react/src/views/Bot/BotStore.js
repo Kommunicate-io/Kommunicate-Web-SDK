e405196becd1fba6a8a7c8a2a2fe9e3ce584113b
Akshat Dhabi <34703178+akshatdhabi@users.noreply.github.com>
2019-02-28 20:41:42 +0530

    fix for enabling of multiple bots

--- react/src/views/Bot/BotStore.js
+++ react/src/views/Bot/BotStore.js
@@ -234,7 +234,8 @@ class BotStore extends Component {
             this.state.listOfIntegratedBots.map(bot => {
               if(bot.allConversations == 1){
                 this.setState({
-                  conversationsAssignedToBot: bot.name
+                  conversationsAssignedToBot: bot.name,
+                  conversationsAssignedToBotId: bot.userName
                 })
               }
             })
@@ -596,7 +597,7 @@ class BotStore extends Component {
         if (this.state.botRoutingEnabled == 0) {
           this.enableBotRouting();
         }
-        conversationHandlingByBot(this.state.conversationsAssignedToBot, 0)
+        conversationHandlingByBot(this.state.conversationsAssignedToBotId, 0)
         conversationHandlingByBot(this.state.latestIntegratedBotId, 1).then(response => {
           if (response.data.code === "success") {
             Notification.info('Conversations assigned to ' + this.state.latestIntegratedBotName);
