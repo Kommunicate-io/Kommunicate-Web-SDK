78945a0fc21ac1865667c93d0cd53ba8d98f48c0
Abhinav Bhardwaj <32258004+ab14bhardwaj@users.noreply.github.com>
2019-03-06 17:47:30 +0530

    Trim search query before making an API call. (#3586)
    1. Trim search query before making an API call.
    2. Remove setState outside of if loop as it is necessary in every case.

--- react/src/views/Users/Users.js
+++ react/src/views/Users/Users.js
@@ -161,13 +161,13 @@ class Users extends Component {
       key: searchQuery
     }
     ApplozicClient.searchContact(params).then(response => {
+      if (_this.state.isSearchBoxActive) {
+        _this.setState({
+          oldResult: _this.state.result
+        })
+      };
       if (response.response.length !== 0) {
         var setPageNumbers = response.response.length;
-        if (_this.state.isSearchBoxActive) {
-          _this.setState({
-            oldResult: _this.state.result
-          })
-        };
         _this.setState({
           isFromSearch: true,
           result: [],
@@ -316,7 +316,7 @@ class Users extends Component {
     var key = event.which || event.keyCode;
     var kmSearchBoxValue = document.getElementById('km-search-box').value;
     if (key === 13 && kmSearchBoxValue.length !== 0) {
-      _this.searchContactInApplozic(kmSearchBoxValue);
+      _this.searchContactInApplozic(kmSearchBoxValue.trim());
       _this.setState({
         searchBoxEmpty: false,
         checkEnter:true
