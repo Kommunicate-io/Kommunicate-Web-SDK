fc05f8f59e1415a5137f009ff52a905ab63c7cc6
Anand <anand@applozic.com>
2019-03-08 14:17:37 +0530

    add elastic search support  (#3611)
    * faq search by word
    
    * add more field to sync with elastic search
    
    * search raw query
    
    * add elastic search support
    
    * update code
    
    * change query
    
    * remove mongoostatic from code base and add npm package
    
    * remove comments
    
    * esClient url changed for prod config

---
+++ server/src/autosuggest/autoSuggestService2.js
@@ -0,0 +1,162 @@
+const logger = require("../utils/logger");
+const { KnowledgeBaseModel } = require('./knowledgeBase')
+const crypto = require('crypto');
+const stringUtils = require("underscore.string");
+const knowledgeBaseESClient = require("./faqSearchService");
+const { getNextCount } = require('./counter');
+
+
+const generateHash = (message) => {
+    if (stringUtils.isBlank(message)) { return null; }
+    message = message.trim()
+    message = message.replace(/\?/g, '');
+    return crypto.createHash('md5').update(message).digest('hex');
+}
+
+const getAllSuggestions = async () => {
+    let arr = await KnowledgeBaseModel.find();
+    return arr;
+}
+
+const getSuggestionsByAppId = (applicationId, type) => {
+    let criteria = { applicationId: applicationId, deleted: false, status: { '$nin': ['un_answered'] } }
+    if (type) {
+        criteria.type = type
+    }
+    return KnowledgeBaseModel.find(criteria);
+}
+const getSuggestionsByCriteria = (criteria, value, applicationId) => {
+    let criteriaObj = { deleted: false, status: { '$nin': ['un_answered'] } };
+    criteriaObj[criteria] = value;
+    if (criteria == 'id') {
+        criteriaObj[criteria] = parseInt(value, 10);
+    }
+    criteriaObj.applicationId = applicationId;
+    return KnowledgeBaseModel.find(criteriaObj);
+}
+
+const createSuggestion = async (suggestion) => {
+    suggestion.id = await getNextCount(KnowledgeBaseModel.modelName, "id")
+    return KnowledgeBaseModel.create(suggestion).then(result => {
+        return result;
+    }).catch(error => {
+        logger.error("error while creating auto suggestion/faq", error);
+        throw error;
+    });
+}
+
+const updateSuggestion = (suggestion) => {
+    return KnowledgeBaseModel.updateOne({ "id": suggestion.id }, suggestion).then(result => {
+        return result;
+    })
+}
+const deleteSuggestion = (suggestion) => {
+    return KnowledgeBaseModel.updateOne({ "id": suggestion.id }, { 'deleted': true }).then(result => {
+        return result;
+    })
+}
+
+const searchFAQ = async (options) => {
+    var data = [];
+    let criteria = {
+        type: "faq",
+        status: "published",
+        applicationId: options.appId,
+        deleted: false
+    }
+    if (options.text) {
+        data = await searchQuery(options);
+    } else {
+        if (options.id) {
+            criteria.id = parseInt(options.id);
+        }
+        if (options.referenceId) {
+            criteria.referenceId = parseInt(options.referenceId);
+            criteria.type = "learning";
+        }
+        if (options.key) {
+            criteria.key = options.key;
+        }
+        data = KnowledgeBaseModel.find(criteria).select({ name: 1, content: 1, referenceId: 1, id: 1, _id: 0 });
+    } 
+    for (var i = 0; i < data.length; i += 1) {
+        var knowledge = data[i];
+        if (knowledge.referenceId != null && (knowledge.content == null || knowledge.content == "")) {
+            var result = KnowledgeBaseModel.find({
+                id: knowledge.referenceId,
+                deleted: false,
+                status: { '$nin': ['un_answered'] }
+            }).select({ name: 1, content: 1, referenceId: 1, id: 1, _id: 0 });
+            data[i].content = result[0].content;
+        }
+    }
+
+    return data;
+}
+/**
+ * @param {Number} pageNumber 
+ * @param {Number} pageSize 
+ * @param {Object} criteria 
+ * pagination API
+ */
+const fetchFAQs = (pageNumber, pageSize, criteria) => {
+    return KnowledgeBaseModel.find(criteria)
+        .skip((pageNumber - 1) * pageSize)
+        .size(pageSize)
+        .sort({ id: 1 })
+        .catch(error => {
+            throw error;
+        });
+}
+
+/**
+ * @param {*} options 
+ * search text into content and name
+ */
+const searchQuery = (options) => {
+    return knowledgeBaseESClient.searchRawQuery({
+        "query": {
+            "bool": {
+                "must": {
+                    "match": {
+                        "content": options.text
+                    }
+                },
+                "filter": {
+                    "bool": {
+                        "must": [
+                            { "term": { "applicationId.keyword": options.appId } },
+                            { "term": { "type.keyword": "faq" } },
+                            { "term": { "deleted.keyword": false } },
+                            { "term": { "status.keyword": "published" } },
+                        ]
+                    }
+                }
+            }
+        }
+    }).catch(error => {
+        throw error;
+    });
+}
+/**
+ * search raw query into esClient
+ */
+const searchESQueryByCriteria = (query)=>{
+    return knowledgeBaseESClient.searchRawQuery(query).catch(error=>{
+        throw error;
+    });
+}
+
+
+module.exports = {
+    getAllSuggestions,
+    createSuggestion,
+    updateSuggestion,
+    deleteSuggestion,
+    searchFAQ,
+    fetchFAQs,
+    getSuggestionsByCriteria,
+    getSuggestionsByAppId,
+    generateHash,
+    searchESQueryByCriteria
+}
---
+++ server/src/autosuggest/knowledgeBase.js
@@ -0,0 +1,137 @@
+const mongoose = require("mongoose");
+const mongoosastic = require("mongoosastic");
+const stringUtils = require("underscore.string");
+const Schema = mongoose.Schema;
+const {COLLECTIONS} = require("../mongodb/collections");
+const config = require('../../conf/config').getProperties();
+var crypto = require('crypto');
+mongoose.pluralize(null);
+
+const KnowledgeBase = new Schema({
+    id:{
+        type:Number,
+        unique:true
+    },
+    status: {
+        type: String, 
+        lowercase: true, 
+        es_type:'text',
+        es_indexed: true,
+        es_index_analyzer:'keyword'
+    },
+    type: { 
+        type: String, 
+        lowercase: true, 
+        es_type:'text',
+        es_indexed: true,
+        es_index_analyzer:'keyword'
+    },
+    category: { 
+        type: String, 
+        lowercase: true, 
+        es_type:'text',
+        es_indexed: true,
+        es_index_analyzer:'keyword' 
+    },
+    referenceId: { 
+        type: Number, 
+        es_type:'integer',
+        es_indexed: true,
+        es_index_analyzer:'keyword'
+    },
+    applicationId: { 
+        type: String, 
+        es_type:'text', 
+        es_indexed: true,
+        es_index_analyzer:'keyword'
+    },
+    userName: { 
+        type: String 
+    },
+    key: { 
+        type: String, 
+        es_type:'text'
+    },
+    name: { 
+        type: String, 
+        es_type:'text', 
+        es_indexed: true 
+    },
+    content: { 
+        type: String, 
+        es_type:'text', 
+        es_indexed: true 
+    },
+    deleted: { 
+        type: String, 
+        default: false,
+        es_indexed: true,
+        es_index_analyzer:'keyword'
+    },
+    created_at: { 
+        type: Number, 
+        default: new Date().getTime(), 
+        es_type:'date'
+    },
+    updated_at: { 
+        type: Number, 
+        default: new Date().getTime(), 
+        es_type:'date' 
+    }
+});
+KnowledgeBase.pre('save', function (next) {
+    let question = this.name ? this.name.trim() : null;
+    if (!stringUtils.isBlank(question)) {
+        question = question.replace(/\?/g, '');
+        this.key = crypto.createHash('md5').update(question).digest('hex');
+    }
+    next();
+});
+KnowledgeBase.pre('updateOne', function (next) {
+    this._update.updated_at = new Date().getTime();
+    let question = this.name ? this.name.trim() : null;
+    if (!stringUtils.isBlank(question)) {
+        question = question.replace(/\?/g, '');
+        this._update.key = crypto.createHash('md5').update(question).digest('hex');
+    }
+    next();
+});
+
+KnowledgeBase.methods.toJSON = function () {
+    var obj = this.toObject();
+    delete obj.key;
+    delete obj._id;
+    return obj;
+}
+KnowledgeBase.plugin(mongoosastic, {
+    hosts: [config.esClientUrl],
+    index: COLLECTIONS.KNOWLEDGE_BASE,
+    type:"_doc"
+});
+
+const KnowledgeBaseModel = mongoose.model(COLLECTIONS.KNOWLEDGE_BASE, KnowledgeBase);
+var stream = KnowledgeBaseModel.synchronize()
+var count = 0;
+
+stream.on('data', function (err, doc) {
+    count++;
+});
+stream.on('close', function () {
+    console.log('indexed ' + count + ' documents!');
+});
+stream.on('error', function (err) {
+    console.log("migration error", err);
+});
+
+KnowledgeBaseModel.createMapping(function (err, mapping) {
+    if (err) {
+        console.log('error creating mapping (you can safely ignore this)');
+        console.log(err);
+    } else {
+        console.log('mapping created!');
+        console.log(mapping);
+    }
+});
+
+
+exports.KnowledgeBaseModel = KnowledgeBaseModel;
