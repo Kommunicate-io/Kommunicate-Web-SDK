51c4670c8468685bfd59dd7c644662cdec18aaf5
Vipin Pandey <vipinsonupandey@gmail.com>
2019-03-11 13:19:08 +0530

    added test case for gatting preference (#3578)

---
+++ server/test/preference/mockPreference.js
@@ -0,0 +1,7 @@
+module.exports.preference = { 
+        "applicationId": "2fb9b50f364b8eadfbf2f17032ce313b2",
+        "userName" : "vipin+testkm3001209@applozic.com",
+        "preferences": {
+                "timeZone": "Asia/Hong_Kong_Test"}
+       
+        };
