c73f9f58d7e1447b33e95b80f25ed27774f13251
Abhinav Bhardwaj <ab14bhardwaj@gmail.com>
2019-03-22 16:35:43 +0530

    Add Search icon to common svg file.

--- react/src/components/Aside/Aside.js
+++ react/src/components/Aside/Aside.js
@@ -29,6 +29,7 @@ import {integration_type} from '../../views/Integrations/ThirdPartyList';
 import ResolutionDropdown from './ResolutionDropdown';
 import CloseButton from '../Modal/CloseButton';
 import { default as DeleteModal } from 'react-modal';
+import { SearchBarIcon } from "../../assets/svg/svgs";
 
 const userDetailMap = {
   "displayName": "km-sidebar-display-name",
@@ -910,9 +911,7 @@ class Aside extends Component {
                               <span id="km-clear-search-text" className=" km-clear-search-text n-vis"> × </span>
                               <input type="text" id="km-search" className= "km-search-icon-properties" placeholder="Search..." autoFocus />
                               <span className="km-search-icon km-conversation-search-icon"> 
-                                <svg id = "km-conversation-search-icon-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966">
-                                  <path fill="#AAAAAA" d="M55.146 51.887L41.588 37.786A22.926 22.926 0 0 0 46.984 23c0-12.682-10.318-23-23-23s-23 10.318-23 23 10.318 23 23 23c4.761 0 9.298-1.436 13.177-4.162l13.661 14.208c.571.593 1.339.92 2.162.92.779 0 1.518-.297 2.079-.837a3.004 3.004 0 0 0 .083-4.242zM23.984 6c9.374 0 17 7.626 17 17s-7.626 17-17 17-17-7.626-17-17 7.626-17 17-17z"/>
-                                </svg>
+                                <SearchBarIcon id = "km-conversation-search-icon-svg"/>
                               </span> 
                               </div>
                             </div>
