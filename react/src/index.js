ac8e80260c7ee37234b9998136a2052072080997
devashish <devashish.mamgain@gmail.com>
2019-03-31 16:45:25 +0530

    KM-2209 lazy loading of components in full container

--- react/src/index.js
+++ react/src/index.js
@@ -1,13 +1,9 @@
 import 'core-js';
 import ReactDOM from 'react-dom'
-import { render } from 'react-dom'
-import React, { Fragment } from 'react'
+import React from 'react'
 import App from './components/App';
 import {BrowserRouter} from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './reducers'
-import logger from 'redux-logger'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from './store/store';
 import * as Sentry from '@sentry/browser'
