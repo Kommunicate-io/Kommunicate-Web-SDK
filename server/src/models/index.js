7a6ad39c894506e6f7eda8ee6c3f273b2b83c4e4
rudranand <k.anand2189@gmail.com>
2019-04-12 01:46:05 +0530

    remove string operator deprecated warning

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,8 +1,43 @@
 const Sequelize = require("sequelize");
 const config = require("../../conf/config.js");
-const path = require("path");
-var winston = require('winston');
+const Op = Sequelize.Op;
 let options = config.getProperties().db.options;
+options.operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+};
 var sequelize = new Sequelize(config.getProperties().db.url, config.getProperties().db.options);
 
 sequelize.authenticate().then(function () {
