e7a18f82f7db8263d568e5573b3a68f1cfcbebe4
jithinbalu <jithin@kommunicate.io>
2019-04-25 16:13:57 +0530

    user authentication service code improvement

--- server/src/userAuthentication/userAuthenticationService.js
+++ server/src/userAuthentication/userAuthenticationService.js
@@ -1,13 +1,5 @@
-const UserAuthenticationModel = require("../models").UserAuthentication
-exports.createUserAuthentication = async (data,transaction) => {
-    let authentication = await UserAuthenticationModel.find({ where: { userName: data.userName } });
-    if (authentication) { 
-        return { message: "RECORD EXIST FOR THIS USER NAME"};
-    }
-    return Promise.resolve(UserAuthenticationModel.create(data, transaction)).then(res => {    
-        return { message: "user credential inserted" , response: res};
-    }).catch(err => {
-        logger.error("error while inserting authentication details", err)
-        throw err;
-    });
+const UserAuthenticationModel = require("../models").UserAuthentication;
+const logger = require('../utils/logger');
+exports.createUserAuthentication = (data,transaction) => {
+    return Promise.resolve(UserAuthenticationModel.create(data, transaction))
 }
