6637e3ce3aea653b813570e1ff41da79f13f6405
jithinbalu <jithin@kommunicate.io>
2019-04-25 16:15:28 +0530

    remove logger

--- server/src/userAuthentication/userAuthenticationService.js
+++ server/src/userAuthentication/userAuthenticationService.js
@@ -1,5 +1,4 @@
 const UserAuthenticationModel = require("../models").UserAuthentication;
-const logger = require('../utils/logger');
 exports.createUserAuthentication = (data,transaction) => {
     return Promise.resolve(UserAuthenticationModel.create(data, transaction))
 }
