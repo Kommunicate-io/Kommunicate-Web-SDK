b5b976c271ebd8b4a8b6339c03e8a630486341dc
Abhinav Bhardwaj <ab14bhardwaj@gmail.com>
2019-04-29 19:16:51 +0530

    KM-2190 Merge plugin.js and kommunicateIframe.js
    1. Merge plugin.js and kommunicateIframe.js
    2. Forward all request internally in router to get old version reference.

--- plugin.js
+++ plugin.js
@@ -1,43 +1,155 @@
-window.applozic = window.kommunicate || {};
-//var MCK_CONTEXTPATH = "${pageContext.request.scheme}://${pageContext.request.serverName}:${pageContext.request.serverPort}${contextPath}";
-//var MCK_STATICPATH = "${pluginStaticPath}";
 var MCK_CONTEXTPATH = ":MCK_CONTEXTPATH";
 var MCK_STATICPATH =":MCK_STATICPATH";
 var MCK_ONINIT = "";
 var KM_PLUGIN_SETTINGS=JSON.parse(':PLUGIN_SETTINGS');
 var MCK_PLUGIN_VERSION = ":MCK_PLUGIN_VERSION";
 var MCK_THIRD_PARTY_INTEGRATION =JSON.parse(':MCK_THIRD_PARTY_INTEGRATION');
-//window.applozic.PRODUCT_ID= "${productId}"?"${productId}":"applozic-chat";
-window.applozic.PRODUCT_ID =":PRODUCT_ID";
-// $.getScript(MCK_STATICPATH + '/sidebox/js/app/mck-app.js');
-var options={};
-var options = applozic._globals;
-options.isAnonymousChat =options.isAnonymousChat;
-options.KM_VER = MCK_PLUGIN_VERSION; 
-if (typeof options !== 'undefined') {
-  MCK_ONINIT = options.onInit;
-}
-window.addEventListener('error', function(e) {
-  let sentryConfig = MCK_THIRD_PARTY_INTEGRATION.sentry.plugin;
-  sentryConfig.enable && typeof Sentry != "undefined" && Sentry.withScope(function (scope) {
-    scope.setTag("applicationId", options.appId);
-    scope.setTag("userId", options.userId);
-    scope.setUser({
-      id: options.appId
-    });
-    Sentry.captureException(e);
-  });
-  if (typeof(e.target.src) !== 'undefined' && e.target.src.indexOf('sidebox') !== -1 && typeof MCK_ONINIT === 'function') {
-    console.log("Plugin loading error. Refresh page.");
-    MCK_ONINIT("error");
-  }
-}, true);
-var imported = document.createElement('script');
-imported.src = MCK_APP_JS;
-imported.crossOrigin = "anonymous";
-document.head.appendChild(imported);
-addFullviewImageModal();
+var PRODUCT_ID =":PRODUCT_ID";
 
+
+// iframe class
+var kmCustomIframe =   
+    '.kommunicate-custom-iframe { ' +
+    '   max-height: calc(100% - 30px)!important;' +
+    '   border: none;' +
+    '   position: fixed;' +
+    '   z-index: 2243000;' +
+    '   bottom: 10px;' +
+    '   right: 20px;' +
+    '   height: 75px;' +
+    '   width: 75px;' +
+    '} \n ' +
+    '@media only screen and (max-width:600px) { .kommunicate-iframe-enable-media-query {' +
+    '   right: 0px;' +
+    '   bottom: 0px;' +
+    '   top: 0;' +
+    '   left: 0;' +
+    '   border-radius: 0px;' +
+    '   height: 100% !important;' +
+    '   width: 100% !important;' +
+    '   max-height: 100% !important;' +
+    '} } \n' + 
+    '.km-iframe-closed{ '+
+    '    height: 75px; '+
+    '    width:  75px; '+
+    '    box-shadow: none!important; '+
+    '}\n' + 
+    '.km-iframe-notification{ '+
+    '    height:100px; '+ 
+    '} \n '+
+    '.km-iframe-dimension-no-popup{' +
+    '    height: 600px;' +
+    '    width: 390px; ' +
+    '    box-shadow: 0 1.5rem 2rem rgba(0,0,0,.3)' +
+    '} \n ' +
+    '.km-iframe-dimension-with-popup{ '+
+    '    height: 700px; '+
+    '    width: 442px; '+
+    '} \n '
+
+isV1Script() ? addKommunicatePluginToIframe() : appendIframe();
+
+function appendIframe () {
+    createKommunicateIframe();
+    createCustomClasses(kmCustomIframe); // Add class to document
+};
+
+function isV1Script(){
+    return MCK_PLUGIN_VERSION === "v1";
+};
+
+function createCustomClasses(classSettings) {
+    // Create custom classes 
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = classSettings;
+    document.getElementsByTagName('head')[0].appendChild(style);
+};
+
+// Create element iframe for kommunicate widget
+function createKommunicateIframe () {
+  var kommunicateIframe = document.createElement("iframe");
+  kommunicateIframe.setAttribute("style", "overflow:hidden;"); // to fix scrollbars appearing before the chat widget loads on slow connections
+  kommunicateIframe.setAttribute("scrolling", "no"); // to fix scrollbars appearing before the chat widget loads on slow connections
+  kommunicateIframe.setAttribute("id", "kommunicate-widget-iframe");
+  kommunicateIframe.setAttribute("class", "kommunicate-custom-iframe");
+  kommunicateIframe.setAttribute('data-protocol', window.location.protocol);
+  kommunicateIframe.setAttribute('data-url', window.location.href);
+  document.body.appendChild(kommunicateIframe);
+  var iframeDocument = kommunicateIframe.contentDocument || kommunicateIframe.contentWindow.document;
+  kommunicateIframe.contentWindow.kommunicate = window.kommunicate;
+
+  if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+      // Do Firefox-related activities
+      var testClick = window.document.getElementById("kommunicate-widget-iframe");
+      testClick.onload = function () {
+          addKommunicatePluginToIframe();
+      };
+  } else {
+      window.setTimeout(function () {
+          addKommunicatePluginToIframe();
+      }, 500);
+  }
+};
+
+function addKommunicatePluginToIframe() {
+    // Add kommunicate plugin inside iframe
+    var addableWindow, addableDocument;
+    if (isV1Script()){
+        addableWindow = window;
+        addableDocument = document;
+    }
+    else {
+        var kommunicateIframe = window.document.getElementById("kommunicate-widget-iframe");
+        var iframeDocument = kommunicateIframe.contentDocument || kommunicateIframe.contentWindow.document;
+        addableWindow = kommunicateIframe.contentWindow;
+        addableDocument = iframeDocument ;
+    }
+
+    addableWindow.applozic = (isV1Script() ? addableWindow.kommunicate : kommunicateIframe.contentWindow.kommunicate) || {};
+    addableWindow.MCK_CONTEXTPATH = MCK_CONTEXTPATH;
+    addableWindow.MCK_STATICPATH =MCK_STATICPATH;
+    addableWindow.MCK_ONINIT = "";
+    addableWindow.KM_PLUGIN_SETTINGS=KM_PLUGIN_SETTINGS;
+    addableWindow.MCK_PLUGIN_VERSION = MCK_PLUGIN_VERSION;
+    addableWindow.MCK_THIRD_PARTY_INTEGRATION =MCK_THIRD_PARTY_INTEGRATION;
+    addableWindow.applozic.PRODUCT_ID = PRODUCT_ID;
+    var options={};
+    var options = addableWindow.applozic._globals;
+    options.isAnonymousChat =options.isAnonymousChat;
+    options.KM_VER = MCK_PLUGIN_VERSION; 
+    if (typeof options !== 'undefined') {
+        addableWindow.MCK_ONINIT = options.onInit;
+    }
+    addableWindow.addEventListener('error', function(e) {
+    let sentryConfig = MCK_THIRD_PARTY_INTEGRATION.sentry.plugin;
+    sentryConfig.enable && typeof Sentry != "undefined" && Sentry.withScope(function (scope) {
+        scope.setTag("applicationId", options.appId);
+        scope.setTag("userId", options.userId);
+        scope.setUser({
+        id: options.appId
+        });
+        Sentry.captureException(e);
+    });
+    if (typeof(e.target.src) !== 'undefined' && e.target.src.indexOf('sidebox') !== -1 && typeof MCK_ONINIT === 'function') {
+        console.log("Plugin loading error. Refresh page.");
+        MCK_ONINIT("error");
+    }
+    }, true);
+    var imported = addableDocument.createElement('script');
+    imported.src = MCK_APP_JS;
+    imported.crossOrigin = "anonymous";
+    addableDocument.head.appendChild(imported);
+    addFullviewImageModal();
+};
+
+/*
+                            <!-- ======================================= -->
+                            <!-- |       New image full view modal     | -->
+                            <!-- ======================================= -->
+*/
+
+//<----------------------------CODE BEGINS HERE---------------------------->
 function addFullviewImageModal () {
   var modalHtml =
     '<span id="km-fullscreen-image-modal-close" class="km-fullscreen-image-modal-close">&times;</span>' +
@@ -135,4 +247,5 @@ function addFullviewImageModal () {
       style.innerHTML = addFullviewImageModalCss;
       parent.document.getElementsByTagName('head')[0].appendChild(style);
 
-}
+};
+//<----------------------------CODE ENDS HERE---------------------------->
