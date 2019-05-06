07934baed07338545428f443a9149c73a4b453ba
rudranand <k.anand2189@gmail.com>
2019-05-06 12:49:08 +0530

    remove unnecessary code

--- server/src/autosuggest/hashGenerator.js
+++ server/src/autosuggest/hashGenerator.js
@@ -1,34 +1,16 @@
-const autoSuggestionService = require('./autosuggestService');
-var crypto = require('crypto');
+var crypto = require("crypto");
 const stringUtils = require("underscore.string");
 
+const generateHash = message => {
+    if (stringUtils.isBlank(message)) {
+        return null;
+    }
+    message = message.trim();
+    message = message.replace(/[\W_]+/g, "");
+    return crypto
+        .createHash("md5")
+        .update(message)
+        .digest("hex");
+};
 
-const generateHash = (message) => {
-	if (stringUtils.isBlank(message)) { return null; }
-	message = message.trim()
-	message = message.replace(/[\W_]+/g, '');
-	return crypto.createHash('md5').update(message).digest('hex');
-}
-
-/**
- * will remove this script
- * its only for generating hash for existing faqs
- */
-const generateHashForExistingRecords = () => {
-    autoSuggestionService.getAllSuggestions().then(suggestions => {
-        suggestions.map(async suggestion => {
-            let question = suggestion.name ? suggestion.name.trim() : null;
-            if (!stringUtils.isBlank(question)) {
-                suggestion.key = generateHash(question);
-                await autoSuggestionService.updateSuggestion(suggestion);
-            }
-        })
-    }).catch(err => {
-        console.log(" error on hash generation", err)
-    });
-}
-
-//generateHashForExistingRecords();
-
-exports.generateHash = generateHash;
-exports.generateHashForExistingRecords = generateHashForExistingRecords;
+module.exports = { generateHash };
