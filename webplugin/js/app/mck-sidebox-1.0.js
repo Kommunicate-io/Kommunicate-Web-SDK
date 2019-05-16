0474670d4b93906fb2c12fc7f4499b744a673855
Akshatdhabi <akshat.dhabi@gmail.com>
2019-05-16 18:01:13 +0530

    fix undefined error in CSAT rating

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -1796,7 +1796,7 @@ var CURRENT_GROUP_DATA={};
                 })
 
                 sendFeedbackComment.addEventListener('click',function(){
-                    document.getElementById("mck-feedback-comment").trim() && (feedbackObject.comments = [document.getElementById("mck-feedback-comment").value]);
+                    document.getElementById("mck-feedback-comment") && document.getElementById("mck-feedback-comment").value.trim() && (feedbackObject.comments = [document.getElementById("mck-feedback-comment").value]);
                     feedbackObject.rating = CURRENT_GROUP_DATA.currentGroupFeedback.rating;
                     feedbackObject.groupId = CURRENT_GROUP_DATA.tabId;
                     feedbackObject.comments && _this.sendFeedback(feedbackObject);
