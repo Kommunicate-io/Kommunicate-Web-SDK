c36e541bdd9a5f66dcb072789d50f46f83372fe4
Vibhor Sharma <vibhor.sharma94@gmail.com>
2019-09-19 17:49:55 +0530

    CO-775 Delay in Messages from Bot will now comw from appSettings API

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -455,7 +455,7 @@ var MCK_CHAT_POPUP_TEMPLATE_TIMER;
             10 : MCK_LABELS['emoji.hover.text'].great
         }; 
         var MCK_BOT_MESSAGE_QUEUE = [];
-        var MCK_BOT_MESSAGE_DELAY = (typeof appOptions.botMessageDelay === "number") ? appOptions.botMessageDelay : 0;
+        var MCK_BOT_MESSAGE_DELAY = WIDGET_SETTINGS && WIDGET_SETTINGS.botMessageDelayInterval ? WIDGET_SETTINGS.botMessageDelayInterval : 0;
 
         _this.toggleMediaOptions = function(){
             var mckTypingBox = document.getElementById("mck-text-box");
