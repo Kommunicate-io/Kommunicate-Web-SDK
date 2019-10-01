9ca1dd6414d0cfad5a649af10f7c93560f69f550
Vibhor Sharma <vibhor.sharma94@gmail.com>
2019-10-01 13:57:15 +0530

    CO-801 Avoiding XSS attack in pre-chat popup message in chat widget

--- webplugin/plugin.js
+++ webplugin/plugin.js
@@ -64,12 +64,12 @@ var kmCustomIframe =
         'width:100vw;'+
     '} \n' + 
     '.kommunicate-custom-iframe.chat-popup-widget-horizontal { ' + 
-    '   width: 410px;' + 
+    '   width: 421px;' + 
     '   height: 80px;' + 
     '} \n' + 
     '.kommunicate-custom-iframe.chat-popup-widget-vertical { ' + 
     '   width: 390px;' + 
-    '   height: 120px;' + 
+    '   height: 153px;' + 
     '} \n' + 
     '@media only screen and (max-width: 420px) { ' + 
       '.kommunicate-custom-iframe.chat-popup-widget-vertical { ' + 
