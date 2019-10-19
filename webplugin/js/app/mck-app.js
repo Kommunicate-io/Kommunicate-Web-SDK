4fccbd052bb20659097d3ab81c0e45c058546ffe
Devashish Mamgain <devashish@Devashishs-MacBook-Air.local>
2019-10-19 14:41:40 +0530

    FW-399 update applozic plugin to 5.8 and merge changes to support encryption and jwt token

--- webplugin/js/app/mck-app.js
+++ webplugin/js/app/mck-app.js
@@ -26,8 +26,8 @@ function ApplozicSidebox() {
     var mck_external_scripts = [
         {
             "name": "applozic-min-js",
-            "url": "https://cdn.applozic.com/applozic/applozic.chat-5.7.min.js", // update the url with every new release of applozic-web-plugin
-            "alternateUrl": MCK_STATICPATH + "/js/app/applozic.chat-5.6.min.js"
+            "url": "https://cdn.applozic.com/applozic/applozic.chat-5.8.min.js", // update the url with every new release of applozic-web-plugin
+            "alternateUrl": MCK_STATICPATH + "/js/app/applozic.chat-5.7.min.js"
         }
     ];
     var mck_style_loader = [
