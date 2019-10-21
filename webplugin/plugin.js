11438e24e629379231ce43c1fe5d111b9991fcf2
Vibhor Sharma <vibhor.sharma94@gmail.com>
2019-10-21 15:08:54 +0530

    CO-852 fix close button pre-chat popup issue in mobile.

--- webplugin/plugin.js
+++ webplugin/plugin.js
@@ -71,10 +71,13 @@ var kmCustomIframe =
     '   width: 415px;' + 
     '   height: 153px;' + 
     '} \n' + 
-    '@media only screen and (max-width: 420px) { ' + 
+    '@media only screen and (max-device-width: 420px) { ' + 
       '.kommunicate-custom-iframe.chat-popup-widget-vertical { ' + 
         'width: 100%;' + 
         'height: 155px;' + 
+      '} \n' + 
+      '.kommunicate-custom-iframe.chat-popup-widget-horizontal { ' + 
+      '   width: 100%;' + 
       '} \n' + 
       '.kommunicate-custom-iframe.chat-popup-widget-container--horizontal { ' + 
         'width: 100%;' + 
