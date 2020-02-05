c639a5e2a58a7d119e3614d1cb2e3bf37125dd47
Kusum Dhoundiyal <kusum.dhoundiyal96@gmail.com>
2020-02-05 11:30:00 +0530

    #remove superAdmin dependency from create group

--- webplugin/js/app/kommunicate-client.js
+++ webplugin/js/app/kommunicate-client.js
@@ -81,7 +81,6 @@ Kommunicate.client={
             //createUrl:Kommunicate.getBaseUrl()+"/conversations/create",
             groupName: conversationDetail.groupName,
             type: conversationDetail.type,
-            admin: conversationDetail.agentId,
             users: conversationDetail.users,
             clientGroupId:conversationDetail.clientGroupId,
             isMessage: conversationDetail.isMessage,
