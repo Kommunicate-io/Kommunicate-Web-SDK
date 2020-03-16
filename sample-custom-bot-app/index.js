4d46a62b3a188a5676a1453c1cf9c2c3ab7c80c3
Hardik Parashar <hardikparashar@Hardiks-MacBook-Air.local>
2020-03-16 19:16:55 +0530

    Added a sample project for custom bot server

---
+++ sample-custom-bot-app/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = '3000';
+const app = express();
+
+app.use(bodyParser.json());
+app.listen(port, function(){
+    console.log("The server is listening to port: " +  port);
+});
+
+app.post("/",(req,res) => {
+    console.log(req.body); //request body contains the user message and other data from Kommunicate. More here : https://docs.kommunicate.io/docs/bot-custom-integration#integrating-with-custom-bot-platform
+
+    //the response to be sent back to Kommunicate 
+    //you can create this response dynamically using data from your db / custom-bot / etc..
+    //please note the format of the response. You can find more info about the format here: https://docs.kommunicate.io/docs/bot-custom-integration
+    let botResponse = [{
+        "message": "A message can be simple as a plain text" 
+    }, {
+        "message": "A message can be a rich message containing metadata",
+        "metadata": {
+        "contentType": "300",
+            "templateId": "6",
+            "payload": [{
+                "title": "Suggested Reply button 1",
+                "message": "Suggested Reply button 1",
+            }, {
+                "title": "Suggested Reply button 2",
+                "message": "Suggested Reply button 2" 
+            }]
+        }
+    }];
+    res.status(200).send(botResponse);
+})
+
+
