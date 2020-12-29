c8de044fb38f6803efbe13e836b6db2f1ce47beb
Subham Tibra <subham.tibra@kommunicate.io>
2020-12-29 12:16:41 +0530

    add try catch to avoid future errors

--- webplugin/js/app/media/media-service.js
+++ webplugin/js/app/media/media-service.js
@@ -39,43 +39,48 @@ Kommunicate.mediaService = {
                 // stop mic effect
                 Kommunicate.typingAreaService.hideMiceRecordingAnimation();
                 window.$applozic.fn.applozic('toggleMediaOptions');
-;            }
+                ;
+            }
         }
     },
     voiceOutputIncomingMessage: function (message) {
-        // get appoptions
-        var appOptions = KommunicateUtils.getDataFromKmSession("appOptions");
+        try {
+            // get appoptions
+            var appOptions = KommunicateUtils.getDataFromKmSession("appOptions");
 
-        // If the message isn't part of the UI, it's not included
-        // in voiceoutput either
-        if (!appOptions || !Kommunicate.visibleMessage(message)) return;
+            // If the message isn't part of the UI, it's not included
+            // in voiceoutput either
+            if (!Kommunicate.visibleMessage(message)) return;
 
-        // if voiceoutput is enabled and browser supports it
-        if (appOptions.voiceOutput && "speechSynthesis" in window) {
-            var textToSpeak = "";
-            if (message.hasOwnProperty("fileMeta")) {
-                textToSpeak += MCK_LABELS['voice.output'].attachment;
-                textToSpeak += message.fileMeta.name;
+            // if voiceoutput is enabled and browser supports it
+            if (appOptions.voiceOutput && "speechSynthesis" in window) {
+                var textToSpeak = "";
+                if (message.hasOwnProperty("fileMeta")) {
+                    textToSpeak += MCK_LABELS['voice.output'].attachment;
+                    textToSpeak += message.fileMeta.name;
+                }
+                else if (message.contentType == KommunicateConstants.MESSAGE_CONTENT_TYPE.LOCATION) {
+                    coord = JSON.parse(message.message);
+                    textToSpeak += MCK_LABELS['voice.output'].location.init;
+                    textToSpeak += MCK_LABELS['voice.output'].location.lat + Math.round(coord.lat * 100) / 100;
+                    textToSpeak += MCK_LABELS['voice.output'].location.lon + Math.round(coord.lon * 100) / 100;
+                }
+                else if (message.message) {
+                    textToSpeak += message.message;
+                }
+                if (textToSpeak) {
+                    var utterance = new SpeechSynthesisUtterance(textToSpeak);
+                    utterance.lang = appOptions.language || "en-US";
+                    utterance.onerror = function (event) {
+                        if (event.error !== "not-allowed") {
+                            throw new Error("Error while converting the message to voice.", event.error);
+                        }
+                    };
+                    speechSynthesis.speak(utterance);
+                }
             }
-            else if (message.contentType == KommunicateConstants.MESSAGE_CONTENT_TYPE.LOCATION) {
-                coord = JSON.parse(message.message);
-                textToSpeak += MCK_LABELS['voice.output'].location.init;
-                textToSpeak += MCK_LABELS['voice.output'].location.lat + Math.round(coord.lat * 100) / 100;
-                textToSpeak += MCK_LABELS['voice.output'].location.lon + Math.round(coord.lon * 100) / 100;
-            }
-            else if (message.message) {
-                textToSpeak += message.message;
-            }
-            if (textToSpeak) {
-                var utterance = new SpeechSynthesisUtterance(textToSpeak);
-                utterance.lang = appOptions.language || "en-US";
-                utterance.onerror = function (event) {
-                    if (event.error !== "not-allowed") {
-                    throw new Error("Error while converting the message to voice.", event.error);
-                    }
-                };
-                speechSynthesis.speak(utterance);
-            }
+        } catch (e) {
+            console.log(e);
         }
     }
 }
