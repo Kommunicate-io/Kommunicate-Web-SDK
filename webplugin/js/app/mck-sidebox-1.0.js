5d5a3eac348b19e7460df65170834b016a31c1ee
ojashwa <sojs220@gmail.com>
2021-04-05 16:55:35 +0530

    IE testing

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -2946,10 +2946,20 @@ var userOverride = {
                         preLeadCollection.options &&
                         mckMessageService.checkArray(preLeadCollection.options)
                     ) {
-                        kmChatInput = _this.createSelectFieldDropdown(
-                            preLeadCollection.options,
-                            kmChatInput
-                        );
+                        var dropDownOption = document.createElement('option');
+                dropDownOption.setAttribute('value', '');
+                dropDownOption.textContent =
+                    MCK_LABELS['lead.collection'].option +
+                    ' ' +
+                    kmChatInput
+                        .getAttribute('name')
+                        .toLowerCase()
+                        .split('-')[1];
+                        kmChatInput.appendChild(dropDownOption);
+                        // kmChatInput = _this.createSelectFieldDropdown(
+                        //     preLeadCollection.options,
+                        //     kmChatInput
+                        // );
                     } else {
                         kmChatInput.setAttribute(
                             'type',
