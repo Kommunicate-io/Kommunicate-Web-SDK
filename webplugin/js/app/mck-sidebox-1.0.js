c8575a31c23f3823b6d16c02edc0179213ea5630
Abhishek Singh <aks3555@gmail.com>
2021-06-07 17:14:50 +0530

    fix for dialogflow

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -8451,7 +8451,8 @@ var userOverride = {
                     richText &&
                     (
                         kmRichTextMarkup.indexOf('km-cta-multi-button-container') != -1 || 
-                        kmRichTextMarkup.indexOf('km-faq-list--footer_button-container') != -1 
+                        kmRichTextMarkup.indexOf('km-faq-list--footer_button-container') != -1 ||
+                        containerType.indexOf('km-cta-multi-button-container') != -1
                     ) &&
                     (   
                         kmRichTextMarkup.indexOf('<button') != -1 || 
