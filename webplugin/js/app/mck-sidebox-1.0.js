c0ad1f9cd34f530625376425f655b1d3ecb5a7ec
Ojashwa Saini <sojs220@gmail.com>
2021-06-14 18:55:26 +0530

    created functionality for Close CSAT Rating btn

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -4477,6 +4477,10 @@ var userOverride = {
                     e.preventDefault();
                     KommunicateUI.triggerCSAT();
                 };
+                document.getElementById('km-csat-close-button').onclick = function(e){
+                    e.preventDefault();
+                    KommunicateUI.showClosedConversationBanner(false);
+                }
 
                 // Voice Output Override trigger
                 document.getElementById(
