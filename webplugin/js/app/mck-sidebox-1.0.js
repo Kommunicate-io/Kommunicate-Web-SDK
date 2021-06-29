8af0732ad06eb3c113095eb7dfe929f1a4c39645
channaveerayya <channaveerayya@kommunicate.io>
2021-06-29 10:35:18 +0530

    image with text message hidden bug fixed

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -7525,7 +7525,8 @@ var userOverride = {
                 '<div class="${nameTextExpr} ${showNameExpr} mck-conversation-name"><span class="mck-ol-status ${contOlExpr}"><span class="mck-ol-icon" title="${onlineLabel}"></span>&nbsp;</span>${msgNameExpr}</div>' +
                 '<div class="blk-lg-12">' +
                 '<div class="mck-msg-avator blk-lg-3">{{html msgImgExpr}}</div>' +
-                '<div class ="km-conversation-container-right">' +
+                '<div class ="km-conversation-container-right ${kmAttchMsg}">' +
+                '<div class="km-msg-box-attachment ${attachmentBoxExpr} ">{{html attachmentTemplate}}<div class="km-msg-box-progressMeter ${progressMeterClassExpr} ">{{html progressMeter}}</div></div>' +
                 '<div class="mck-msg-box ${msgClassExpr} ${msgBoxColor}">' +
                 '<div class="move-right mck-msg-text"></div>' +
                 '<div class="mck-msg-reply mck-verticalLine ${msgReplyToVisibleExpr}">' +
@@ -7537,7 +7538,6 @@ var userOverride = {
                 '</div>' +
                 '<div class="mck-msg-text mck-msg-content notranslate" tabindex="-1"></div>' +
                 '</div>' +
-                '<div class="km-msg-box-attachment ${attachmentBoxExpr} ">{{html attachmentTemplate}}<div class="km-msg-box-progressMeter ${progressMeterClassExpr} ">{{html progressMeter}}</div></div>' +
                 '<div class="mck-msg-box-rich-text-container notranslate ${kmRichTextMarkupVisibility} ${containerType}">' +
                 '<div class="email-message-indicator ${emailMsgIndicatorExpr}"><span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11"><path fill="#BCBABA" fill-rule="nonzero" d="M12 3.64244378L7.82144281 0v2.08065889h-.0112584c-1.2252898.0458706-2.30872368.23590597-3.23022417.58877205-1.03614858.39436807-1.89047392.92952513-2.56710409 1.60169828-.53552482.53356847-.95771502 1.14100649-1.27501442 1.8173497-.08349984.17792235-.16437271.35624185-.23304899.54349718-.32987128.89954044-.56029331 1.87632619-.49311816 2.87991943C.02781163 9.76011309.1572833 10.5.30795828 10.5c0 0 .18801538-1.03695368.94795775-2.22482365.23267371-.36259621.50437656-.70533502.81698495-1.02186205l.0350887.03038182v-.06533086c.19420749-.19301397.40079923-.37828356.63497407-.54588006.63272238-.45433742 1.40748832-.8141536 2.32279668-1.0796471.74962217-.21763716 1.60432278-.34412883 2.54909064-.39019801h.20809286l-.00150112 2.08085746L12 3.64244378z"/></svg></span><span>via email</span></div>{{html kmRichTextMarkup}}</div>' +
                 '<div class="${msgFloatExpr}-muted mck-text-light mck-text-xs mck-t-xs ${timeStampExpr} vis"><span class="mck-created-at-time notranslate">${createdAtTimeExpr}</span> <span class="mck-message-status notranslate" aria-hidden="${msgStatusAriaTag}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.06103 10.90199" width="24" height="24" class="${statusIconExpr} mck-message-status notranslate" focusable="false" aria-hidden="true" ><path fill="#859479" d="M16.89436.53548l-.57-.444a.434.434 0 0 0-.609.076l-6.39 8.2a.38.38 0 0 1-.577.039l-.427-.388a.381.381 0 0 0-.578.038l-.451.576a.5.5 0 0 0 .043.645l1.575 1.51a.38.38 0 0 0 .577-.039l7.483-9.6a.436.436 0 0 0-.076-.609z" class="mck-delivery-report--delivered-read"></path><path fill="#859479" d="M12.00236.53548l-.57-.444a.434.434 0 0 0-.609.076l-6.39 8.2a.38.38 0 0 1-.577.039l-2.614-2.558a.435.435 0 0 0-.614.007l-.505.516a.435.435 0 0 0 .007.614l3.887 3.8a.38.38 0 0 0 .577-.039l7.483-9.6A.435.435 0 0 0 12.00109.536l-.00073-.00052z"  class="mck-delivery-report--sent"></path><path fill="#859479" d="M9.75 7.713H8.244V5.359a.5.5 0 0 0-.5-.5H7.65a.5.5 0 0 0-.5.5v2.947a.5.5 0 0 0 .5.5h.094l.003-.001.003.002h2a.5.5 0 0 0 .5-.5v-.094a.5.5 0 0 0-.5-.5zm0-5.263h-3.5c-1.82 0-3.3 1.48-3.3 3.3v3.5c0 1.82 1.48 3.3 3.3 3.3h3.5c1.82 0 3.3-1.48 3.3-3.3v-3.5c0-1.82-1.48-3.3-3.3-3.3zm2 6.8a2 2 0 0 1-2 2h-3.5a2 2 0 0 1-2-2v-3.5a2 2 0 0 1 2-2h3.5a2 2 0 0 1 2 2v3.5z" class="mck-delivery-report--pending"></path></svg><p class="mck-sending-failed">Sending failed</p></span></div>' +
@@ -8258,7 +8258,7 @@ var userOverride = {
                 var messageClass = 'vis';
                 var progressMeterClass = 'n-vis';
                 var attachmentBox = 'n-vis';
-
+                var kmAttchMsg= ''
                 if (!Kommunicate.visibleMessage(msg)) return;
 
                 if (
@@ -8324,9 +8324,9 @@ var userOverride = {
                     msg.contentType ==
                         KommunicateConstants.MESSAGE_CONTENT_TYPE.LOCATION
                 ) {
-                    messageClass = 'n-vis';
                     progressMeterClass = 'n-vis';
                     attachmentBox = 'vis';
+                    kmAttchMsg = 'km-attach-msg';
                 } else {
                     messageClass =
                         (msg.contentType ==
@@ -8357,10 +8357,26 @@ var userOverride = {
                 ) {
                     floatWhere = 'mck-msg-center';
                 }
-                msgBoxColorStyle =
-                    floatWhere === 'mck-msg-right'
-                        ? 'km-custom-widget-background-color'
-                        : 'km-custom-widget-background-color-secondary';
+                if (floatWhere === 'mck-msg-right') {
+                    msgBoxColorStyle = 'km-custom-widget-background-color';
+                    if (kmAttchMsg === 'km-attach-msg') {
+                        messageClass = 'vis message-class-right';
+                        attachmentBox = 'message-class-right';
+                        if (!msg.message) {
+                            messageClass = 'n-vis';
+                        }
+                    }
+                } else {
+                    msgBoxColorStyle =
+                        'km-custom-widget-background-color-secondary';
+                    if (kmAttchMsg === 'km-attach-msg') {
+                        messageClass = 'vis message-class-left';
+                        attachmentBox = 'message-class-left';
+                        if (!msg.message) {
+                            messageClass = 'n-vis';
+                        }
+                    }
+                }
 
                 statusIcon = _this.getStatusIconName(msg);
                 var replyId = msg.key;
@@ -8490,13 +8506,21 @@ var userOverride = {
                     KommunicateConstants.MESSAGE_CONTENT_TYPE.ATTACHMENT
                 ) {
                     var progressMeterClass = attachment ? 'n-vis' : 'vis';
-                    attachmentBox = attachment ? 'vis' : 'n-vis';
+                    attachmentBox = attachment ? attachmentBox : 'n-vis';
                     var progressMeter =
                         attachment && !msg.fileMeta.url && !msg.fileMeta.blobKey
                             ? Kommunicate.messageTemplate.getProgressMeterContanier(
                                   msg.key
                               )
                             : '';
+                }
+
+                if (
+                    !msg.message ||
+                    msg.contentType ==
+                        KommunicateConstants.MESSAGE_CONTENT_TYPE.LOCATION
+                ) {
+                    messageClass = 'n-vis';
                 }
                 var botMessageDelayClass = 'vis';
                 if (
@@ -8537,6 +8561,7 @@ var userOverride = {
 
                 var msgList = [
                     {
+                        kmAttchMsg: kmAttchMsg,
                         msgReply: replyMsg ? replyMsg.message + '\n' : '',
                         msgReplyTo: replyMsg ? replyTo + '\n' : '',
                         msgReplyDivExpr: replyMsg ? 'vis' : 'n-vis',
