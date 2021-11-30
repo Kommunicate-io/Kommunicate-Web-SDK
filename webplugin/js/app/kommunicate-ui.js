e181800ea122d0b6b009dcf9989507b9f55030c6
Abhishek Singh <aks3555@gmail.com>
2021-11-30 15:43:31 +0530

    added a fix for greeting notifications

--- webplugin/js/app/kommunicate-ui.js
+++ webplugin/js/app/kommunicate-ui.js
@@ -1408,7 +1408,6 @@ KommunicateUI = {
         var playPopupTone = KommunicateUtils.getDataFromKmSession(
             'playPopupNotificationTone'
         );
-        console.log(playPopupTone, mckChatPopupNotificationTone)
         if (showTemplate && !kommunicateCommons.isWidgetOpen()) {
             if (playPopupTone == null || playPopupTone) {
                 mckChatPopupNotificationTone &&
