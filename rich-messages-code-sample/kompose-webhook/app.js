5467b82d6a45bf3fd784bf4637f7fe9222f4697b
Shantnu Garg <gargshantnu@gmail.com>
2022-01-12 18:42:35 +0530

    add api to get welcome message

--- rich-messages-code-sample/kompose-webhook/app.js
+++ rich-messages-code-sample/kompose-webhook/app.js
@@ -18,10 +18,9 @@ function startApp() {
     });
 }
 
-app.post('/', function (req, res) {
-    const response = [{
-        "message": "This message can be generated from webhook" 
-    }];
+app.post('/welcome', function (req, res) {
+    console.log("Req body: ", req.body);
+    const response = getWelcomeMessage(req.body);
     return res.status(200).send(response);
 });
 
@@ -33,5 +32,35 @@ app.use((err, req, res, next) => {
     console.log("executing error handler", err);
     return res.status(500).send(err);
 });
+
+const getWelcomeMessage = (payload) => {
+    const userName = payload.metadata.KM_CHAT_CONTEXT.name || "There";
+    
+    // if user name is coming in context, use that, else put just `There`
+    const message = `Hey ${userName}, How may i help you today`;
+    
+    return [{
+        "platform": "kommunicate",
+        "message": message,
+        "metadata": {
+            "contentType": "300",
+            "templateId": "6",
+            "payload": [
+             {
+                "title": "Just Browsing!",
+                "message": "Just Browsing!"
+            },
+            {
+                "title": "Book a Demo",
+                "message": "Book a Demo"
+            },
+            {
+                "title": "Talk to Human",
+                "message": "Talk to Human"
+            }
+           ]
+        }
+    }];
+}
 
 module.exports = app;
