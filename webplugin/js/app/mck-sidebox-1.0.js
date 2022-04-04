9beeb23ac50a49254d7b48bfe55a0b5bff38f2b5
shubhamAgarwal3210 <shubhamagarwal@cerebry.co>
2022-04-04 12:57:58 +0530

    Removed string template as it is not supported in IE

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -3118,7 +3118,7 @@ var userOverride = {
                         );
                         kmChatInput.setAttribute(
                             'oninvalid',
-                            `setCustomValidity("${MCK_LABELS['lead.collection'].errorEmail}")`
+                            'setCustomValidity(' + MCK_LABELS['lead.collection'].errorEmail + ')'
                         );
                         kmChatInput.setAttribute(
                             'oninput',
