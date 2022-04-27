4a534e7272040b262141c52e6a3eb27d54cda296
Raj Karmakar <97rajmath@gmail.com>
2022-04-27 16:01:28 +0530

    Added separate function to handle memberleave event

--- webplugin/js/app/zendesk-chat-service.js
+++ webplugin/js/app/zendesk-chat-service.js
@@ -74,25 +74,8 @@ function ZendeskChatService() {
                     _this.handleZendeskAgentMessageEvent(eventDetails);
                 } else if (eventDetails.type == "chat.file") { //If agent sends file attachments
                     _this.handleZendeskAgentFileSendEvent(eventDetails);
-                } else if (eventDetails.type == "chat.memberleave") {
-                    //Resolve conversation on widget
-                    KommunicateConstants.CONVERSATION_RESOLVED_STATUS = true;
-                    KommunicateUI.showClosedConversationBanner(
-                        true
-                    );
-                    KommunicateUI.isConvJustResolved = true;
-                    
-                    //Call API to resolve the chat on Dashboard
-                    kommunicate.client.resolveConversation({ 
-                        groupId: CURRENT_GROUP_DATA.tabId 
-                    }, function(err, result) {
-                        if (err || !result) {
-                            console.log('An error occurred while resolving conversation ',err);
-                            return;
-                        }
-                        console.log(result)
-                    });
-
+                } else if (eventDetails.type == "chat.memberleave") { //If agent leaves conversation
+                    _this.handleZendeskAgentLeaveEvent(eventDetails);
                 }
             });
         }
@@ -266,7 +249,27 @@ function ZendeskChatService() {
             },
         });
 
-    }
+    };
+
+    _this.handleZendeskAgentLeaveEvent = function (event) {
+        //Resolve conversation on widget
+        KommunicateUI.showClosedConversationBanner(
+            true
+        );
+        KommunicateUI.isConvJustResolved = true;
+        
+        //Call API to resolve the conversation on Dashboard
+        kommunicate.client.resolveConversation({ 
+            groupId: CURRENT_GROUP_DATA.tabId 
+        }, function(err, result) {
+            if (err || !result) {
+                console.log("An error occurred while resolving conversation ",err);
+                return;
+            }
+            console.log("Resolved conversation on Kommunicate Dashboard", result);
+        });
+
+    }; 
 };
 
 
