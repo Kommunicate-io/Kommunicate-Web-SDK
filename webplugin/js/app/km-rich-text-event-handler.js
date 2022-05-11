9fedafc729f29b2fa52afa586bcfe49bc1e0c104
Pranav Pradeep <pranav@kommunicate.io>
2022-05-11 11:23:02 +0530

    [TD-2601] Partner Page data to be sent to the Sales Team

--- webplugin/js/app/km-rich-text-event-handler.js
+++ webplugin/js/app/km-rich-text-event-handler.js
@@ -647,12 +647,12 @@ Kommunicate.richMsgEventHandler = {
             };
             xhr.open('POST', form.action);
             xhr.send(JSON.stringify(data));
-        }else if(requestType == 'application/json') {
+        } else if(requestType == 'application/json') {
             var xhr = new XMLHttpRequest();
             xhr.open('POST', form.action);
             xhr.setRequestHeader('Content-type','application/json');
             xhr.send(JSON.stringify(data));
-        }else {
+        } else {
             !isActionableForm && form.submit(); // called for submit button
             isActionableForm &&
                 KommunicateUtils.isURL(form.action) &&
