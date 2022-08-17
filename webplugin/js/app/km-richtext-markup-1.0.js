c4e0e019ce94e8788f8009f4bf1bffd508f876ac
siddharth <sid9057819561@gmail.com>
2022-08-17 16:07:22 +0530

    radio_date_validation

--- webplugin/js/app/km-richtext-markup-1.0.js
+++ webplugin/js/app/km-richtext-markup-1.0.js
@@ -418,22 +418,33 @@ Kommunicate.markup = {
                                 {{#supported}}
                                     {{#radio}}
                                         <p class="mck-radio-group-title">{{title}}</p>
-                                        <div class="mck-form-radio-wrapper">
-                                            {{#options}}
-                                                <div>
-                                                    <input type="{{type}}" name="{{name}}" value="{{value}}">
-                                                    <label for="{{label}}" class="mck-form-label"><b>{{label}}</b></label>   
-                                                </div>                                     
-                                            {{/options}}
+                                        <div>
+                                        <div class="mck-form-radio-wrapper" style="margin-bottom:0px">
+                                        {{#options}}
+                                        <div>
+                                            <input type="{{type}}" name="{{name}}" value="{{value}}" data-regex="{{validation.regex}}" data-error-text="{{validation.errorText}}" >
+                                            <label for="{{label}}" class="mck-form-label"><b>{{label}}</b></label>   
+                                        </div>                                 
+                                    {{/options}} 
                                         </div>
+                                        {{#validation}}
+                                        <span class="mck-form-error-text mck-form-error-{{className}}" style="margin-top:0px"></span>
+                                    {{/validation}}     
+                                        </div>
+                                       
+                                      
                                     {{/radio}}
                                     {{#checkbox}}
                                         <p class="mck-radio-group-title">{{title}}</p>
                                         <div class="mck-form-radio-wrapper">
+    
                                             {{#options}}
                                                 <div>
                                                     <input type="{{type}}" name="{{name}}" value="{{value}}">
                                                     <label for="{{label}}" class="mck-form-label"><b>{{label}}</b></label>   
+                                                    {{#validation}}
+                                                    <span class="mck-form-error-text mck-form-error-{{className}}"></span>
+                                                {{/validation}}
                                                 </div>                                     
                                             {{/options}}
                                         </div>
@@ -484,7 +495,10 @@ Kommunicate.markup = {
                                 {{^supported}}
                                     <div class="mck-form-text-wrapper">
                                         <label for="{{label}}" class="mck-form-label"><b>{{label}}</b></label>
-                                        <input type="{{type}}" placeholder="{{placeholder}}" name="{{label}}" >
+                                        <input type="{{type}}" placeholder="{{placeholder}}" name="{{label}}" data-regex="{{validation.regex}}" data-error-text="{{validation.errorText}}" >
+                                        {{#validation}}
+                                                <span class="mck-form-error-text mck-form-error-{{className}}"></span>
+                                            {{/validation}}
                                     </div>
                                 {{/supported}}
                             {{/.}}
@@ -829,6 +843,7 @@ Kommunicate.markup.getActionableFormMarkup = function (options) {
                 : {};
         options.payload = payload;
         options.buttons = [];
+        console.log(options.payload)
         if (kommunicateCommons.isObject(options.payload[0].data)) {
             options.payload = options.payload.map(function (item) {
                 data = {};
@@ -840,7 +855,7 @@ Kommunicate.markup.getActionableFormMarkup = function (options) {
                 }
                 return data;
             });
-        }
+            console.log(options.payload)
         options.payload.forEach(function (item, index) {
             if (item.type == 'submit') {
                 isActionObject = kommunicateCommons.isObject(item.action);
@@ -879,6 +894,7 @@ Kommunicate.markup.getActionableFormMarkup = function (options) {
                 }
             }
         });
+        // console.log(options)
         return Mustache.to_html(Kommunicate.markup.getFormTemplate(), options);
     }
 };
@@ -1102,3 +1118,4 @@ Kommunicate.markup.getVideoMarkup = function (options) {
         return Mustache.to_html(Kommunicate.markup.getVideoTemplate(), options);
     }
 };
+}
