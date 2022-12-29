349b8a9f186fbee4bdfe1dbc63a0bfa2fd567f8d
Sanjeev Singh Negi <sanjeev@kommunicate.io>
2022-12-29 06:22:35 +0530

    adding running scripts
    adding running scripts

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,6 @@
-import {danger, message, fail, warn, schedule} from "danger";
-import { jiraIssue } from ("danger-plugin-jira-issue");
-import { noConsole } from 'danger-plugin-no-console'
+const { danger, message, fail, warn, schedule } = require("danger");
+const { default: jiraIssue } = require("danger-plugin-jira-issue");
+// const { default: noConsole } = require("danger-plugin-no-console"); this plugin has some issues
 
 const PR_REVIEWS_CHECKS = Object.freeze({
     BIG_PR_THRESHOLD: 700,
@@ -10,7 +10,7 @@ const PR_REVIEWS_CHECKS = Object.freeze({
         TITLE: `Title required Issue number`,
         CHECK_LOCK_FILE: `:exclamation: Changes were made to package.json, but not to package-lock.json - <i>'Perhaps you need to run \`npm install\`?'`,
         WIP: `PR is classed as Work in Progress`,
-        EXCEED_THRESHOLD: `:exclamation: Your PR has over ${BIG_PR_THRESHOLD} lines of code, please consider breaking it down so we can effectively review it. :thumbsup:`,
+        EXCEED_THRESHOLD: `:exclamation: Your PR has over ${this.BIG_PR_THRESHOLD} lines of code, please consider breaking it down so we can effectively review it. :thumbsup:`,
         MERGE_CONFLICTS: `Please merge development branch`,
         ONLY_DEV_BRANCH: `:exclamation: Please re-submit this PR to development branch, we may have already fixed your issue. 🙏`,
         MISSING_DES: `:exclamation: Please add a description to your PR to make it easier to review`,
@@ -22,7 +22,7 @@ const PR_REVIEWS_CHECKS = Object.freeze({
     },
     SUCCESS: {
         LINES_REMOVED : `:tada: The PR removed ${danger.github.pr.deletions} lines. :clap:`,
-        NEW_FILES: `New Files in this PR: \n - " ${newFiles}`
+        NEW_FILES: `New Files in this PR: \n - " ${`newFiles`}`
     }
 });
 
@@ -59,7 +59,7 @@ const packageChanged = danger.git.modified_files.includes("package.json"),
 if (packageChanged && !lockfileChanged) fail(PR_REVIEWS_CHECKS.WARNS.PACKAGE_JSON_ERR);
 
 //checks for console.log in PR
-schedule(noConsole({ whitelist: ['error', 'warn'] }))
+// schedule(noConsole({ whitelist: ['error', 'warn'] }))
 
 jiraIssue({
     key: PR_REVIEWS_CHECKS.TEAMS_INITIALS,
