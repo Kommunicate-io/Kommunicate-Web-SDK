98d3c039514cbf080bbf26f734391ac5e5fe0436
Abhishek Singh <abhishek@kommunicate.io>
2023-03-31 18:28:22 +0530

    Update km-richtext-markup-1.0.js

--- webplugin/js/app/km-richtext-markup-1.0.js
+++ webplugin/js/app/km-richtext-markup-1.0.js
@@ -439,8 +439,8 @@ getListMarkup:function(){
     
                                             {{#options}}
                                                 <div class="{{checkboxClass}}">
-                                                    <label class="mck-form-label ${msgKey}">
-                                                        <input id="{{label}} ${msgKey}" type="{{type}}" name="{{name}}" value="{{value}}">
+                                                    <label class="mck-form-label {{msgKey}}">
+                                                        <input id="{{label}} {{msgKey}}" type="{{type}}" name="{{name}}" value="{{value}}">
                                                         <span>
                                                             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                 <rect width="18" height="18" rx="9" fill="#00A4BF"/>
