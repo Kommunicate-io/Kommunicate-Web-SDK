04aac37ac00d7e69377e9cf08c5bd9f33ad155af
Sanjeev Singh Negi <sanjeev@kommunicate.io>
2023-05-14 16:20:12 +0530

    showing bot icon over last message when hidePostCta is enabled

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -5653,16 +5653,13 @@ var userOverride = {
             _this.sendMessage = function (messagePxy, file, callback) {
                 var key;
                 var message;
+                var lastMessageBeforeSend = $applozic("#mck-message-cell .mck-message-inner div[name='message']:last-child");
+
                 kmWidgetEvents.eventTracking(eventMapping.onMessageSent);
                 if (
-                    Kommunicate.internetStatus &&
-                    $applozic(
-                        "#mck-message-cell .mck-message-inner div[name='message']:last-child"
-                    ).data('msgkey') !== undefined
+                    Kommunicate.internetStatus && lastMessageBeforeSend.data('msgkey') !== undefined
                 ) {
-                    key = $applozic(
-                        "#mck-message-cell .mck-message-inner div[name='message']:last-child"
-                    ).data('msgkey');
+                    key = lastMessageBeforeSend.data('msgkey');
                     message = alMessageService.getReplyMessageByKey(key);
                 }
                 if (typeof messagePxy !== 'object') {
@@ -5779,6 +5776,9 @@ var userOverride = {
                                 );
                             }
                         );
+                        HIDE_POST_CTA &&
+                            lastMessageBeforeSend &&
+                            Kommunicate.hideMessage(lastMessageBeforeSend);
                     }
                     var optns = {
                         tabId: contact.contactId,
@@ -5883,6 +5883,9 @@ var userOverride = {
                                     );
                                 }
                             );
+                            HIDE_POST_CTA &&
+                                lastMessageBeforeSend &&
+                                Kommunicate.hideMessage(lastMessageBeforeSend);
                         }
                         var optns = {
                             tabId: contact.contactId,
@@ -14636,7 +14639,7 @@ var userOverride = {
             }
             _this.init = function () {
                 $applozic.template('fileboxTemplate', mck_filebox_tmpl);
-                //ataching events for rich msh templates
+                //ataching events for rich msg templates
                 Kommunicate.attachEvents($applozic);
                 $mck_file_upload.on('click', function (e) {
                     e.preventDefault();
