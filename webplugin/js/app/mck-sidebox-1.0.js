1aa9ac858a023f7b3eb4154f5a0b25c51f7de1ca
Sanjeev Singh Negi <sanjeev@kommunicate.io>
2023-05-16 01:17:23 +0530

    handling bot icon remove while csat given
    handling bot icon remove while csat given

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -3043,6 +3043,13 @@ var userOverride = {
                     contentType: 'application/json',
                     success: function (result) {
                         if (result && result.data) {
+                            var lastMessageBeforeSend = $applozic(
+                                "#mck-message-cell .mck-message-inner div[name='message']:last-child"
+                            );
+                            HIDE_POST_CTA &&
+                                lastMessageBeforeSend &&
+                                Kommunicate.hideMessage(lastMessageBeforeSend);
+
                             CURRENT_GROUP_DATA.currentGroupFeedback =
                                 result.data.data;
                             KommunicateUI.showClosedConversationBanner(true);
@@ -3054,6 +3061,7 @@ var userOverride = {
                                 'n-vis',
                                 ''
                             );
+
                         }
                     },
                     error: function () {
@@ -9163,7 +9171,6 @@ var userOverride = {
                 ) {
                         // this class is added to the message template if the message contains CTA buttons having only quick replies.
                        botMessageDelayClass = botMessageDelayClass + " contains-quick-replies-only";
-                 
                 }
                 
 
