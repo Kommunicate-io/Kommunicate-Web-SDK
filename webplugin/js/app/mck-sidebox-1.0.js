6f982792fab637ece152500a9dba7165f76192cc
panwaranuj01 <anuj@kommunicate.io>
2024-02-17 02:09:03 +0530

    fixed the bug

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -10230,7 +10230,7 @@ var userOverride = {
                             shouldScroll = true;
                             break;
                         case isUserMsg:
-                            shouldScroll = true;
+                            shouldScroll = false;
                             break;
                         case !firstMsgOfMsgsGroup?.classList.contains('n-vis'):
                             shouldScroll = true;
