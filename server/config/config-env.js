f2516f1ea7962ada4510e88540cd6187d591e02e
Ranjeet Priyadarshi <ranjeet@kommunicate.io>
2024-05-29 13:08:20 +0530

    Update config-env.js

--- server/config/config-env.js
+++ server/config/config-env.js
@@ -77,7 +77,7 @@ const configEnv = {
         urls: {
             applozicBaseUrl: 'https://chat-release.kommunicate.io',
             kommunicateBaseUrl: 'https://api-release.kommunicate.io',
-            botPlatformApi: 'https://bots-test.kommunicate.io',
+            botPlatformApi: 'https://bots-release.kommunicate.io',
             hostUrl: 'https://widget-release.kommunicate.io',
             dashboardUrl: 'https://dashboard-release.kommunicate.io',
         },
