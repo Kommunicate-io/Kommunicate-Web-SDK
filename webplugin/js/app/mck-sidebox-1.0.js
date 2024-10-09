63ebd383cfca82a3c4743cd6be58d4206f44b8d4
sakshi thakare <sakshi.thakare@kommunicate.io>
2024-10-09 10:11:53 +0530

    added  event listener and removing it on click

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -3559,6 +3559,7 @@ const firstVisibleMsg = {
 
                 modal.style.display = 'block';
                 captionText.innerHTML = title ? title : '';
+                document.addEventListener("keydown", closeWindowOnEscape);
             });
 
             parent.document.getElementById(
@@ -3567,15 +3568,17 @@ const firstVisibleMsg = {
                 parent.document.getElementById(
                     'km-fullscreen-image-modal'
                 ).style.display = 'none';
+                document.removeEventListener("click", closeWindowOnEscape);
             };
 
-            window.onkeyup= function(event){
-              if(event.keyCode==27) {
-                parent.document.getElementById(
-                    'km-fullscreen-image-modal'
-                ).style.display = 'none';
-              }
-            }
+            function closeWindowOnEscape(event) {
+                if(event.keyCode == 27) {
+                    parent.document.getElementById(
+                        'km-fullscreen-image-modal'
+                    ).style.display = 'none';
+                }
+                  document.removeEventListener("keydown", closeWindowOnEscape);
+                }
 
             $applozic(w).on('resize', function () {
                 if ($mck_file_menu.css('display') === 'block') {
