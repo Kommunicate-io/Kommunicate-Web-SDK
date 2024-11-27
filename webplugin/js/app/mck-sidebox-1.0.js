6c668629248d21db6f6014b7955ff9dab94ef3c3
deepaktiwari03 <152845256+deepaktiwari03@users.noreply.github.com>
2024-11-27 14:29:30 +0530

    Update mck-sidebox-1.0.js

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -4162,6 +4162,11 @@ const firstVisibleMsg = {
                                 `${time.substring(0, 2)}:${time.substring(2)}`
                         );
 
+                    // if start and end time are same, then it is a 24 hour business
+                    if (start === end) {
+                        return true;
+                    }
+
                     const startOfDay = moment.tz(
                         `${userMessageTimeInAgentTz.format(
                             'YYYY-MM-DD'
