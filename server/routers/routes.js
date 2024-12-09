0930a919ada9fd3de87772ca5f2f472508917ea8
Rachit Garg <rachit.garg@kommunicate.io>
2024-12-09 12:40:08 +0530

    Revert "Update routes.js"
    This reverts commit a399d83ca11dfa2882c74a07a5a5ea8f11ab8edf.

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -25,10 +25,9 @@ home.get('/:version/kommunicate.app', webpluginController.getPlugin);
 home.get('/chat', webpluginController.getPluginHTML)
 
 home.get('/robots.txt', (req, res) => {
-    const sanitizedHost = req.hostname.toLowerCase().trim();
     const allowedHosts = ['widget-cn.kommunicate.io', 'widget.kommunicate.io'];
     const robotsPath = path.resolve(__dirname, '../../robots.txt');
-    if (allowedHosts.includes(sanitizedHost)) {
+    if (allowedHosts.includes(req.hostname)) {
         res.type('text/plain');
         res.sendFile(robotsPath, err => {
             if (err) {
