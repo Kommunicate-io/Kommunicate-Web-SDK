51fd62e99e729d5abb3aa1a828c5224d1344dd90
jaitens <jaiten.sahu@kommunicate.io>
2024-12-12 17:17:21 +0530

    removing username and userId form cookies in case of Invalid password

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -2452,7 +2452,22 @@ const firstVisibleMsg = {
                             }
                             // if there is error then removing LeadCollection from cookies
                             kmCookieStorage.deleteCookie({
-                                name: KommunicateConstants.COOKIES.IS_USER_ID_FOR_LEAD_COLLECTION,
+                                name:
+                                    KommunicateConstants.COOKIES
+                                        .IS_USER_ID_FOR_LEAD_COLLECTION,
+                                domain: MCK_COOKIE_DOMAIN,
+                            });
+                            kmCookieStorage.deleteCookie({
+                                name:
+                                    KommunicateConstants.COOKIES
+                                        .KOMMUNICATE_LOGGED_IN_USERNAME,
+                                domain: MCK_COOKIE_DOMAIN,
+                            });
+                            kmCookieStorage.deleteCookie({
+                                name:
+                                    KommunicateConstants.COOKIES
+                                        .KOMMUNICATE_LOGGED_IN_ID,
+                                domain: MCK_COOKIE_DOMAIN,
                             });
                             throw new Error('INVALID_PASSWORD');
                         } else if (result === 'INVALID_APPID') {
