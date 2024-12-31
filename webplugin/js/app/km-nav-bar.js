c1412ed2dca455014230bfe8075ee9a235aa3b03
jaitens <jaiten.sahu@kommunicate.io>
2024-12-31 11:35:36 +0530

    added optional chaining

--- webplugin/js/app/km-nav-bar.js
+++ webplugin/js/app/km-nav-bar.js
@@ -34,7 +34,7 @@ function KmNavBar(mckMsgLayout) {
 
     _this.modifyTalkToHumanVisibility = function (isAssignedToHuman) {
         if (isAssignedToHuman) {
-            document.getElementById("km-talk-to-human").disabled = false;
+            document.getElementById("km-talk-to-human")?.disabled = false;
             kommunicateCommons.modifyClassList(
                 {
                     id: ['km-talk-to-human'],
