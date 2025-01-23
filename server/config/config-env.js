13f6b5c810289994ddd871418cfba078d263f705
jaitens <jaiten.sahu@kommunicate.io>
2025-01-23 17:40:52 +0530

    test gcp release

--- server/config/config-env.js
+++ server/config/config-env.js
@@ -73,29 +73,29 @@ const configEnv = {
     },
     
     release: {
-        port: '3031',
-        urls: {
-            applozicBaseUrl: 'https://chat-test.kommunicate.io',
-            kommunicateBaseUrl: 'https://api-test.kommunicate.io',
-            botPlatformApi: 'https://bots-test.kommunicate.io',
-            hostUrl: 'https://widget-release.kommunicate.io',
-            dashboardUrl: 'https://dashboard-test.kommunicate.io',
-        },
-        pluginProperties: {
-            pseudoNameEnabled: true,
-        },
-        thirdPartyIntegration: {
-            sentry: {
-                dsn:
-                    'https://b1187d11fbdc4632b29259b258dd9053@o418176.ingest.sentry.io/5338197',
-                enabled: true,
+            port: '3031',
+            urls: {
+                applozicBaseUrl: 'https://chat-gcp.kommunicate.io/',
+                kommunicateBaseUrl: 'https://api-server-prod-dot-km-prod-us.uk.r.appspot.com/',
+                botPlatformApi: 'https://bot-server-prod-dot-km-prod-us.uk.r.appspot.com/',
+                hostUrl: 'https://km-prod.web.app/',
+                dashboardUrl: 'https://dashboard-release.kommunicate.io',
             },
-            aws: {
-                cdnUrl: 'https://cdn-test.kommunicate.io',
-                bucket: 'kom-test-cdn',
+            pluginProperties: {
+                pseudoNameEnabled: true,
+            },
+            thirdPartyIntegration: {
+                sentry: {
+                    dsn:
+                        'https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911',
+                    enabled: false,
+                },
+                aws: {
+                    cdnUrl: 'https://cdn.kommunicate.io',
+                    bucket: 'kom-prod-cdn',
+                },
             },
         },
-    },
     
     prod: {
         port: '3030',
