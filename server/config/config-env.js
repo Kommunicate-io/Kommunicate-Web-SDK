74dbbc7bd0d914ff1a7a44d766834d9a58794788
jaitens <jaiten.sahu@kommunicate.io>
2025-01-23 18:02:38 +0530

    Revert "Update config-env.js"
    This reverts commit 7faeeeb4deb82c3329d6641f4baee98d16479f5a.

--- server/config/config-env.js
+++ server/config/config-env.js
@@ -1,116 +1,234 @@
-{
-  "development": {
-    "port": "3030",
-    "urls": {
-      "applozicBaseUrl": "https://chat-test.kommunicate.io",
-      "kommunicateBaseUrl": "https://api-test.kommunicate.io",
-      "botPlatformApi": "https://bots-test.kommunicate.io",
-      "hostUrl": "http://localhost:3030",
-      "dashboardUrl": "https://dashboard-test.kommunicate.io"
+const configEnv = {
+    development: {
+        port: '3030',
+        urls: {
+            applozicBaseUrl: 'https://chat-test.kommunicate.io',
+            kommunicateBaseUrl: 'https://api-test.kommunicate.io',
+            botPlatformApi: 'https://bots-test.kommunicate.io',
+            hostUrl: 'http://localhost:3030',
+            dashboardUrl: 'https://dashboard-test.kommunicate.io',
+        },
+        pluginProperties: {
+            pseudoNameEnabled: true,
+        },
+        thirdPartyIntegration: {
+            sentry: {
+                dsn:
+                    'https://b1187d11fbdc4632b29259b258dd9053@o418176.ingest.sentry.io/5338197',
+                enabled: false,
+            },
+            aws: {
+                cdnUrl: 'https://cdn-test.kommunicate.io',
+                bucket: 'kom-test-cdn',
+            },
+        },
     },
-    "pluginProperties": {
-      "pseudoNameEnabled": true
+    test: {
+        port: '3030',
+        urls: {
+            applozicBaseUrl: 'https://chat-test.kommunicate.io',
+            kommunicateBaseUrl: 'https://api-test.kommunicate.io',
+            botPlatformApi: 'https://bots-test.kommunicate.io',
+            hostUrl: 'https://widget-test.kommunicate.io',
+            dashboardUrl: 'https://dashboard-test.kommunicate.io',
+        },
+        pluginProperties: {
+            pseudoNameEnabled: true,
+        },
+        thirdPartyIntegration: {
+            sentry: {
+                dsn:
+                    'https://b1187d11fbdc4632b29259b258dd9053@o418176.ingest.sentry.io/5338197',
+                enabled: true,
+            },
+            aws: {
+                cdnUrl: 'https://cdn-test.kommunicate.io',
+                bucket: 'kom-test-cdn',
+            },
+        },
     },
-    "thirdPartyIntegration": {
-      "sentry": {
-        "dsn": "https://b1187d11fbdc4632b29259b258dd9053@o418176.ingest.sentry.io/5338197",
-        "enabled": false
-      },
-      "aws": {
-        "cdnUrl": "https://cdn-test.kommunicate.io",
-        "bucket": "kom-test-cdn"
-      }
-    }
-  },
-  "test": {
-    "port": "3030",
-    "urls": {
-      "applozicBaseUrl": "https://chat-test.kommunicate.io",
-      "kommunicateBaseUrl": "https://api-test.kommunicate.io",
-      "botPlatformApi": "https://bots-test.kommunicate.io",
-      "hostUrl": "https://widget-test.kommunicate.io",
-      "dashboardUrl": "https://dashboard-test.kommunicate.io"
+
+    release: {
+            port: '3031',
+            urls: {
+                applozicBaseUrl: 'https://chat-gcp.kommunicate.io/',
+                kommunicateBaseUrl: 'https://api-server-prod-dot-km-prod-us.uk.r.appspot.com/',
+                botPlatformApi: 'https://bot-server-prod-dot-km-prod-us.uk.r.appspot.com/',
+                hostUrl: 'https://km-prod.web.app/',
+                dashboardUrl: 'https://dashboard-release.kommunicate.io',
+            },
+            pluginProperties: {
+                pseudoNameEnabled: true,
+            },
+            thirdPartyIntegration: {
+                sentry: {
+                    dsn:
+                        'https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911',
+                    enabled: false,
+                },
+                aws: {
+                    cdnUrl: 'https://cdn.kommunicate.io',
+                    bucket: 'kom-prod-cdn',
+                },
+            },
+        },
+    
+    prod: {
+        port: '3030',
+        urls: {
+            applozicBaseUrl: 'https://chat.kommunicate.io',
+            kommunicateBaseUrl: 'https://api.kommunicate.io',
+            botPlatformApi: 'https://bots.kommunicate.io',
+            hostUrl: 'https://widget.kommunicate.io',
+            dashboardUrl: 'https://dashboard.kommunicate.io',
+        },
+        pluginProperties: {
+            pseudoNameEnabled: true,
+        },
+        thirdPartyIntegration: {
+            sentry: {
+                dsn:
+                    'https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911',
+                enabled: false,
+            },
+            aws: {
+                cdnUrl: 'https://cdn.kommunicate.io',
+                bucket: 'kom-prod-cdn',
+            },
+        },
     },
-    "pluginProperties": {
-      "pseudoNameEnabled": true
+    prod_cn: {
+        port: '3030',
+        urls: {
+            applozicBaseUrl: 'https://chat-cn.kommunicate.io',
+            kommunicateBaseUrl: 'https://api-cn.kommunicate.io',
+            hostUrl: 'https://widget-cn.kommunicate.io',
+            dashboardUrl: 'https://dashboard-cn.kommunicate.io',
+        },
+        pluginProperties: {
+            pseudoNameEnabled: true,
+        },
+        thirdPartyIntegration: {
+            sentry: {
+                dsn:
+                    'https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911',
+                enabled: false,
+            },
+            aws: {
+                cdnUrl: 'https://cdn.kommunicate.io',
+                bucket: 'kom-prod-cdn',
+            },
+        },
     },
-    "thirdPartyIntegration": {
-      "sentry": {
-        "dsn": "https://b1187d11fbdc4632b29259b258dd9053@o418176.ingest.sentry.io/5338197",
-        "enabled": true
-      },
-      "aws": {
-        "cdnUrl": "https://cdn-test.kommunicate.io",
-        "bucket": "kom-test-cdn"
-      }
-    }
-  },
-  "release": {
-    "port": "3031",
-    "urls": {
-      "applozicBaseUrl": "https://chat-gcp.kommunicate.io/",
-      "kommunicateBaseUrl": "https://api-server-prod-dot-km-prod-us.uk.r.appspot.com/",
-      "botPlatformApi": "https://bot-server-prod-dot-km-prod-us.uk.r.appspot.com/",
-      "hostUrl": "https://km-prod.web.app/",
-      "dashboardUrl": "https://dashboard-release.kommunicate.io"
+
+    prod_in: {
+        port: '3030',
+        urls: {
+            applozicBaseUrl: 'https://chat-in.kommunicate.io',
+            kommunicateBaseUrl: 'https://api-in.kommunicate.io',
+            botPlatformApi: 'https://bots-in.kommunicate.io',
+            hostUrl: 'https://widget-in.kommunicate.io',
+            dashboardUrl: 'https://dashboard-in.kommunicate.io',
+        },
+        pluginProperties: {
+            pseudoNameEnabled: true,
+        },
+        thirdPartyIntegration: {
+            sentry: {
+                dsn:
+                    'https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911',
+                enabled: false,
+            },
+            aws: {
+                cdnUrl: 'https://cdn.kommunicate.io',
+                bucket: 'kom-prod-cdn',
+            },
+        },
     },
-    "pluginProperties": {
-      "pseudoNameEnabled": true
+    
+    prod_eu: {
+        port: '3030',
+        urls: {
+            applozicBaseUrl: 'https://chat-eu.kommunicate.io',
+            kommunicateBaseUrl: 'https://api-eu.kommunicate.io',
+            botPlatformApi: 'https://bots-eu.kommunicate.io',
+            hostUrl: 'https://widget-eu.kommunicate.io',
+            dashboardUrl: 'https://dashboard-eu.kommunicate.io',
+        },
+        pluginProperties: {
+            pseudoNameEnabled: true,
+        },
+        thirdPartyIntegration: {
+            sentry: {
+                dsn:
+                    'https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911',
+                enabled: false,
+            },
+            aws: {
+                cdnUrl: 'https://cdn.kommunicate.io',
+                bucket: 'kom-prod-cdn',
+            },
+        },
     },
-    "thirdPartyIntegration": {
-      "sentry": {
-        "dsn": "https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911",
-        "enabled": false
-      },
-      "aws": {
-        "cdnUrl": "https://cdn.kommunicate.io",
-        "bucket": "kom-prod-cdn"
-      }
-    }
-  },
-  "prod": {
-    "port": "3030",
-    "urls": {
-      "applozicBaseUrl": "https://chat.kommunicate.io",
-      "kommunicateBaseUrl": "https://api.kommunicate.io",
-      "botPlatformApi": "https://bots.kommunicate.io",
-      "hostUrl": "https://widget.kommunicate.io",
-      "dashboardUrl": "https://dashboard.kommunicate.io"
+    
+    prod_enterprise: {
+        port: '3033',
+        urls: {
+            applozicBaseUrl: 'https://chat.kommunicate.io',
+            kommunicateBaseUrl: 'https://api-enterprise.kommunicate.io',
+            botPlatformApi: 'https://bots.kommunicate.io',
+            hostUrl: 'https://widget-enterprise.kommunicate.io',
+            dashboardUrl: 'https://dashboard-enterprise.kommunicate.io',
+        },
+        pluginProperties: {
+            pseudoNameEnabled: true,
+        },
+        thirdPartyIntegration: {
+            sentry: {
+                dsn:
+                    'https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911',
+                enabled: false,
+            },
+            aws: {
+                cdnUrl: 'https://cdn.kommunicate.io',
+                bucket: 'kom-prod-cdn',
+            },
+        },
     },
-    "pluginProperties": {
-      "pseudoNameEnabled": true
+    prod_beta: {
+        port: '3034',
+        urls: {
+            applozicBaseUrl: 'https://chat.kommunicate.io',
+            kommunicateBaseUrl: 'https://api.kommunicate.io',
+            botPlatformApi: 'https://bots.kommunicate.io',
+            hostUrl: 'https://widget-beta.kommunicate.io',
+            dashboardUrl: 'https://beta.kommunicate.io',
+        },
+        pluginProperties: {
+            pseudoNameEnabled: true,
+        },
+        thirdPartyIntegration: {
+            sentry: {
+                dsn:
+                    'https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911',
+                enabled: false,
+            },
+            aws: {
+                cdnUrl: 'https://cdn.kommunicate.io',
+                bucket: 'kom-prod-cdn',
+            },
+        },
     },
-    "thirdPartyIntegration": {
-      "sentry": {
-        "dsn": "https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911",
-        "enabled": false
-      },
-      "aws": {
-        "cdnUrl": "https://cdn.kommunicate.io",
-        "bucket": "kom-prod-cdn"
-      }
-    }
-  },
-  "prod_cn": {
-    "port": "3030",
-    "urls": {
-      "applozicBaseUrl": "https://chat-cn.kommunicate.io",
-      "kommunicateBaseUrl": "https://api-cn.kommunicate.io",
-      "hostUrl": "https://widget-cn.kommunicate.io",
-      "dashboardUrl": "https://dashboard-cn.kommunicate.io"
+    commonResources: {
+        // add common resources for all environments below
     },
-    "pluginProperties": {
-      "pseudoNameEnabled": true
-    },
-    "thirdPartyIntegration": {
-      "sentry": {
-        "dsn": "https://9f71614ef8184d0cab00074555dad9a7@sentry.io/1321911",
-        "enabled": false
-      },
-      "aws": {
-        "cdnUrl": "https://cdn.kommunicate.io",
-        "bucket": "kom-prod-cdn"
-      }
-    }
-  }
-}
+};
+
+const getEnvId = function () {
+    return process.env.NODE_ENV || 'development';
+};
+
+const config = configEnv[getEnvId()];
+
+module.exports = config;
+module.exports.getEnvId = getEnvId;
