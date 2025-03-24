ad2b86239115e81cbfd3c7dd1d15b0be2536f94e
MohitChaudhari <mohit.chaudhari@kommunicate.io>
2025-03-24 11:36:39 +0530

    Revert "style code moved to custom element"
    This reverts commit a01c5f6c24082d1cd2b5ba4497f799121df230e0.

--- webplugin/js/app/components/custom-element.js
+++ webplugin/js/app/components/custom-element.js
@@ -2,29 +2,10 @@ class ShadowDomComponent extends HTMLElement {
     constructor() {
         super();
         this._shadow = this.attachShadow({ mode: 'open' });
-        
-        const style = document.createElement('style');
-        style.textContent = `
-            table {
-                border-collapse: separate;
-                border-spacing: 0;
-                background-color: #EFEFEF;
-                overflow: hidden;
-                border: 1px solid #D0D5DD;
-                border-radius: 15px;
-                font-weight: 300;
-                color: #535862;
-            }
-            td {
-                padding: 15px;
-                border: 1px solid #D0D5DD;
-                text-align: center;
-            }
-        `;
-        this._shadow.appendChild(style);
     }
 }
 
+// Define the custom element
 if ('customElements' in window) {
     if (!window.customElements.get('mck-html-rich-message')) {
         window.customElements.define('mck-html-rich-message', ShadowDomComponent);
