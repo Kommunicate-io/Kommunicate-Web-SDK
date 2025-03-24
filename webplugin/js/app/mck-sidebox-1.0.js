a01c5f6c24082d1cd2b5ba4497f799121df230e0
MohitChaudhari <mohit.chaudhari@kommunicate.io>
2025-03-24 10:13:44 +0530

    style code moved to custom element

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -8756,28 +8756,7 @@ const firstVisibleMsg = {
                         KommunicateUtils.customElementSupported()
                     ) {
                         const kmElement = document.createElement('mck-html-rich-message');
-                        kmElement._shadow.innerHTML = emoji_template;
-                        if (kmElement && kmElement.shadowRoot) {
-                            const style = document.createElement('style');
-                            style.textContent = `
-                            table {
-                                border-collapse: separate;
-                                border-spacing: 0;
-                                background-color: #EFEFEF;
-                                overflow: hidden;
-                                border: 1px solid #D0D5DD;
-                                border-radius: 15px;
-                                font-weight: 300;
-                                color: #535862;
-                            }
-                            td {
-                                padding: 15px;
-                                border: 1px solid #D0D5DD;
-                                text-align: center;
-                            }
-                        `;
-                            kmElement.shadowRoot.appendChild(style);
-                        }
+                        kmElement._shadow.innerHTML += emoji_template;
                         $textMessage.append(kmElement);
 
                         htmlRichMessage = true;
