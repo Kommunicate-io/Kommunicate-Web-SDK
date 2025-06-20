87d75d0ebe9dadeae317081ec5e3fccc958784ec
thakaresakshi121 <sakshi.thakare@kommunicate.io>
2025-06-20 19:06:06 +0530

    updated

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -105,6 +105,7 @@ const firstVisibleMsg = {
             // updating groupName to conversationTitle, supporting groupName for backward compatibility
             appOptions.conversationTitle = appOptions.conversationTitle || appOptions.groupName;
             appOptions.accessToken = appOptions.password || appOptions.accessToken;
+            appOptions.connectSocketOnWidgetClick = true;
         }
         var oInstance = undefined;
         if (typeof $mck_sidebox.data('applozic_instance') !== 'undefined') {
@@ -633,7 +634,7 @@ const firstVisibleMsg = {
                 clearTimeout(this.SOCKET_DISCONNECT_TIMEOUT);
             },
         };
-        var CONNECT_SOCKET_ON_WIDGET_CLICK = true;
+        var CONNECT_SOCKET_ON_WIDGET_CLICK = appOptions.connectSocketOnWidgetClick || false;
         var SUBSCRIBE_TO_EVENTS_BACKUP = [];
         var DEFAULT_ENCRYPTED_APP_VERSION = 111; // Update it to 112 to enable encryption for socket messages.
         kommunicateCommons.checkIfDeviceIsHandheld() &&
