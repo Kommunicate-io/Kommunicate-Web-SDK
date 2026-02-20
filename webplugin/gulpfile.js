664318b17fc9452d951ea465aa2cf53c039a73b9
AashishBhatt5 <aashish.bhatt@kommunicate.io>
2026-02-20 12:07:49 +0530

    adding utils file

--- webplugin/gulpfile.js
+++ webplugin/gulpfile.js
@@ -259,6 +259,11 @@ const generateBuildFiles = () => {
             'lib/js/mck-emojis.min.js',
             `${resourceLocation}/third-party-scripts/mck-emojis.min.js`
         );
+        // intl-tel-input utils for pre-chat phone validation
+        copyFileToBuild(
+            'lib/js/intl-tel-utils-25.14.0.js',
+            `${legacyThirdPartyDir}/intl-tel-utils-25.14.0.js`
+        );
         // legacy path for existing redirects
         copyFileToBuild('lib/js/mck-emojis.min.js', `${legacyThirdPartyDir}/mck-emojis.min.js`);
         // legacy /plugin/lib/js path for CDN compatibility
