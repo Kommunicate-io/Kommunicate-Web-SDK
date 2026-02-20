d4adab5054e13994f50a6e77c3c58a923dbfe2af
AashishBhatt5 <aashish.bhatt@kommunicate.io>
2026-02-20 12:33:36 +0530

    fix

--- webplugin/gulpfile.js
+++ webplugin/gulpfile.js
@@ -261,9 +261,10 @@ const generateBuildFiles = () => {
         );
         // intl-tel-input utils for pre-chat phone validation
         copyFileToBuild(
-            'lib/js/intl-tel-utils-25.14.0.js',
-            `${legacyThirdPartyDir}/intl-tel-utils-25.14.0.js`
+            'lib/js/intl-tel-utils.js',
+            `${resourceLocation}/third-party-scripts/intl-tel-utils.js`
         );
+        copyFileToBuild('lib/js/intl-tel-utils.js', `${legacyThirdPartyDir}/intl-tel-utils.js`);
         // legacy path for existing redirects
         copyFileToBuild('lib/js/mck-emojis.min.js', `${legacyThirdPartyDir}/mck-emojis.min.js`);
         // legacy /plugin/lib/js path for CDN compatibility
