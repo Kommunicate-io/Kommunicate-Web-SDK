7ba6e3cc6ad7fb211e0885accdcbd700b4150b66
Int <int@Ints-MacBook-Pro.local>
2026-04-07 18:14:57 +0530

    cleanup

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -5435,7 +5435,7 @@ const firstVisibleMsg = {
                 $applozic(d).on('click', '#km-modal-close', function (e) {
                     e.preventDefault();
                     e.stopPropagation();
-                    if (mckInit && typeof mckInit.closeLeadCollectionWindow === 'function') {
+                    if (mckInit) {
                         mckInit.closeLeadCollectionWindow();
                         return;
                     }
