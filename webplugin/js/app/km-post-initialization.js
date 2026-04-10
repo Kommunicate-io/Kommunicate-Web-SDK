8978e75c4ff2d4d84ae6ebf45f555d371b6a8a4a
Int <int@Ints-MacBook-Pro.local>
2026-04-10 17:42:30 +0530

    cleanup

--- webplugin/js/app/km-post-initialization.js
+++ webplugin/js/app/km-post-initialization.js
@@ -97,9 +97,10 @@ Kommunicate.getFaqCategories = function (data) {
                 var safeCategoryNameAttr = kommunicateCommons.encodeCategoryNameForAttribute(
                     categoryName
                 );
+                var safeCategoryNameForAria = kommunicateCommons.escapeAttributeValue(categoryName);
                 $applozic('.km-faq-category-list-container').append(
                     '<div class="km-faq-category-card" role="button" tabindex="0" aria-label="' +
-                        safeCategoryName +
+                        safeCategoryNameForAria +
                         '" data-category-name="' +
                         safeCategoryNameAttr +
                         '">' +
