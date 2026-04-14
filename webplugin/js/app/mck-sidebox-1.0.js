ccdb5e9efcebb690bebb4c05d884d98e92db2071
thakaresakshi121 <sakshi.thakare@kommunicate.io>
2026-04-14 19:43:03 +0530

    updated

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -12413,10 +12413,7 @@ const firstVisibleMsg = {
             _this.onMessage = function (resp) {
                 // In case of encryption enabled, response is comming after getting decrypted from the parent function.
                 var activeTabId = $mck_message_inner.data('mck-id');
-                if (
-                    typeof kmVoiceMessageHandler !== 'undefined' &&
-                    kmVoiceMessageHandler.handleSocketVoiceStream(resp, activeTabId, appOptions)
-                ) {
+                if (kmVoiceMessageHandler.handleSocketVoiceStream(resp, activeTabId, appOptions)) {
                     return;
                 }
                 typeof resp.message == 'object' &&
