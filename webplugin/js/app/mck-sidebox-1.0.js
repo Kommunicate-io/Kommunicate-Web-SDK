4461739102c30127c5297284bcd7b1310a10e237
Int <int@Ints-MacBook-Pro.local>
2026-04-22 15:23:13 +0530

    cleanup

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -7002,6 +7002,15 @@ const firstVisibleMsg = {
                         }
                     );
             }
+            function bindRoleButtonKeyboardActivation() {
+                var selector =
+                    '#km-restart-conversation, #user-overide-voice-output, .voiceNote, .voiceInput, #delete-recording, #pause-btn, #play-btn, #send-btn, #mck-stop-recording, #mck-voice-repeat-last-msg';
+                $applozic(d)
+                    .off('keydown.mckRoleButtonActivation', selector)
+                    .on('keydown.mckRoleButtonActivation', selector, function (event) {
+                        handleActivationKey(event, this.click.bind(this));
+                    });
+            }
             _this.latestMessageReceivedTime = '';
             _this.init = function () {
                 $applozic.template('messageTemplate', markup);
@@ -7010,6 +7019,7 @@ const firstVisibleMsg = {
                 $applozic.template('staticMessageTemplate', staticMessageModule);
                 $applozic.template('assigneeModule', assigneeModule);
                 bindDropdownKeyboardActivation();
+                bindRoleButtonKeyboardActivation();
             };
             _this.removeStaticMessage = function () {
                 var staticMessageContainer = document.getElementById('km-static-message');
