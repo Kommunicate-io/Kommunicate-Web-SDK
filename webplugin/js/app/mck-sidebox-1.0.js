a55a9a72193320c6e82f55a1fcf5936c7469ebe1
Int <int@Ints-MacBook-Pro.local>
2026-07-09 16:26:26 +0530

    cleanup

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -5739,6 +5739,7 @@ const firstVisibleMsg = {
                     resolvedMetadata.KM_INPUT_TYPE ||
                     typeof mckVoice === 'undefined' ||
                     !mckVoice ||
+                    typeof mckVoice.isVoiceModeActive !== 'function' ||
                     !mckVoice.isVoiceModeActive() ||
                     !messagePxy.message ||
                     messagePxy.fileMeta
